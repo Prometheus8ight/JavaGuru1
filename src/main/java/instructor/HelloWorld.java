9443d278ab0c10082537b55551e502198af8c869
Konstantins Buts <kjb.dgsv@gmail.com>
2021-01-20 21:33:00 +0200

    Add another text message

--- src/main/java/instructor/HelloWorld.java
+++ src/main/java/instructor/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("It is a great day today!");
+        System.out.println("Yet another update");
     }
 }
