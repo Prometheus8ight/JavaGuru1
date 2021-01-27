68d356c5075d6a397bfb05c863e67453a507707d
CoArchie <c.chikun@icloud.com>
2021-01-27 21:47:48 +0200

    Hello, i did it. :) This is my 1st time made homework solution.

---
+++ src/main/java/students/arkadij/lesson_2/level_0/task_0/FirstDemoClass.java
@@ -0,0 +1,7 @@
+package students.arkadij.lesson_2.level_0.task_0;
+
+class FirstDemoClass {
+    public static void main(String[] args) {
+        System.out.println("My Homework Soulution!");
+    }
+}
