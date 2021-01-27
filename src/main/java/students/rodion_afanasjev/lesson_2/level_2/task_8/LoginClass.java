031c286b2da920bc080a3ea3f30c4b958f7f8773
RodionA <raf91@inbox.lv>
2021-01-28 00:44:58 +0200

    Lesson-2-task-8

---
+++ src/main/java/students/rodion_afanasjev/lesson_2/level_2/task_8/LoginClass.java
@@ -0,0 +1,15 @@
+package students.rodion_afanasjev.lesson_2.level_2.task_8;
+
+public class LoginClass {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println ("Enter your Name: ");
+
+        String name = scanner.next();
+
+        System.out.println("Hello " + name);
+
+    }
+}
