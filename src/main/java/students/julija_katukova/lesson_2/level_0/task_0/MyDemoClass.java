aaaebffa51655b6da31f36f502a59b0238ba6d49
Julija Katukova <julija.katukova@gmail.com>
2021-01-28 17:18:32 +0200

    My homework solution

--- src/main/java/students/julija_katukova/lesson_2/level_0/task_0/MyDemoClass.java
+++ src/main/java/students/julija_katukova/lesson_2/level_0/task_0/MyDemoClass.java
@@ -2,7 +2,7 @@ package students.julija_katukova.lesson_2.level_0.task_0;
 
 class MyDemoClass {
     public static void main(String[] args) {
-        System.out.println("My homework Solution!");
+        System.out.println("My homework Solution! lalalala");
     }
 
 
