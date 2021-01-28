92c43674a73f4367ceaf01fe5fc0c339c8700519
RodionA <raf91@inbox.lv>
2021-01-28 05:36:17 +0200

    Lesson-2-task-31

---
+++ src/main/java/students/rodion_afanasjev/lesson_2/level_7/task_31/Comment.java
@@ -0,0 +1,14 @@
+package students.rodion_afanasjev.lesson_2.level_7.task_31;
+
+class Comment {
+
+    public static void main(String[] args) {
+
+        System.out.println("Now i will comment this code!"); // Right now you are reading this comment ;)
+        System.out.println("Now i will comment second code!"); /* Second comment! ;)
+
+        */
+        System.out.println("Second comment ends above this code!");
+    }
+
+}
