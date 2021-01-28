401db6ea5b4aabcafab42921edf531cde36b805a
andrewhaka78 <andrewhaka@mail.ru>
2021-01-28 11:13:14 +0200

    My homework solution

---
+++ src/main/java/students/andrei_tkatsuk/lesson_2/level_0/task_0/AndrewsDemoClass.java
@@ -0,0 +1,8 @@
+package students.andrei_tkatsuk.lesson_2.level_0.task_0;
+
+class AndrewsDemoClass {
+
+    public static void main(String[] args) {
+        System.out.println("My Homework");
+    }
+}
