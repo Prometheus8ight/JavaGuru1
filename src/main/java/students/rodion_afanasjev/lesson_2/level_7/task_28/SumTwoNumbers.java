4d032e519e67f61c5730ffa6bd219f797b0d5d80
RodionA <raf91@inbox.lv>
2021-01-28 05:35:32 +0200

    Lesson-2-task-28

---
+++ src/main/java/students/rodion_afanasjev/lesson_2/level_7/task_28/SumTwoNumbers.java
@@ -0,0 +1,17 @@
+package students.rodion_afanasjev.lesson_2.level_7.task_28;
+
+class SumTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        int numberTwo = 20;
+        int sum = numberOne + numberTwo;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+        System.out.println("Sum = " + sum);
+    }
+
+}
+
