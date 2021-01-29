1a0d8040cda2edc23eed60f7bd18f18a56e081fc
Konstantins Buts <kjb.dgsv@gmail.com>
2021-01-29 16:04:04 +0200

    Revert removed file

--- src/main/java/students/Placeholder.java
+++ src/main/java/students/Placeholder.java
@@ -3,6 +3,16 @@ package students;
 public class Placeholder {
 
     public static void main(String[] args) {
-        System.out.println("Nothing to do here");
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter b: ");
+        int b = scanner.nextInt();
+
+        int sum = a + b;
+        System.out.println("a + b = " + sum);
+
     }
 }
