08dccd3dd1d703e2ee83dbad0dc9a00fd7af274a
CoArchie <c.chikun@icloud.com>
2021-01-29 23:39:32 +0200

    Level_4 - task_15

---
+++ src/main/java/students/arkadij/lesson_2/level_5/Task_15.java
@@ -0,0 +1,26 @@
+package students.arkadij.lesson_2.level_5;
+
+public class Task_15 {
+    public static void main(String[] args) {
+        System.out.println("Let's play a little. ");
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("How old are you? ");
+        int howOld = scanner.nextInt();
+
+        System.out.println("Okay, now try to guess how old I am?");
+        int oldIam = scanner.nextInt();
+
+        System.out.print("Your age is: " + howOld + " || ");
+        System.out.print(" You think my age is like that: " + oldIam + " || ");
+
+        if (oldIam == (30)) {
+            System.out.println(" You're right sir!");
+        } // end of if-else
+        else {
+            System.out.println(" You didn't guess. ");
+        } // end of if
+
+
+    }
+}
