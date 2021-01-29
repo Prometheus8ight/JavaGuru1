ab6453a3564ed197240ad6742fa10bb0eec69d36
Julija Katukova <julija.katukova@gmail.com>
2021-01-29 14:39:17 +0200

    Task1 Attempt1 Hopefully, it works right

--- src/main/java/instructor/HelloWorld.java
+++ src/main/java/instructor/HelloWorld.java
@@ -7,5 +7,10 @@ public class HelloWorld {
         System.out.println("It is a great day today!");
         System.out.println("Yet another update");
         System.out.println("It is sunday!");
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = scanner.nextInt();
+        System.out.println("Спасибо! Вы ввели число " + number);
     }
 }
