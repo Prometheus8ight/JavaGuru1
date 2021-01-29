7e959ba60b53ffff5c404510c59d92c9aea98e65
CoArchie <c.chikun@icloud.com>
2021-01-30 00:37:03 +0200

    Level_5 - task_18

---
+++ src/main/java/students/arkadij/lesson_2/level_5/Task_18.java
@@ -0,0 +1,9 @@
+package students.arkadij.lesson_2.level_5;
+
+public class Task_18 {
+    public static void main(String[] args) {
+        System.out.println("Sorry, i have an errors.");
+        System.out.println("Please fix me!");
+        // думаю, вы догодались, что было исправлено.
+    }
+}
