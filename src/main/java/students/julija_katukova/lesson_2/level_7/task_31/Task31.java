75360108063eefa3ec12e247efe8638a0aca2b26
Julija Katukova <julija.katukova@gmail.com>
2021-01-30 12:03:45 +0200

    Lesson2 Level7 Task31

---
+++ src/main/java/students/julija_katukova/lesson_2/level_7/task_31/Task31.java
@@ -0,0 +1,13 @@
+package students.julija_katukova.lesson_2.level_7.task_31;
+
+class Task31 {
+    public static void main(String[] args) {
+        System.out.println("Три девицы под окном");
+      /*  System.out.println("Жил-был поп,\n" +
+                "Толоконный лоб.");*/
+        System.out.println("Пряли поздно вечерком.");
+        System.out.println("Кабы я была царица, —\n" +
+                "Говорит одна девица...");
+        //Сказка о царе Салтане - Пушкин
+    }
+}
