7c50e78233647e1bca648fb201603c44367479e2
Julija Katukova <julija.katukova@gmail.com>
2021-01-30 11:14:12 +0200

    Lesson2 Level6 Task24

---
+++ src/main/java/students/julija_katukova/lesson_2/level_6/task_24/NoSleepNoFocus.java
@@ -0,0 +1,12 @@
+package students.julija_katukova.lesson_2.level_6.task_24;
+
+class NoSleepNoFocus {
+
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee"); //добавить двойные кавычки
+        System.out.println("So bad"); // и тут тоже
+    }
+
+
+
+}
