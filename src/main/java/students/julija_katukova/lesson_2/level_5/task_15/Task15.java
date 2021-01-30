4c1d9fe5b2d6d6c1f0e2a47bce347104389d286a
Julija Katukova <julija.katukova@gmail.com>
2021-01-30 10:29:24 +0200

    Lesson2 Level5 Task15

---
+++ src/main/java/students/julija_katukova/lesson_2/level_5/task_15/Task15.java
@@ -0,0 +1,11 @@
+package students.julija_katukova.lesson_2.level_5.task_15;
+
+class Task15 {
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+        System.out.print("Konstantin! ");
+        System.out.print("How are you?");
+
+    }
+
+}
