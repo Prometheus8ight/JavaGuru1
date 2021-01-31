87da7b5c0235e38e824722a6ca0f9f9a9c776a9c
KateBristol <jekaterina.telezkina@gmail.com>
2021-01-31 21:27:28 +0200

    task 8

---
+++ src/main/java/students/kate_br/lesson_2/level_2/task_8/Hello.java
@@ -0,0 +1,17 @@
+package students.kate_br.lesson_2.level_2.task_8;
+
+
+ class Hello {
+
+     public static void main(String[] args) {
+
+         java.util.Scanner scanner = new java.util.Scanner(System.in);
+         System.out.println("Enter name:");
+         String username = scanner.nextLine();
+
+         System.out.println("Hello" + username);
+
+
+     }
+}
+
