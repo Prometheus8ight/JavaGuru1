6ad80c1932c4514e98ddfd8135eb5aa91fc98618
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-01-31 14:35:06 +0100

    Bogdans Pavlovs - Lesson 2 - Level 2 - Task 8

---
+++ src/main/java/students/bogdans_pavlovs/level_2/task_8/task_8.java
@@ -0,0 +1,16 @@
+package students.bogdans_pavlovs.level_2.task_8;
+
+class task_8 {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + (name) + "!");
+
+        //Hello имя пользователя!
+    }
+}
