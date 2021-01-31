1b52996438a3e519772af7da4d1a89d6ab8349be
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-01-31 14:00:34 +0100

    Bogdans Pavlovs - Lesson 2 - Level 1 - Task 2

---
+++ src/main/java/students/bogdans_pavlovs/level_1/task_2/task_2.java
@@ -0,0 +1,22 @@
+package students.bogdans_pavlovs.level_1.task_2;
+
+class task_2 {
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Enter 1st number: ");
+        double firstDoubleNumber = scanner.nextDouble();
+
+        System.out.print("Enter 2nd number: ");
+        double secondDoubleNumber = scanner.nextDouble();
+
+        //(числа с запятой) и выводит на консоль результаты: сложения, вычитания, умножения и деления.
+
+        System.out.println("Addition: " + (firstDoubleNumber + secondDoubleNumber));
+        System.out.println("Subtraction: " + (firstDoubleNumber - secondDoubleNumber));
+        System.out.println("Multiplication: " + (firstDoubleNumber * secondDoubleNumber));
+        System.out.println("Division: " + (firstDoubleNumber / secondDoubleNumber));
+
+    }
+}
