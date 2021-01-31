e433ec6a6d966c8823fac93c6009cb8efc351db1
Konstantins Buts <35545007+konstantins-buts@users.noreply.github.com>
2021-01-31 13:46:47 +0100

    Bogdans Pavlovs - Lesson 2 - Level 1 - Task 1

--- src/main/java/students/bogdans_pavlovs/level_1/task_1/task_1.java
+++ src/main/java/students/bogdans_pavlovs/level_1/task_1/task_1.java
@@ -2,6 +2,20 @@ package students.bogdans_pavlovs.level_1.task_1;
 
 class task_1 {
     public static void main(String[] args) {
-        System.out.println("homework?");
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Enter 1st number: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Enter 2nd number: ");
+        int secondNumber = scanner.nextInt();
+
+        // выведите на консоль полученные результаты сложения, вычитания, умножения и деления
+
+        System.out.println("Addition: " + (firstNumber + secondNumber));
+        System.out.println("Subtraction: " +(firstNumber - secondNumber));
+        System.out.println("Multiplication: " +(firstNumber * secondNumber));
+        System.out.println("Division: " + (firstNumber / secondNumber));
+
     }
 }
