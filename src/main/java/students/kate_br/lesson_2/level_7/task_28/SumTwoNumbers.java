b0e2e2d55efe700ff6d0ebaa9bf7ff8121877967
KateBristol <jekaterina.telezkina@gmail.com>
2021-01-31 19:19:09 +0200

    task 28

---
+++ src/main/java/students/kate_br/lesson_2/level_7/task_28/SumTwoNumbers.java
@@ -0,0 +1,19 @@
+package students.kate_br.lesson_2.level_7.task_28;
+
+ class SumTwoNumbers {
+
+     public static void main(String[] args) {
+
+         int Number1 = 30;
+         int Number2 = 40;
+
+         System.out.println(Number1 + Number2);
+
+         int NumberOne = 10;
+         int NumberTwo = 20;
+         int sum = NumberOne + NumberTwo;
+
+         System.out.println(sum);
+
+     }
+}
