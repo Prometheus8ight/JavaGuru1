06c227a008298c8769e77b04e4d375f5f182f977
DJ9780772 <denissjankovskis@outlook.com>
2021-02-01 17:43:33 +0200

    Task 9

---
+++ src/main/java/students/deniss_jankovskis/lesson_2/level_1/task_1/Task_9.java
@@ -0,0 +1,21 @@
+package students.deniss_jankovskis.lesson_2.level_1.task_1;
+
+public class Task_9 {
+    public static void main(String[] args) {
+
+        int a = 9;
+
+        System.out.println(a + " x 1 = " + (a * 1));
+        System.out.println(a + " x 2 = " + (a * 2));
+        System.out.println(a + " x 3 = " + (a * 3));
+        System.out.println(a + " x 4 = " + (a * 4));
+        System.out.println(a + " x 5 = " + (a * 5));
+        System.out.println(a + " x 6 = " + (a * 6));
+        System.out.println(a + " x 7 = " + (a * 7));
+        System.out.println(a + " x 8 = " + (a * 3));
+        System.out.println(a + " x 9 = " + (a * 9));
+        System.out.println(a + " x 10 = " + (a * 10));
+
+
+    }
+}
