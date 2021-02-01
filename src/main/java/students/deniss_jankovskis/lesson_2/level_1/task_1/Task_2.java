a241a255654d173516c90f6032256ce6ee3beab0
DJ9780772 <denissjankovskis@outlook.com>
2021-02-01 16:09:43 +0200

    Task 2

---
+++ src/main/java/students/deniss_jankovskis/lesson_2/level_1/task_1/Task_2.java
@@ -0,0 +1,34 @@
+package students.deniss_jankovskis.lesson_2.level_1.task_1;
+
+class Task_2 {
+    public static void main(String[] args) {
+
+        float a = 3.15f;
+        float b = 6.45f;
+        float result = a + b;
+
+        System.out.println(result);
+
+
+        double firstNumber = 13.65;
+        double secondNumber = 7.83;
+        double finalNumber = firstNumber - secondNumber;
+
+        System.out.println(finalNumber);
+
+        float x = 134.43f;
+        float y = 256.82f;
+        float z = x * y;
+
+        System.out.println(x * y);
+
+        double c = 157.25;
+        double d = 25.57;
+        double finalResult = c / d;
+
+        System.out.println(finalResult);
+
+
+
+    }
+}
