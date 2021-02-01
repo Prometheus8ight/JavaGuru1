7122ffcf90e4e146fd79e3713189396c72249e08
DJ9780772 <denissjankovskis@outlook.com>
2021-02-01 19:31:21 +0200

    Task 15

---
+++ src/main/java/students/deniss_jankovskis/lesson_2/level_1/task_1/Task_18.java
@@ -0,0 +1,9 @@
+package students.deniss_jankovskis.lesson_2.level_1.task_1;
+
+class SizeMatters {
+    public static void main(String[] args) {
+
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
