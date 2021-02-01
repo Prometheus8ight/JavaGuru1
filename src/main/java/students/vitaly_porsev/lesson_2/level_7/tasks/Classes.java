b4a5dbbfec09904ede9eadf3d0fb3384f12dd444
Prometheus <pointofview88@gmail.com>
2021-02-01 15:49:52 +0300

    Lesson 2, level 7 tasks.

---
+++ src/main/java/students/vitaly_porsev/lesson_2/level_7/tasks/Classes.java
@@ -0,0 +1,28 @@
+package students.vitaly_porsev.lesson_2.level_7.tasks;
+
+class Classes {
+}
+
+class Book{
+
+}
+
+class BankAccount{
+
+}
+
+class SystemUnit{
+
+}
+
+class LoginService{
+
+}
+
+class BankTransaction{
+
+}
+
+class Task1{
+
+}
---
+++ src/main/java/students/vitaly_porsev/lesson_2/level_7/tasks/Program.java
@@ -0,0 +1,17 @@
+package students.vitaly_porsev.lesson_2.level_7.tasks;
+
+public class Program {
+    public static void main(String[] args) {
+        int a = 55;
+        int b = 45;
+
+        System.out.println(a * b); //выводим умножение
+
+        /*
+
+        Здесь могла быть ваша реклама!
+
+         */
+
+    }
+}
