f2c7b6a8ec4907f2b0b36af090635d45b60006a7
Prometheus <pointofview88@gmail.com>
2021-02-01 12:43:17 +0300

    Lesson 2, level 6 tasks.

---
+++ src/main/java/students/vitaly_porsev/lesson_2/level_6/tasks/AttentionPlease.java
@@ -0,0 +1,7 @@
+package students.vitaly_porsev.lesson_2.level_6.tasks;
+
+class AttentionPlease {
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
---
+++ src/main/java/students/vitaly_porsev/lesson_2/level_6/tasks/NoSleepFocus.java
@@ -0,0 +1,8 @@
+package students.vitaly_porsev.lesson_2.level_6.tasks;
+
+class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
