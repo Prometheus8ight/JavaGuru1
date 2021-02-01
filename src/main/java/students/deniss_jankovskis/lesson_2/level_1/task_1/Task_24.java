f64985afb2d3ecab8e2bd670c66da54fc879df48
DJ9780772 <denissjankovskis@outlook.com>
2021-02-01 19:34:24 +0200

    Task 24

---
+++ src/main/java/students/deniss_jankovskis/lesson_2/level_1/task_1/Task_24.java
@@ -0,0 +1,11 @@
+package students.deniss_jankovskis.lesson_2.level_1.task_1;
+
+class NoSleepNoFocus {
+
+    public static void main(String[] args) {
+
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
+
