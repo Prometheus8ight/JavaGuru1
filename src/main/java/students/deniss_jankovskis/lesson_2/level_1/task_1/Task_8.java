d35ed67430c2a0f9497bb0bdfd4119e2a6d8fab5
DJ9780772 <denissjankovskis@outlook.com>
2021-02-01 17:40:50 +0200

    Task 8

---
+++ src/main/java/students/deniss_jankovskis/lesson_2/level_1/task_1/Task_8.java
@@ -0,0 +1,12 @@
+package students.deniss_jankovskis.lesson_2.level_1.task_1;
+
+public class Task_8 {
+
+    public static void main(String[] args) {
+
+        String txt = "Hello";
+
+        System.out.println(txt);
+
+    }
+}
