ea63828047fa096e8fa8cf89a5c55cffca2d265d
DJ9780772 <denissjankovskis@outlook.com>
2021-02-01 19:33:44 +0200

    Task 23

---
+++ src/main/java/students/deniss_jankovskis/lesson_2/level_1/task_1/Task_23.java
@@ -0,0 +1,10 @@
+package students.deniss_jankovskis.lesson_2.level_1.task_1;
+
+class AttentionPlease {
+
+    public static void main(String[] args) {
+
+    System.out.println("Code outside the method will not compile!");
+
+    }
+}
