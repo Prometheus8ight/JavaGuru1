4bbbd414a50e65b9e33897388d2e006fe3e0c3ea
Prometheus <pointofview88@gmail.com>
2021-02-01 12:05:40 +0300

    Lesson 2, level5 tasks.

---
+++ src/main/java/students/vitaly_porsev/lesson_2/level_5/tasks/SizeMatters.java
@@ -0,0 +1,8 @@
+package students.vitaly_porsev.lesson_2.level_5.tasks;
+
+class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
