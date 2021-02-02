abcaced0c4c1a431d943197351a4cbb5e0c77757
DJ9780772 <denissjankovskis@outlook.com>
2021-02-02 11:18:33 +0200

    Task 31

--- src/main/java/students/deniss_jankovskis/lesson_2/level_1/task_1/Task_27.java
+++ src/main/java/students/deniss_jankovskis/lesson_2/level_1/task_1/Task_27.java
@@ -1,10 +1,3 @@
 package students.deniss_jankovskis.lesson_2.level_1.task_1;
 
-class Book {
-BankAccount
-SystemUnit
-LoginService
-BankTransaction
-Task_1
 
-}
---
+++ src/main/java/students/deniss_jankovskis/lesson_2/level_1/task_1/Task_31.java
@@ -0,0 +1,17 @@
+package students.deniss_jankovskis.lesson_2.level_1.task_1;
+
+class Comment {
+
+    public static void main(String[] args) {
+
+        String txt = "Напишите программу и";
+        String txt2 = "и продемонстрируйте в ней использование"; // You are reading this code
+        String txt3 = "однострочного и многострочного коментариев."; // I hope I did everything correct
+
+
+        System.out.println(txt + txt2);
+        System.out.println(txt3);
+
+    }
+
+}
