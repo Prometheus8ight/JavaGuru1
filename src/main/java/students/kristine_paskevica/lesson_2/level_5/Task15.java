016f326f571a5698e8551557ea03ad751fa59085
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-02-02 13:29:32 +0200

    2. nodarbības, 5 līmeņa - uzdevumi

---
+++ src/main/java/students/kristine_paskevica/lesson_2/level_5/Task15.java
@@ -0,0 +1,8 @@
+package students.kristine_paskevica.lesson_2.level_5;
+
+class Task15 {
+
+    public static void main(String[] args) {
+        System.out.print("Izvada tekstu, bet kursors nepāriet jaunā rindā!");
+    }
+}
