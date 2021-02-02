00c0caf9d01a7c29c78859ad7028ce74b109ad68
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-02-02 13:46:35 +0200

    2. nodarbības, 6 līmeņa - 23. un 24.uzd

---
+++ src/main/java/students/kristine_paskevica/lesson_2/level_6/Task24.java
@@ -0,0 +1,8 @@
+package students.kristine_paskevica.lesson_2.level_6;
+
+class Task24 {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
