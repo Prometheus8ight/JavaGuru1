caba94b69f70cb177f7b4260e63f13946a976986
DJ9780772 <denissjankovskis@outlook.com>
2021-02-02 10:38:08 +0200

    Task 28

---
+++ src/main/java/students/deniss_jankovskis/lesson_2/level_1/task_1/Task_28.java
@@ -0,0 +1,25 @@
+package students.deniss_jankovskis.lesson_2.level_1.task_1;
+
+class SumTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        int numberTwo = 20;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        int number_1 = 10;
+        int number_2 = 20;
+
+        int sum = number_1 + number_2;
+
+        System.out.println("Sum = " + sum);
+
+    }
+
+}
+
+
+
