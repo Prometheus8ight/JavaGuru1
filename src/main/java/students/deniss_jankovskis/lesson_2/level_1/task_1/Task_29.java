e41ba2f61cd9c7eb176d82cf43fdf46726d9a50e
DJ9780772 <denissjankovskis@outlook.com>
2021-02-02 10:40:15 +0200

    Task 29

---
+++ src/main/java/students/deniss_jankovskis/lesson_2/level_1/task_1/Task_29.java
@@ -0,0 +1,19 @@
+package students.deniss_jankovskis.lesson_2.level_1.task_1;
+
+class CorrectDataTypes {
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        double numberTwo = 20.0;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        double sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+
+    }
+
+}
