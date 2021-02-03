320163cb771e4e1b7a9e1df110e6573d1d258bd4
RodionA <raf91@inbox.lv>
2021-02-04 01:56:02 +0200

    Lesson-3-level-1-task-3

---
+++ src/main/java/students/rodion_afanasjev/lesson_3/level_1/task_3/RobotDemo.java
@@ -0,0 +1,22 @@
+package students.rodion_afanasjev.lesson_3.level_1.task_3;
+import students.rodion_afanasjev.lesson_3.level_1.task_1.Robot;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot robot1 = new Robot();
+
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot();
+
+        robot2.hello();
+        robot2.sayYourNameTwo();
+        robot2.age();
+        robot2.bank();
+
+    }
+
+}
