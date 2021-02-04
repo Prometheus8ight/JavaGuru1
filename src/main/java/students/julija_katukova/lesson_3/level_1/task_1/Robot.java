e63f120fa4318b138a0a1a045d60479ce178d157
Julija Katukova <julija.katukova@gmail.com>
2021-02-04 16:49:39 +0200

    Lesson3 Level1 Task1

---
+++ src/main/java/students/julija_katukova/lesson_3/level_1/task_1/Robot.java
@@ -0,0 +1,11 @@
+package students.julija_katukova.lesson_3.level_1.task_1;
+
+class Robot {
+    public void sayHello() {
+        System.out.println("Hello!");
+        // тут напишите команду распечатки на консоли строки "Hello!"
+    }
+    public void myAge() {
+        System.out.println("I am 25 years old.");
+    }
+}
