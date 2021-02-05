2ed21cf76f835e5303d67944c24d06bec503028b
Julija Katukova <julija.katukova@gmail.com>
2021-02-05 14:25:23 +0200

    Lesson3 Level5 Task18

---
+++ src/main/java/students/julija_katukova/lesson_3/level_5/task_18/Dog.java
@@ -0,0 +1,15 @@
+package students.julija_katukova.lesson_3.level_5.task_18;
+
+class Dog {
+    String name;
+
+    Dog(String dogName) {
+        this.name = dogName;
+    }
+
+    public void voice () {
+        System.out.println(this.name + " " + this.name + " " + this.name);
+    }
+
+
+}
