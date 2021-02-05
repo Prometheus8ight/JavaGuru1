0b0d445c5c88ee61777e634cc2b7305f58339060
Julija Katukova <julija.katukova@gmail.com>
2021-02-05 14:44:25 +0200

    Lesson3 Level5 Task20

---
+++ src/main/java/students/julija_katukova/lesson_3/level_5/task_20/DogDemo.java
@@ -0,0 +1,13 @@
+package students.julija_katukova.lesson_3.level_5.task_20;
+
+class DogDemo {
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog ("Aisedora", 3);
+        dog1.voice();
+        dog1.happyBirthday();
+        dog1.voice();
+    }
+
+
+}
