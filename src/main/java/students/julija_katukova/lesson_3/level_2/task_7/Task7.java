c4ece0c95583f7cfb24ebf2f1395e7db9d84caa4
Julija Katukova <julija.katukova@gmail.com>
2021-02-05 10:05:43 +0200

    Lesson3 Level2 Task7

---
+++ src/main/java/students/julija_katukova/lesson_3/level_2/task_7/Task7.java
@@ -0,0 +1,11 @@
+package students.julija_katukova.lesson_3.level_2.task_7;
+
+public class Task7 {
+    public static void main(String[] args) {
+        String myName = "Julija";
+        System.out.println(myName);
+
+        }
+
+
+}
