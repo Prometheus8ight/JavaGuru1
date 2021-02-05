fe40a54960434204e82e0fb0048f06c320b9e534
Julija Katukova <julija.katukova@gmail.com>
2021-02-05 09:54:03 +0200

    Lesson3 Level1

---
+++ src/main/java/students/julija_katukova/lesson_3/level_1/RobotDemo.java
@@ -0,0 +1,21 @@
+package students.julija_katukova.lesson_3.level_1;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot robot = new Robot();
+        robot.sayHello();
+        robot.sayYourName();
+
+        Robot miki = new Robot();
+        miki.sayHello();
+        miki.sayYourName();
+
+        Robot maus = new Robot();
+        maus.sayHello();
+        maus.sayYourName();
+
+    }
+
+
+}
