f6a38ad4832cd164eb69cb51ae146b9707fb1303
Kirils_Odnobokovs <kirils.odnobokovs@gmail.com>
2021-02-05 15:10:14 +0200

    kirils_odnobokovs_lesson_2_level_1_task_2

---
+++ src/main/java/students/kirils_odnobokovs/lesson_2/level_1/task_2/Task_2.java
@@ -0,0 +1,16 @@
+package students.kirils_odnobokovs.lesson_2.level_1.task_2;
+
+public class Task_2 {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        double firstDoubleNumber = scanner.nextDouble();
+        double secondDoubleNumber = scanner.nextDouble();
+
+        System.out.println(firstDoubleNumber + secondDoubleNumber);
+        System.out.println(firstDoubleNumber - secondDoubleNumber);
+        System.out.println(firstDoubleNumber / secondDoubleNumber);
+        System.out.println(firstDoubleNumber * secondDoubleNumber);
+    }
+}
