fc7c4a1d7718e6f3c290fd2d7ca1429da01cc0ce
RodionA <raf91@inbox.lv>
2021-02-05 16:35:49 +0200

    Lesson-3-level-7-task-29

---
+++ src/main/java/students/rodion_afanasjev/lesson_3/level_7/task_29/Circle.java
@@ -0,0 +1,8 @@
+package students.rodion_afanasjev.lesson_3.level_7.task_29;
+
+class Circle {
+
+    double calculateArea(double radius){
+        return Math.PI * radius * radius;
+    }
+}
---
+++ src/main/java/students/rodion_afanasjev/lesson_3/level_7/task_29/CircleDemo.java
@@ -0,0 +1,13 @@
+package students.rodion_afanasjev.lesson_3.level_7.task_29;
+
+class CircleDemo {
+
+    public static void main(String[] args) {
+
+        Circle math = new Circle();
+        double a = math.calculateArea(7.5);
+
+        System.out.println("Circle area = " + a);
+
+    }
+}
