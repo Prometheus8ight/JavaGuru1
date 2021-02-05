579d688d3b41711b25dbebfd28cda0b9ad2a8063
Prometheus <pointofview88@gmail.com>
2021-02-05 12:16:42 +0300

    Lesson 3, level 5, task 22.

---
+++ src/main/java/students/vitaly_porsev/lesson_3/level_5/task_22/DogDemo.java
@@ -0,0 +1,9 @@
+package students.vitaly_porsev.lesson_3.level_5.task_22;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog lucky = new Dog("Lucky", 5, "Black");
+        lucky.voice();
+        lucky.changeColor();
+    }
+}
