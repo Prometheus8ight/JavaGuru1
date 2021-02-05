24411ada2985eb5846e641e093e44f023a7499ab
Julija Katukova <julija.katukova@gmail.com>
2021-02-05 13:07:06 +0200

    Lesson3 Level4 Task14

---
+++ src/main/java/students/julija_katukova/lesson_3/level_4/task_14/Book.java
@@ -0,0 +1,23 @@
+package students.julija_katukova.lesson_3.level_4.task_14;
+
+class Book {
+    String title;
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
+
+class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+
+}
