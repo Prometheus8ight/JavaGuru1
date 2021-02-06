0a0685365a558c9071a4bbc9266d103e345df343
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-02-06 15:58:15 +0100

    Bogdans Pavlovs - Lesson 3 - Level 5 - Task 18

---
+++ src/main/java/students/bogdans_pavlovs/lesson_3/level_5/task_18/Dog.java
@@ -0,0 +1,13 @@
+package students.bogdans_pavlovs.lesson_3.level_5.task_18;
+
+class Dog {
+
+    String name;
+
+    void speak(){
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+    }
+
+}
