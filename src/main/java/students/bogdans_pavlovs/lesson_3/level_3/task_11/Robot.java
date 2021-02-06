bb9a82a0ec79fcf82362de87860d302299310e78
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-02-06 15:04:13 +0100

    Bogdans Pavlovs - Lesson 3 - Level 3 - Task 11

---
+++ src/main/java/students/bogdans_pavlovs/lesson_3/level_3/task_11/Robot.java
@@ -0,0 +1,23 @@
+package students.bogdans_pavlovs.lesson_3.level_3.task_11;
+
+class Robot {
+
+    String name;
+
+    public Robot() {
+
+    }
+
+    public void sayHello() {
+
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+
+        System.out.println("My name is ROBO!");
+    }
+
+
+}
+
