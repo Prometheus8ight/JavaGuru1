b7ce44fbc6b02c9bb9024fe71d4a57b43c8b64df
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-02-06 16:24:14 +0100

    Bogdans Pavlovs - Lesson 3 - Level 5 - Task 20

---
+++ src/main/java/students/bogdans_pavlovs/lesson_3/level_5/task_21/Dog.java
@@ -0,0 +1,16 @@
+package students.bogdans_pavlovs.lesson_3.level_5.task_21;
+
+class Dog {
+
+    String name;
+    int age;
+    String color;
+
+    void voice (){
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age + " Years");
+        System.out.println("Color: " + color);
+    }
+
+}
