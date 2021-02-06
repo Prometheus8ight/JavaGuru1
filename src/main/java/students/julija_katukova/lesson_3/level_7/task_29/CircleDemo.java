b33b7a93240e52c096f0c96cabd2f78d7fe060af
Julija Katukova <julija.katukova@gmail.com>
2021-02-06 10:51:51 +0200

    Lesson3 Level7 Task29

--- src/main/java/students/julija_katukova/lesson_3/level_7/task_29/CircleDemo.java
+++ src/main/java/students/julija_katukova/lesson_3/level_7/task_29/CircleDemo.java
@@ -1,6 +1,6 @@
 package students.julija_katukova.lesson_3.level_7.task_29;
 
-public class CircleDemo {
+class CircleDemo {
     public static void main(String[] args) {
         Circle circle1 = new Circle(5.3);
         circle1.calculateArea();
