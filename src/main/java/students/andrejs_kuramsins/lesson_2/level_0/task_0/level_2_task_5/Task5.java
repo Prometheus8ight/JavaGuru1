fea821b0faa1e11a73dc098cb1787377334b38a7
Andrejs24 <andrejs.kuramsins@gmail.com>
2021-02-06 16:45:26 +0200

    Andrejs Kuramšins lesson 2 task 5

---
+++ src/main/java/students/andrejs_kuramsins/lesson_2/level_0/task_0/level_2_task_5/Task5.java
@@ -0,0 +1,13 @@
+package students.andrejs_kuramsins.lesson_2.level_0.task_0.level_2_task_5;
+
+ class Task5 {
+
+ }
+    class BankAccount {
+
+        public static void main(String[] args) {
+
+
+        }
+
+} // закрыл класс скобкой
