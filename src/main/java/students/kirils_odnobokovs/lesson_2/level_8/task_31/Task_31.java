230aa3328ed5d419eeb89f9631b7bf2758436c87
Kirils_Odnobokovs <kirils.odnobokovs@gmail.com>
2021-02-06 16:40:37 +0200

    kirils_odnobokovs_lesson_2_level_8_task_31

---
+++ src/main/java/students/kirils_odnobokovs/lesson_2/level_8/task_31/Task_31.java
@@ -0,0 +1,15 @@
+package students.kirils_odnobokovs.lesson_2.level_8.task_31;
+
+public class Task_31 {
+
+    public static void main(String[] args) {
+
+        // KOmentarij
+        /* komentarij Vol.2
+        bala
+        bla
+         */
+
+        System.out.println("Hello, World!");
+    }
+}
