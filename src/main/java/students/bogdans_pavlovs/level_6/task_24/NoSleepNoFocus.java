6693de21847213cccc4f5b25cbe7b828549e6e61
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-02-06 11:36:34 +0100

    Bogdans Pavlovs - Moving Lesson 2 homework to a "lesson_2" package.

--- src/main/java/students/bogdans_pavlovs/level_6/task_24/NoSleepNoFocus.java
+++
@@ -1,9 +0,0 @@
-package students.bogdans_pavlovs.level_6.task_24;
-
-class NoSleepNoFocus {
-    public static void main(String[] args) {
-
-        System.out.println("I wanna coffee");
-        System.out.println("So bad");
-    }
-}
