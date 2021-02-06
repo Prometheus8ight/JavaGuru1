265ebfd3df3ae99d924147ba9c024a624a8a9f5f
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-02-06 15:30:43 +0100

    Bogdans Pavlovs - Lesson 3 - Level 4 - Task 14

---
+++ src/main/java/students/bogdans_pavlovs/lesson_3/level_4/task_14/Book.java
@@ -0,0 +1,22 @@
+package students.bogdans_pavlovs.lesson_3.level_4.task_14;
+
+class Book {
+
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
+
+
+/*
+Найти ошибку в коде.
+Исправить и запустить программу.
+Менять можно только класс Book.
+ */
