9a3e861eb83766b10b28a9fec47a478d05890ded
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-02-06 12:55:14 +0100

    Bogdans Pavlovs - Lesson 3 - Level 1 - Task 4

---
+++ src/main/java/students/bogdans_pavlovs/lesson_3/level_1/task_6/RobotDemo.java
@@ -0,0 +1,23 @@
+package students.bogdans_pavlovs.lesson_3.level_1.task_6;
+
+class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot robot1 = new Robot();
+        Robot robot2 = new Robot();
+        Robot robot3 = new Robot();
+
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        robot2.sayHello();
+        robot2.sayYourName();
+
+        robot3.sayHello();
+        robot3.sayYourName();
+
+    }
+
+}
+
