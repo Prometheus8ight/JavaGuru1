86e64f5145d2c0299f8f00dff8e5943883f46a02
Andrejs24 <andrejs.kuramsins@gmail.com>
2021-02-06 17:25:41 +0200

    Andrejs Kuramšins lesson 2 task 6

---
+++ src/main/java/students/andrejs_kuramsins/lesson_2/level_0/task_0/level_2/task_7/Task7.java
@@ -0,0 +1,8 @@
+package students.andrejs_kuramsins.lesson_2.level_0.task_0.level_2.task_7;
+
+class Task7 {
+    public static void main(String[] args) { // добавил static между public и void
+        System.out.println("You can login");
+    }
+
+}
