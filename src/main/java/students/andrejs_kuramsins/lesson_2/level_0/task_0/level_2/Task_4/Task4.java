41d3e35ceb02cb1670df2bb81a929bbb21852b22
Andrejs24 <andrejs.kuramsins@gmail.com>
2021-02-06 16:38:48 +0200

    Andrejs Kuramšins lesson 2 task 4

--- src/main/java/students/andrejs_kuramsins/lesson_2/level_0/task_0/level_2/Task_4/Task4.java
+++ src/main/java/students/andrejs_kuramsins/lesson_2/level_0/task_0/level_2/Task_4/Task4.java
@@ -1,10 +1,8 @@
 package students.andrejs_kuramsins.lesson_2.level_0.task_0.level_2.Task_4;
 
- class Task4 {
+public class Task4 {
+    class Phone { // просто скобку открыл
 
-     class Phone{ //поставил скобку в начале
-
- }
 }
 
-
+}
