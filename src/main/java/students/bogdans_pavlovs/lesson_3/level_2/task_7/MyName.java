a9fc07684755cad44fcaf7d55442f993dbbf08b7
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-02-06 13:00:06 +0100

    Bogdans Pavlovs - Lesson 3 - Level 2 - Task 7

---
+++ src/main/java/students/bogdans_pavlovs/lesson_3/level_2/task_7/MyName.java
@@ -0,0 +1,11 @@
+package students.bogdans_pavlovs.lesson_3.level_2.task_7;
+
+class MyName {
+
+    public static void main(String[] args) {
+
+        String myName = "Bogdans";
+        System.out.println(myName);
+
+    }
+}
