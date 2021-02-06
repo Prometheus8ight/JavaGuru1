94fb02d7f85329c699b017f6b78059085dd6280a
Andrejs24 <andrejs.kuramsins@gmail.com>
2021-02-06 18:50:10 +0200

    Andrejs Kuramšins lesson 2 task 11

--- src/main/java/students/andrejs_kuramsins/lesson_2/level_0/task_0/level_3/task_11/Task11.java
+++ src/main/java/students/andrejs_kuramsins/lesson_2/level_0/task_0/level_3/task_11/Task11.java
@@ -1,22 +1,22 @@
 package students.andrejs_kuramsins.lesson_2.level_0.task_0.level_3.task_11;
 
- class Task11 {
+class Task11 {
     public static void main(String[] args) {
 
-        System.out.println("Enter your number a : ");
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        double a = scanner.nextDouble();
+            System.out.println("Enter your number a : ");
+            java.util.Scanner scanner = new java.util.Scanner(System.in);
+            double a = scanner.nextDouble();
 
-        System.out.println("Enter your number b : ");
-        double b = scanner.nextDouble();
+            System.out.println("Enter your number b : ");
+            double b = scanner.nextDouble();
 
-        System.out.println("Enter your number c : ");
-        double c = scanner.nextInt();
+            System.out.println("Enter your number c : ");
+            double c = scanner.nextInt();
 
-        double arithmetic = (a + b + c) / 3;
+            double arithmetic = (a + b + c) / 3;
 
-        System.out.println("The medium arithmetic of your numbers " + "= " + arithmetic );
+            System.out.println("The medium arithmetic of your numbers " + "= " + arithmetic );
 
 
+        }
     }
-}
