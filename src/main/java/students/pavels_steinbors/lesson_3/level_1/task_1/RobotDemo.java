99b52c3a72834bfc8c2f88ff7c7eea9b4bc55d4f
PavStein <pav210985@gmail.com>
2021-02-06 15:56:03 +0100

    Lesson 3 Level 1 task 1

---
+++ src/main/java/students/pavels_steinbors/lesson_3/level_1/task_1/RobotDemo.java
@@ -0,0 +1,10 @@
+package students.pavels_steinbors.lesson_3.level_1.task_1;
+
+class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot robot1 = new Robot();
+        robot1.sayHello();
+    }
+}
