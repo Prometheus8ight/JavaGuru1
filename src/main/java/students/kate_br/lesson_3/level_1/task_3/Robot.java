4a501b1a19ef52ba2fb8b8f4cfba96a87792a765
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-07 18:42:23 +0200

    Lesson 3. Task 3

---
+++ src/main/java/students/kate_br/lesson_3/level_1/task_3/Robot.java
@@ -0,0 +1,9 @@
+package students.kate_br.lesson_3.level_1.task_3;
+
+
+ class Robot {
+
+     Robot () {
+
+     }
+}
---
+++ src/main/java/students/kate_br/lesson_3/level_1/task_3/RobotDemo.java
@@ -0,0 +1,10 @@
+package students.kate_br.lesson_3.level_1.task_3;
+
+ class RobotDemo {
+
+  public static void main(String[] args) {
+
+   Robot robot = new Robot();
+
+  }
+}
