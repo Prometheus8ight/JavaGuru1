672a51737758b36780995ee00e04e1176593a219
NikitaBunevich <nikitei2001@gmail.com>
2021-02-08 00:44:05 +0200

    st-nikita-bunevich-lesson2-level2-task7

---
+++ src/main/java/students/nikita_bunevich/lesson_2/level_2/task_7/LoginService.java
@@ -0,0 +1,8 @@
+package students.nikita_bunevich.lesson_2.level_2.task_7;
+
+class LoginService {
+    public static void main(String[] args) {
+
+        System.out.println("You can login!");
+    }
+}
