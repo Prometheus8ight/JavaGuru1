d8e8c88d7d93b601a78741de7dac32ea6f1c46d8
DmitryGrishin1805 <dg9261115311@gmail.com>
2021-02-07 22:36:14 +0200

    new task 18

--- src/main/java/students/Dmitry_Gr/lesson_2/level_0/task_0/World.java
+++
@@ -1,4 +0,0 @@
-package students.Dmitry_Gr.lesson_2.level_0.task_0;
-
-class World {
-}
---
+++ src/main/java/students/Dmitry_Gr/lesson_3/level_5/task_18/DogDemo.java
@@ -0,0 +1,11 @@
+package students.Dmitry_Gr.lesson_3.level_5.task_18;
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+
+        Dog korsar = new Dog("Korsar");
+        korsar.voice();
+    }
+
+}
