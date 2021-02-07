f6cca08136e78ec71c56fc2f44a735290c8c5417
CoArchie <c.chikun@icloud.com>
2021-02-07 17:40:42 +0200

    Lesson 3 - Task 7

---
+++ src/main/java/students/arkadij/lesson_3/level_2/Name.java
@@ -0,0 +1,11 @@
+package students.arkadij.lesson_3.level_2;
+
+public class Name {
+
+    public static void main(String[] args) {
+
+        String myName = "Archie";
+        System.out.println(myName);
+
+    }
+}
