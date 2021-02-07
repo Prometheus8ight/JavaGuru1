7a89a7e1112224df390a6ff41a6eb9bd94d4dcf7
CoArchie <c.chikun@icloud.com>
2021-02-07 19:26:38 +0200

    Lesson 3 - Task 19

--- src/main/java/students/arkadij/lesson_3/level_5/DogDemo.java
+++ src/main/java/students/arkadij/lesson_3/level_5/DogDemo.java
@@ -2,7 +2,19 @@ package students.arkadij.lesson_3.level_5;
 
 public class DogDemo {
     public static void main(String[] args) {
-        Dog doggy = new Dog("Sifon");
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("How old is your dog?");
+        String dogAge = scanner.nextLine();
+
+        System.out.println("Does your dog have a name?");
+        String name = scanner.nextLine();
+
+        Dog doggy = new Dog(name);
+
+        System.out.println("You have a wonderful dog named " + name + " and your dog is only " + dogAge + " years old.");
+        System.out.println("The dog makes a voice:");
         doggy.voice();
+
     }
 }
