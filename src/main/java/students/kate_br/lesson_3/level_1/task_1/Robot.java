93a4f6dc3cf53510e19dd610e78891e95509b8d5
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-07 18:07:01 +0200

    Lesson 3. Task 1

--- src/main/java/students/kate_br/lesson_3/level_1/task_1/Robot.java
+++ src/main/java/students/kate_br/lesson_3/level_1/task_1/Robot.java
@@ -2,7 +2,7 @@ package students.kate_br.lesson_3.level_1.task_1;
 
  class Robot {
 
-     public void sayHello(String text){
+     public void sayHello(String){
          System.out.println("Hello!");
      }
 
