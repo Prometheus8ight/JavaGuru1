d85feebb8a79c9d02cbfd3fb49d5aa6bc1bef422
alex <kalashnikov_alex@yahoo.com>
2021-02-07 22:59:06 +0200

    Homework, lesson 3, level 2, 7,8,9 tasks.

---
+++ src/main/java/students/alex_kalashnikov/lesson_3/level_2/task_7/Name.java
@@ -0,0 +1,12 @@
+package students.alex_kalashnikov.lesson_3.level_2.task_7;
+
+class Name {
+
+    public static void main(String[] args) {
+
+        String myName = "Alex";
+        System.out.println(myName);
+
+    }
+
+}
