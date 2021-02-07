b8845fdc0b5d2edbb0574cd01eb245e668762ad9
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-07 20:31:45 +0200

    Lesson 3. Task 12

---
+++ src/main/java/students/kate_br/lesson_3/level_3/task_12/Robot.java
@@ -0,0 +1,15 @@
+package students.kate_br.lesson_3.level_3.task_12;
+
+ class Robot {
+
+     String name;
+     String sayHello;
+     String sayYourName;
+
+
+     Robot(String robotName){
+         this.name = robotName;
+
+     }
+
+}
