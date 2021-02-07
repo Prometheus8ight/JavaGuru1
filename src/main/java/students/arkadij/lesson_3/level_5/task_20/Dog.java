92c85d3ee67b15f3f462a37d177789b003105810
CoArchie <c.chikun@icloud.com>
2021-02-07 19:38:22 +0200

    Lesson 3 - Task 20

---
+++ src/main/java/students/arkadij/lesson_3/level_5/task_20/Dog.java
@@ -0,0 +1,25 @@
+package students.arkadij.lesson_3.level_5.task_20;
+
+public class Dog {
+
+    String name;
+    int age;
+
+    public Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public String getName(){
+        return name;
+    }
+    public void voice(){
+        System.out.println("I am " + age + " years old!");
+        System.out.println("Oh and my name is " + name + "!");
+    }
+    void happyBirthday(){
+        System.out.println("Happy Birthday " + name + "!");
+        age++;
+        voice();
+    }
+
+}
---
+++ src/main/java/students/arkadij/lesson_3/level_5/task_20/DogDemo.java
@@ -0,0 +1,11 @@
+package students.arkadij.lesson_3.level_5.task_20;
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+        Dog doggy = new Dog("Babom", 3);
+        doggy.voice();
+        doggy.happyBirthday();
+    }
+
+}
