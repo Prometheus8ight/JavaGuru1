bb0c2ea488fa01d499863904972d8492667440b4
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-07 20:39:56 +0200

    Lesson 3. Task 13

---
+++ src/main/java/students/kate_br/lesson_3/level_3/task_13/Robot.java
@@ -0,0 +1,14 @@
+package students.kate_br.lesson_3.level_3.task_13;
+
+ class Robot {
+
+     String name;
+
+     Robot(String robotName){
+         this.name = robotName;
+     }
+
+     void sayYourName() {
+         System.out.println("My name is " + this.name);
+     }
+}
