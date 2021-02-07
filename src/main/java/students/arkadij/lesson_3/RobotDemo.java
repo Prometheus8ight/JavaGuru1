00c88ce3571fff52b5a33c32532ecb48b72148da
CoArchie <c.chikun@icloud.com>
2021-02-07 17:37:06 +0200

    Lesson 3 - Task 6

--- src/main/java/students/arkadij/lesson_3/RobotDemo.java
+++ src/main/java/students/arkadij/lesson_3/RobotDemo.java
@@ -3,9 +3,20 @@ package students.arkadij.lesson_3;
 class RobotDemo {
 
     public static void main(String[] args) {
+
         Robot_2 android = new Robot_2();
+        Robot_2 ios = new Robot_2();
+        Robot_2 harmony = new Robot_2();
+
         android.sayHello();
         android.sayYourName();
+
+        ios.sayHello();
+        ios.sayYourName();
+
+        harmony.sayHello();
+        harmony.sayYourName();
+
     }
 
 }
