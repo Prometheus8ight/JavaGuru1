1e360b9eac0c80ed31941b22c83608f200f655de
CoArchie <c.chikun@icloud.com>
2021-02-07 17:17:56 +0200

    Lesson 3 - Task 2

---
+++ src/main/java/students/arkadij/lesson_3/Robot_2.java
@@ -0,0 +1,13 @@
+package students.arkadij.lesson_3;
+
+class Robot_2 {
+
+    public Robot_2() {
+
+    }
+
+    public void sayHello(){
+        System.out.println("Hey there!");
+    }
+
+}
--- src/main/java/students/arkadij/lesson_3/Task_1_Robot.java
+++
@@ -1,9 +0,0 @@
-package students.arkadij.lesson_3;
-
-public class Task_1_Robot {
-
-    public void sayHello(){
-        System.out.println("Hello there!");
-    }
-
-}
