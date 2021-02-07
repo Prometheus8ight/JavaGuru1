6d16f61b4ea595a03262ed425e55b9fb251a5567
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-02-07 17:52:36 +0200

    lesson3-level3-allTask

---
+++ src/main/java/students/kristine_paskevica_lesson_3/level_3/RobotDemo.java
@@ -0,0 +1,18 @@
+package students.kristine_paskevica_lesson_3.level_3;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+        Robot robots1 = new Robot("Rider");
+        robots1.sayHello();
+        robots1.sayYourName();
+
+
+        Robot robots2 = new Robot("John");
+        robots2.sayHello();
+        robots2.sayYourName();
+
+        Robot robots3 = new Robot("Karll");
+        robots3.sayHello();
+        robots3.sayYourName();
+    }}
