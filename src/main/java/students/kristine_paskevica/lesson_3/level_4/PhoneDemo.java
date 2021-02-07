dd74b3d347985f3c273fd0b7c391e426ee0a48b6
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-02-07 18:27:16 +0200

    lesson3-level4-allTask

---
+++ src/main/java/students/kristine_paskevica/lesson_3/level_4/PhoneDemo.java
@@ -0,0 +1,8 @@
+package students.kristine_paskevica.lesson_3.level_4;
+
+public class PhoneDemo {
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }}
