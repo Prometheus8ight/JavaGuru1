150f36da9fa9afd7cb273a600efd3d13b0eb7216
CoArchie <c.chikun@icloud.com>
2021-02-07 18:27:44 +0200

    Lesson 3 - Task 14

---
+++ src/main/java/students/arkadij/lesson_3/level_4/Book.java
@@ -0,0 +1,14 @@
+package students.arkadij.lesson_3.level_4;
+
+public class Book {
+
+    String title;
+
+    Book(String bookTitle){
+        this.title = bookTitle;
+    }
+    String getTitle(){
+        return this.title;
+    }
+
+}
