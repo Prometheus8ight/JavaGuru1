3a5417b1d4c87a76690eaaf3042f1890894958bd
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-07 19:56:27 +0200

    Lesson 3. Task 6

---
+++ src/main/java/students/kate_br/lesson_3/_level_1/task_6/Robot.java
@@ -0,0 +1,9 @@
+package students.kate_br.lesson_3._level_1.task_6;
+
+ class Robot {
+
+
+    String sayHello;
+    String sayYourName;
+
+}
---
+++ src/main/java/students/kate_br/lesson_3/_level_1/task_6/RobotDemo.java
@@ -0,0 +1,34 @@
+package students.kate_br.lesson_3._level_1.task_6;
+
+ class RobotDemo {
+
+     public static void main(String[] args) {
+
+         Robot robot1 = new Robot();
+         robot1.sayHello = "Hello";
+         robot1.sayYourName = "My name is ROBO";
+
+         System.out.println(robot1.sayHello);
+         System.out.println(robot1.sayYourName);
+
+         Robot robot2 = new Robot();
+         robot2.sayHello = "Hello!";
+         robot2.sayYourName = "My name is ROBO2";
+
+         System.out.println(robot2.sayHello);
+         System.out.println(robot2.sayYourName);
+
+         Robot robot3 = new Robot();
+         robot3.sayHello = "Hello :)";
+         robot3.sayYourName = "My name is ROBO3";
+
+         System.out.println(robot3.sayHello);
+         System.out.println(robot3.sayYourName);
+
+
+
+     }
+
+
+
+}
