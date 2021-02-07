9a46c7035c6761c607d37bfce9cd70ffbb6f4134
Julija Katukova <julija.katukova@gmail.com>
2021-02-07 15:19:12 +0200

    Lesson3 Level7 Task30

--- src/main/java/students/julija_katukova/lesson_3/level_7/task_30/Product.java
+++ src/main/java/students/julija_katukova/lesson_3/level_7/task_30/Product.java
@@ -4,21 +4,20 @@ class Product {
     String name;
     double regularPrice;
     double discount; //Скидка в процентах
-    double newPrice;
 
     Product(String productName){
         this.name = productName;
         }
 
     double actualPrice () {
-        this.newPrice = this.regularPrice - (this.regularPrice * (this.discount / 100.00)); //нужно поделить на 100, потомуу то скидка указывается в процентах
-        return newPrice;
+        return regularPrice - (regularPrice * (discount / 100.00)); //нужно поделить на 100, потомуу то скидка указывается в процентах
+
     }
     void printInformation () {
         System.out.println("Продукт: " + this.name);
         System.out.println("Цена без скидки: " + this.regularPrice);
         System.out.println("Скидка (%): " + this.discount);
-        System.out.println("Цена со скидкой: " + this.newPrice);
+        System.out.println("Цена со скидкой: " + actualPrice());
         }
     }
 
