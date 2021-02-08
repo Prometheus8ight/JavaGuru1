8a73106cf42f3f7fa25a96d3d6dd7be6e001e5dd
CoArchie <c.chikun@icloud.com>
2021-02-08 22:30:17 +0200

    Lesson 3 - Task 22

---
+++ src/main/java/students/arkadij/lesson_3/level_5/task_22/Dog.java
@@ -0,0 +1,29 @@
+package students.arkadij.lesson_3.level_5.task_22;
+
+public class Dog {
+
+    String name;
+    String color;
+    String newColor;
+    int age;
+
+    public Dog(String name, String color, int age){
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void voice(){
+        System.out.println("I am " + name + ", " + age + " years old, and the color of my wool is " + color + ".");
+    }
+
+    void changeColor(String newColor){
+        System.out.println("Now i've get a new color of my wool and it is colored in " + newColor + "!");
+        System.out.println("So on i am " + name + ", " + age + " years old and from now i am colored to " + newColor + "!");
+    }
+
+}
---
+++ src/main/java/students/arkadij/lesson_3/level_5/task_22/DogDemo.java
@@ -0,0 +1,11 @@
+package students.arkadij.lesson_3.level_5.task_22;
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+        Dog cDog = new Dog("Taison", "Orange", 4);
+        cDog.voice();
+        cDog.changeColor("Black");
+    }
+
+}
