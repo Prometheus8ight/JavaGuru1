69ca8801a532d63c55806474bbe47785667d653d
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-02-08 14:29:10 +0200

    lesson3-level5-18,19,20,21Task

---
+++ src/main/java/students/kristine_paskevica_lesson_3/level_5/DogTask19.java
@@ -0,0 +1,13 @@
+package students.kristine_paskevica_lesson_3.level_5;
+
+class DogTask19 {
+
+    String name;
+    byte age;
+    void ageDog(){
+        System.out.println("Suņa " + name + " vecums ir " + age + " gadi!");
+    }
+    void bowwow(){
+
+        System.out.println("Suni sauc " + name);
+    }}
---
+++ src/main/java/students/kristine_paskevica_lesson_3/level_5/DogTask21.java
@@ -0,0 +1,18 @@
+package students.kristine_paskevica_lesson_3.level_5;
+
+class DogTask21 {
+
+    String name;
+    byte age;
+    String color;
+
+    void bowwow(){
+    }
+    void happyBirthday(){
+    }
+    void colorDog(){
+        System.out.println("Suņa vārds ir " + name + ", vecums " + age + " gadi. Apspalvojuma krāsa " + color);
+    }
+
+
+}
