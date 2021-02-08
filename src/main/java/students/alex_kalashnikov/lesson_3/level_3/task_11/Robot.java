cf5971282fdc9a2c02fcca510b5bf518ecd147e6
alex <kalashnikov_alex@yahoo.com>
2021-02-08 20:11:51 +0200

    Homework; lesson 3; level 3; 11,12,13 tasks.

---
+++ src/main/java/students/alex_kalashnikov/lesson_3/level_3/task_11/Robot.java
@@ -0,0 +1,23 @@
+package students.alex_kalashnikov.lesson_3.level_3.task_11;
+
+class Robot {
+
+    String name;
+
+    Robot() {
+
+    }
+
+    public void sayHello() {
+
+        System.out.print("My name is ");
+
+    }
+
+    public void sayYourName() {
+
+        System.out.println(name + "!");
+
+    }
+
+}
---
+++ src/main/java/students/alex_kalashnikov/lesson_3/level_3/task_13/RobotDemo.java
@@ -0,0 +1,16 @@
+package students.alex_kalashnikov.lesson_3.level_3.task_13;
+
+class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot a = new Robot("Rider");
+        a.sayYourName();
+
+        Robot b = new Robot("John");
+        b.sayYourName();
+
+    }
+
+
+}
