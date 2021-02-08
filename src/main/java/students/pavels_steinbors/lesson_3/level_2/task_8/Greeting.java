355bd2eacc4d805768a3a306a40ab9d8b2d912b1
PavStein <pav210985@gmail.com>
2021-02-08 16:08:41 +0100

    Lesson 3 Level 2 task 8

---
+++ src/main/java/students/pavels_steinbors/lesson_3/level_2/task_8/Greeting.java
@@ -0,0 +1,12 @@
+package students.pavels_steinbors.lesson_3.level_2.task_8;
+
+class Greeting {
+
+    public static void main(String[] args) {
+
+        String userName = "Pavels";
+        String greeting = "Hi " + userName + "!";
+
+        System.out.println(greeting);
+    }
+}
