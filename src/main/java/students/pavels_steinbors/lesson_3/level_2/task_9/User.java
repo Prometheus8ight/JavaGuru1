ad7fd4a34f106d67b5793bba455febda83164531
PavStein <pav210985@gmail.com>
2021-02-08 16:39:19 +0100

    Lesson 3 Level 2 task 9

--- src/main/java/students/pavels_steinbors/lesson_3/level_2/task_9/User.java
+++ src/main/java/students/pavels_steinbors/lesson_3/level_2/task_9/User.java
@@ -3,8 +3,10 @@ package students.pavels_steinbors.lesson_3.level_2.task_9;
 class User {
 
     public static void main(String[] args) {
+
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         System.out.print("Write your name: ");
+
         String userName = scanner.next();
         String hello = "Hello " + userName + "!";
 
