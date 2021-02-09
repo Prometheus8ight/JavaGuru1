21c182041dcf51b7932cb012139bd0c203617365
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-09 23:24:51 +0200

    Lesson 3. Task 31

---
+++ src/main/java/students/kate_br/lesson_3/level_7/task_31/BankAccount.java
@@ -0,0 +1,23 @@
+package students.kate_br.lesson_3.level_7.task_31;
+
+ class BankAccount {
+
+     String owner;
+     String money;
+
+     BankAccount(String owner, int money){
+         this.owner = owner;
+
+
+     }
+
+     String getOwner(){
+         return this.owner;
+     }
+
+     String getMoney(){
+         return this.money;
+     }
+
+
+}
