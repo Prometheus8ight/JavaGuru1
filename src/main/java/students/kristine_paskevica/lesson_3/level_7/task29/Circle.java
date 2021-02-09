b47f7ef8c6de6c129976592894dfda8e43614ca7
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-02-09 14:04:33 +0200

    lesson3-level7-29Task

---
+++ src/main/java/students/kristine_paskevica/lesson_3/level_7/task29/Circle.java
@@ -0,0 +1,12 @@
+package students.kristine_paskevica.lesson_3.level_7.task29;
+
+ class Circle {
+
+  double radiuss;
+
+  Circle(double radiuss){
+   this.radiuss = radiuss;
+  }
+  double calculateArea(){
+   return Math.PI * this.radiuss * this.radiuss;
+  }}
