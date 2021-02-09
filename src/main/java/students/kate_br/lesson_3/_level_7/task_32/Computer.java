bcf0830f4070464d26f883b7f201a722e707a70f
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-09 17:19:51 +0200

    Lesson 3. Task 32

---
+++ src/main/java/students/kate_br/lesson_3/_level_7/task_32/Computer.java
@@ -0,0 +1,21 @@
+package students.kate_br.lesson_3._level_7.task_32;
+
+ class Computer {
+
+     String manufacturer;
+     String model;
+
+     Computer(String manufacturer, String model){
+
+         this.manufacturer = manufacturer;
+         this.model = model;
+     }
+
+     String getManufacturer(){
+         return this.manufacturer;
+     }
+
+     String getModel(){
+         return this.model;
+     }
+}
