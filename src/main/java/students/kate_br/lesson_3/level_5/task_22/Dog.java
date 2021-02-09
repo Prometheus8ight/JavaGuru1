567bd69e349c8653208aa455500340ce54fd2361
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-09 15:15:01 +0200

    Lesson 3. Task 22

---
+++ src/main/java/students/kate_br/lesson_3/level_5/task_22/Dog.java
@@ -0,0 +1,30 @@
+package students.kate_br.lesson_3.level_5.task_22;
+
+ class Dog {
+
+     String name;
+     String color;
+
+
+
+     Dog (String dogName, String dogColor){
+         this.name = dogName;
+         this.color = dogColor;
+
+
+
+     }
+
+     void voice(){
+
+         System.out.println("This is a dog");
+         System.out.println("The dog name is " + this.name + " and she is a " + this.color);
+     }
+
+     void changeColor(String newColor){
+
+         System.out.println("When " + this.name + " was a puppy she was a " + newColor + " color dog");
+     }
+
+
+}
---
+++ src/main/java/students/kate_br/lesson_3/level_5/task_22/DogDemo.java
@@ -0,0 +1,12 @@
+package students.kate_br.lesson_3.level_5.task_22;
+
+ class DogDemo {
+
+     public static void main(String[] args) {
+         Dog dog = new Dog("Penny", "black and white");
+         dog.voice();
+         dog.changeColor("black");
+
+
+     }
+}
