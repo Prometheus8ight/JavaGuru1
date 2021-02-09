ed8630c9d5068028dedad61a567beadeb3ddeb2b
CoArchie <c.chikun@icloud.com>
2021-02-09 22:33:43 +0200

    Lesson 3 - task 32

---
+++ src/main/java/students/arkadij/lesson_3/level_7/task_32/Computer.java
@@ -0,0 +1,21 @@
+package students.arkadij.lesson_3.level_7.task_32;
+
+class Computer {
+
+    String manufacturer;
+    String model;
+
+    Computer(String manufacturer, String model) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+
+    String getManufacturer(){
+        return manufacturer;
+    }
+
+    String getModel(){
+        return model;
+    }
+
+}
