54781881e4d46c0cb9059c883a70451656847502
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-02-09 13:44:38 +0200

    lesson3-level7-28Task

---
+++ src/main/java/students/kristine_paskevica/lesson_3/level_7/DemoMasina.java
@@ -0,0 +1,13 @@
+package students.kristine_paskevica.lesson_3.level_7;
+
+ class DemoMasina {
+     public static void main(String[] args) {
+
+        Masina manaMasina = new Masina();
+        manaMasina.masinasMarka = "Volvo";
+        manaMasina.masinasNumurs = "EM-1700";
+        manaMasina.ipasnieks = "Kristīne Sauliete";
+
+        manaMasina.masinasRakst();
+        manaMasina.masinasPard();
+     }}
