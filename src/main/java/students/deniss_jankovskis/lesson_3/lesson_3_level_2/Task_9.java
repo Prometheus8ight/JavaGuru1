88ccb0fcfc76f97b89a1314ec74f4e2bbd8812bb
DJ9780772 <denissjankovskis@outlook.com>
2021-02-09 15:37:57 +0200

    Level 2

---
+++ src/main/java/students/deniss_jankovskis/lesson_3/lesson_3_level_2/Task_9.java
@@ -0,0 +1,14 @@
+package students.deniss_jankovskis.lesson_3.lesson_3_level_2;
+
+class Task_9 {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Please enter your name: ");
+
+        String userName = scanner.nextLine();
+
+        System.out.println("Hello " + userName);
+    }
+}
