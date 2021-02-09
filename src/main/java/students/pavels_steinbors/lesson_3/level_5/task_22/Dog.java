0aa9f18d2b81a5451facdfd718127bf48c60ee5f
PavStein <pav210985@gmail.com>
2021-02-09 17:24:25 +0100

    Lesson 3 Level 5 task 22

---
+++ src/main/java/students/pavels_steinbors/lesson_3/level_5/task_22/Dog.java
@@ -0,0 +1,32 @@
+package students.pavels_steinbors.lesson_3.level_5.task_22;
+
+class Dog {
+
+    String name;
+    String color;
+    String voice;
+
+
+    Dog(String dogsName, String dogsColor, String dogsVoice) {
+        this.name = dogsName;
+        this.color = dogsColor;
+        this.voice = dogsVoice;
+    }
+
+    public void sayYourName() {
+        System.out.println("Dog's name is: " + this.name);
+    }
+
+    public void voice() {
+        System.out.println("Voice: " + this.voice);
+    }
+
+    public void myDogColor() {
+        System.out.println("My dog's color is " + this.color);
+    }
+
+    void changeColor(String newColor) {
+        System.out.println("My dog's new color is " + newColor);
+
+    }
+}
---
+++ src/main/java/students/pavels_steinbors/lesson_3/level_5/task_22/DogDemo.java
@@ -0,0 +1,15 @@
+package students.pavels_steinbors.lesson_3.level_5.task_22;
+
+class DogDemo {
+
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog("Roy", "Black", "Roy, Roy, Roy");
+        dog1.voice();
+        dog1.sayYourName();
+        dog1.myDogColor();
+        dog1.voice();
+        dog1.changeColor("White");
+
+    }
+}
