14d4542699adbd4434fd85cfc1d5fc3cadfa7088
PavStein <pav210985@gmail.com>
2021-02-09 12:13:35 +0100

    Lesson 3 Level 4 task 16

---
+++ src/main/java/students/pavels_steinbors/lesson_3/level_4/task_16/Car.java
@@ -0,0 +1,16 @@
+package students.pavels_steinbors.lesson_3.level_4.task_16;
+
+class Car {
+
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+
+}
