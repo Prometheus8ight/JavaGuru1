e24f0699bb82b5bf06ba654f74ad023c3eeaf9e8
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-09 22:36:21 +0200

    Lesson 3. Task 19

--- src/main/java/students/kate_br/lesson_3/level_5/task_19/Dog.java
+++ src/main/java/students/kate_br/lesson_3/level_5/task_19/Dog.java
@@ -2,15 +2,17 @@ package students.kate_br.lesson_3.level_5.task_19;
 
  class Dog {
 
-     int age = 4;
+     String name;
+     int age;
 
-     Dog(int dogAge){
-         this.age = dogAge;
+     Dog(String name, int age){
+         this.age = age;
+         this.name = name;
 
      }
 
-     void voice(String name){
-         System.out.println("Dog name is " + name);
+     void voice(){
+         System.out.println("Dog name is " + this.name);
          System.out.println("The dog is " + this.age + " years old");
      }
 
--- src/main/java/students/kate_br/lesson_3/level_5/task_19/DogDemo.java
+++ src/main/java/students/kate_br/lesson_3/level_5/task_19/DogDemo.java
@@ -4,10 +4,8 @@ package students.kate_br.lesson_3.level_5.task_19;
 
      public static void main(String[] args) {
 
-         Dog dog = new Dog(4);
-         dog.voice("Penny");
-
-         System.out.println(dog);
+         Dog dog = new Dog("Penny", 4);
+         dog.voice();
 
      }
 }
