8a9af92f532bb4727d6696b5a9e54b3cfc4ecfc0
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-09 22:29:49 +0200

    Lesson 3. Task 12

--- src/main/java/students/kate_br/lesson_3/level_3/task_12/Robot.java
+++ src/main/java/students/kate_br/lesson_3/level_3/task_12/Robot.java
@@ -3,13 +3,20 @@ package students.kate_br.lesson_3.level_3.task_12;
  class Robot {
 
      String name;
-     String sayHello;
-     String sayYourName;
 
 
      Robot(String robotName){
          this.name = robotName;
 
+     }
+
+     void sayHello(){
+         System.out.println("Hello!");
+
+     }
+
+     void sayYourName(){
+         System.out.println("My name is " + this.name);
      }
 
 }
