f627f25ddbf633cf4d741e38d7d7c6291a76739c
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-09 22:19:37 +0200

    Lesson 3. Task 6

--- src/main/java/students/kate_br/lesson_3/_level_1/task_6/Robot.java
+++ src/main/java/students/kate_br/lesson_3/_level_1/task_6/Robot.java
@@ -2,8 +2,13 @@ package students.kate_br.lesson_3._level_1.task_6;
 
  class Robot {
 
+     void sayHello(){
+         System.out.println("Hello!");
 
-    String sayHello;
-    String sayYourName;
+     }
 
+     void sayYourName(){
+         System.out.println("My name is ROBO");
+     }
 }
+
--- src/main/java/students/kate_br/lesson_3/_level_1/task_6/RobotDemo.java
+++ src/main/java/students/kate_br/lesson_3/_level_1/task_6/RobotDemo.java
@@ -4,28 +4,17 @@ package students.kate_br.lesson_3._level_1.task_6;
 
      public static void main(String[] args) {
 
-         Robot robot1 = new Robot();
-         robot1.sayHello = "Hello";
-         robot1.sayYourName = "My name is ROBO";
+         Robot robot = new Robot();
+         robot.sayHello();
+         robot.sayYourName();
 
-         System.out.println(robot1.sayHello);
-         System.out.println(robot1.sayYourName);
+         Robot robot1 = new Robot();
+         robot1.sayHello();
+         robot1.sayYourName();
 
          Robot robot2 = new Robot();
-         robot2.sayHello = "Hello!";
-         robot2.sayYourName = "My name is ROBO2";
-
-         System.out.println(robot2.sayHello);
-         System.out.println(robot2.sayYourName);
-
-         Robot robot3 = new Robot();
-         robot3.sayHello = "Hello :)";
-         robot3.sayYourName = "My name is ROBO3";
-
-         System.out.println(robot3.sayHello);
-         System.out.println(robot3.sayYourName);
-
-
+         robot2.sayHello();
+         robot2.sayYourName();
 
      }
 
