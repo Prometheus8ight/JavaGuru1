06f2b56eb374bd13fa7c9815db7a2f2e162f72e1
PavStein <pav210985@gmail.com>
2021-02-09 16:56:56 +0100

    Lesson 3 Level 5 task 21

---
+++ src/main/java/students/pavels_steinbors/lesson_3/level_5/task_21/DogDemo.java
@@ -0,0 +1,14 @@
+package students.pavels_steinbors.lesson_3.level_5.task_21;
+
+class DogDemo {
+
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog("Roy", "Black", "Roy, Roy, Roy");
+        dog1.voice();
+        dog1.sayYourName();
+        dog1.myDogColor();
+
+    }
+
+}
