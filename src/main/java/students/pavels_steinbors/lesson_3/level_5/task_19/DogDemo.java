4bd8bdb695c68e659389c45d18d622ae679b0f63
PavStein <pav210985@gmail.com>
2021-02-09 13:57:34 +0100

    Lesson 3 Level 5 task 19

---
+++ src/main/java/students/pavels_steinbors/lesson_3/level_5/task_19/DogDemo.java
@@ -0,0 +1,14 @@
+package students.pavels_steinbors.lesson_3.level_5.task_19;
+
+class DogDemo {
+
+
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog("Roy");
+            dog1.sayYourName();
+            dog1.voice();
+            dog1.howOldAreYou();
+
+    }
+}
