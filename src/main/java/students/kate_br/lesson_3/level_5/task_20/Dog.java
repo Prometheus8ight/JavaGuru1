11ff3a187a5341a016b98727401e7ef404a9558d
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-09 14:35:31 +0200

    Lesson 3. Task 20

---
+++ src/main/java/students/kate_br/lesson_3/level_5/task_20/Dog.java
@@ -0,0 +1,30 @@
+package students.kate_br.lesson_3.level_5.task_20;
+
+ class Dog {
+
+     String name;
+     int age;
+
+     Dog(String dogName, int dogAge) {
+
+         this.name = dogName;
+         this.age = dogAge;
+     }
+
+
+     void voice() {
+
+         System.out.println("This is " + this.name);
+         System.out.println(this.name + " is " + this.age + " years old");
+
+
+     }
+
+     void happyBirthday() {
+
+         System.out.println("Happy Birthday, " + name);
+         System.out.println("Today you are " + (age + 1) + " years old");
+         System.out.println("Congratulations, good girl!");
+
+     }
+ }
