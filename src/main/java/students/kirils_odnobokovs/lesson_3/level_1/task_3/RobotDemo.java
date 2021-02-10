e542568a5e21758ac83f7684fb9153a2640fd4ca
Kirils_Odnobokovs <kirils.odnobokovs@gmail.com>
2021-02-10 12:35:58 +0200

    kirils_odnobokovs_lesson_3_level_1_task_3

---
+++ src/main/java/students/kirils_odnobokovs/lesson_3/level_1/task_3/RobotDemo.java
@@ -0,0 +1,8 @@
+package students.kirils_odnobokovs.lesson_3.level_1.task_3;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot myRobot = new Robot();
+    }
+}
