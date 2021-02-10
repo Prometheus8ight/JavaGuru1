0fe8f0584b0b3beaccda34918d7951caf53e87a6
Kirils_Odnobokovs <kirils.odnobokovs@gmail.com>
2021-02-10 12:26:50 +0200

    kirils_odnobokovs_lesson_3_level_1_task_2

---
+++ src/main/java/students/kirils_odnobokovs/lesson_3/level_1/task_2/Robot.java
@@ -0,0 +1,13 @@
+package students.kirils_odnobokovs.lesson_3.level_1.task_2;
+
+public class Robot {
+
+    Robot() {
+
+    }
+
+    public void sayHello() {
+
+        System.out.println("Hello!");
+    }
+}
