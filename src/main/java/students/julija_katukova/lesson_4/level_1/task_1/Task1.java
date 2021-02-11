9237e9b14bfc6093be0796d0044915dd27ead447
Julija Katukova <julija.katukova@gmail.com>
2021-02-11 10:29:44 +0200

    Lesson4 Level1 Task1

---
+++ src/main/java/students/julija_katukova/lesson_4/level_1/task_1/Task1.java
@@ -0,0 +1,23 @@
+package students.julija_katukova.lesson_4.level_1.task_1;
+
+import java.util.Scanner;
+
+class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Please enter integer number: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        if (num > 0) {
+            System.out.println("You entered positive number.");
+        }
+        else if (num < 0) {
+            System.out.println("You entered negative number.");
+        }
+        else {
+            System.out.println("You entered 0. Please try again.");
+        }
+    }
+
+
+}
