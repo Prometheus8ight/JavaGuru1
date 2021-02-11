13c051c8698470ac21099bc8d10f8b29a3215839
Julija Katukova <julija.katukova@gmail.com>
2021-02-11 14:03:00 +0200

    Lesson4 Level1 Task3

---
+++ src/main/java/students/julija_katukova/lesson_4/level_1/task_3/Task3.java
@@ -0,0 +1,40 @@
+package students.julija_katukova.lesson_4.level_1.task_3;
+
+import java.util.Scanner;
+
+class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Please enter integer number from 1 to 7: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+            default:
+                System.out.println("Try again");
+
+        }
+
+
+    }
+
+}
