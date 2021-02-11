5cbc6438b57c64c02926ea323d388fa7be4b5258
Prometheus <pointofview88@gmail.com>
2021-02-11 18:05:23 +0300

    Lesson 4, level 3, task 9.

---
+++ src/main/java/students/vitaly_porsev/lesson_4/level_3/task_9/NumOrderDemo.java
@@ -0,0 +1,8 @@
+package students.vitaly_porsev.lesson_4.level_3.task_9;
+
+class NumOrderDemo {
+    public static void main(String[] args) {
+        NumOrder nums = new NumOrder();
+        nums.numIncDec();
+    }
+}
