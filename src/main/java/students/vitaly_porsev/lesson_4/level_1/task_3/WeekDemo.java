bff148bbeaa5cae131efa6ee31410ba0c721bacb
Prometheus <pointofview88@gmail.com>
2021-02-11 11:59:30 +0300

    Lesson 4, level 1, task 3.

---
+++ src/main/java/students/vitaly_porsev/lesson_4/level_1/task_3/WeekDemo.java
@@ -0,0 +1,8 @@
+package students.vitaly_porsev.lesson_4.level_1.task_3;
+
+class WeekDemo {
+    public static void main(String[] args) {
+        Week week = new Week();
+        week.whatDayIsIt();
+    }
+}
