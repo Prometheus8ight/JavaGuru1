d05d95edaa71e2b3eaaec3d08e5bb2051b340a0f
Julija Katukova <julija.katukova@gmail.com>
2021-02-11 15:38:23 +0200

    Lesson4 Level3 Task10

---
+++ src/main/java/students/julija_katukova/lesson_4/level_3/task_10/Task10.java
@@ -0,0 +1,39 @@
+package students.julija_katukova.lesson_4.level_3.task_10;
+
+import java.util.Scanner;
+
+class Task10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter first integer number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Please enter second integer number:");
+        int num2 = scanner.nextInt();
+        System.out.println("Please enter third integer number:");
+        int num3 = scanner.nextInt();
+        // 1,2,3   1,3,2   2,1,3   2,3,1   3,1,2   3,2,1
+
+        if (num1 > num2 && num2 >= num3) {
+            System.out.println(num1);
+        }
+        else if (num1 > num3 && num3 >= num2) {
+            System.out.println(num1);
+        }
+        else if (num2 > num1 && num1 >= num3) {
+            System.out.println(num2);
+        }
+        else if (num2 > num3 && num3 >= num1)  {
+            System.out.println(num2);
+        }
+        else if (num3 > num1 && num1 >= num2) {
+            System.out.println(num3);
+        }
+        else if (num3 > num2 && num2 >= num1) {
+            System.out.println(num3);
+        }
+        else {
+            System.out.println("Try again");
+        }
+    }
+
+}
