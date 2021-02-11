38517c63761361ec3fe1f7fc175b76d7f5b898aa
Prometheus <pointofview88@gmail.com>
2021-02-11 13:21:32 +0300

    Lesson 4, level 2, task 6.

---
+++ src/main/java/students/vitaly_porsev/lesson_4/level_2/task_6/MinNumDemo.java
@@ -0,0 +1,9 @@
+package students.vitaly_porsev.lesson_4.level_2.task_6;
+
+public class MinNumDemo {
+
+    public static void main(String[] args) {
+        MinNum minimalNumber = new MinNum();
+        System.out.println(minimalNumber.minNum());
+    }
+}
