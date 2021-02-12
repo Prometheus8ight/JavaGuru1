b346620c74e9fa452763811a837052a94282f831
Prometheus <pointofview88@gmail.com>
2021-02-12 19:49:28 +0300

    Lesson 4, level 7, task 22.

---
+++ src/main/java/students/vitaly_porsev/lesson_4/level_7/task_22/FizzBuzzDemo.java
@@ -0,0 +1,16 @@
+package students.vitaly_porsev.lesson_4.level_7.task_22;
+
+class FizzBuzzDemo {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.detect(1));
+
+        FizzBuzzTest fizzBuzzTest = new FizzBuzzTest();
+
+        fizzBuzzTest.FizzTest();
+        fizzBuzzTest.BuzzTest();
+        fizzBuzzTest.FizzBuzzT();
+        fizzBuzzTest.NumTest();
+
+    }
+}
