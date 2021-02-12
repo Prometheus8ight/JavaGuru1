ad13c1053883f88e3c9d59361e0c26e501c600aa
PavStein <pav210985@gmail.com>
2021-02-12 18:05:35 +0100

    Lesson 3 Level 7 task 32

---
+++ src/main/java/students/pavels_steinbors/lesson_3/level_7/task_32/Computer.java
@@ -0,0 +1,22 @@
+package students.pavels_steinbors.lesson_3.level_7.task_32;
+
+
+class Computer {
+
+
+    String manufacturer;
+    String model;
+
+    Computer(String manufacturer, String model) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+        String getManufacturer() {
+            return manufacturer;
+        }
+
+        String getModel() {
+            return model;
+        }
+    }
+
