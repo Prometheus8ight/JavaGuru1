a2812119f7717a3b50cccf1d30d47c56aca4bf5f
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-02-13 21:24:14 +0100

    Bogdans Pavlovs - Lesson 4 - Level 3 - Task 11

--- src/main/java/students/bogdans_pavlovs/lesson_4/level_4/task_11/Calculator.java
+++ src/main/java/students/bogdans_pavlovs/lesson_4/level_4/task_11/Calculator.java
@@ -2,28 +2,10 @@ package students.bogdans_pavlovs.lesson_4.level_4.task_11;
 
 class Calculator {
 
-    public int addition (int firstNumber, int secondNumber) {
+    public int add (int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
     }
-    public int subtraction (int firstNumber, int secondNumber) {
-
-        if (firstNumber > secondNumber){
-            return firstNumber - secondNumber;
-        } else {
-            return secondNumber - firstNumber;
-        }
-
-    }
-
-    public int division (int firstNumber, int secondNumber) {
-        if (firstNumber > secondNumber){
-            return firstNumber / secondNumber;
-        } else {
-            return secondNumber / firstNumber;
-        }
-    }
-
-    public int multiplication (int firstNumber, int secondNumber) {
-        return firstNumber * secondNumber;
-    }
+    public int subtract (int firstNumber, int secondNumber) { return firstNumber - secondNumber; }
+    public int divide (int firstNumber, int secondNumber) { return firstNumber / secondNumber; }
+    public int multiply (int firstNumber, int secondNumber) { return firstNumber * secondNumber; }
 }
