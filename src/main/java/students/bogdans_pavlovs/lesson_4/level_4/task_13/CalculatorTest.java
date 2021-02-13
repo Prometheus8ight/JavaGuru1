9900c0f304a17b25aa651bb02d97fabb8018ec14
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-02-13 22:17:16 +0100

    Bogdans Pavlovs - Lesson 4 - Level 4 - Task 13

---
+++ src/main/java/students/bogdans_pavlovs/lesson_4/level_4/task_13/CalculatorTest.java
@@ -0,0 +1,79 @@
+package students.bogdans_pavlovs.lesson_4.level_4.task_13;
+
+class CalculatorTest {
+
+    public static void main(String[] args) {
+        CalculatorTest calculatorTest = new CalculatorTest();
+        calculatorTest.addTest();
+        calculatorTest.subtractTest();
+        calculatorTest.divideTest();
+        calculatorTest.multiplyTest();
+        calculatorTest.isEvenTest();
+        // по аналогии реализуйте тесты для остальных методов
+    }
+
+    public void addTest() {
+        int firstNumber = 10;  // подготавливаем тестовые данные
+        int secondNumber = 5;  // подготавливаем тестовые данные
+        int expectedResult = 15;  // подготавливаем тестовые данные
+        Calculator calculator = new Calculator();
+        int realResult = calculator.add(firstNumber, secondNumber);  // вызываем продакшен код, который хотим протестировать
+        if (realResult == expectedResult) {  // проверяем правильно ли сработал тестируемый код
+            System.out.println("Add test = OK");
+        } else {
+            System.out.println("Add test = FAIL");
+        }
+    }
+
+    public void subtractTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 5;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.subtract(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Subtract test = OK");
+        } else {
+            System.out.println("Subtract test = FAIL");
+        }
+    }
+
+    public void divideTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 2;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.divide(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Divide test = OK");
+        } else {
+            System.out.println("Divide test = FAIL");
+        }
+    }
+
+    public void multiplyTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 50;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.multiply(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Multiply test = OK");
+        } else {
+            System.out.println("Multiply test = FAIL");
+        }
+    }
+
+    public void isEvenTest() {
+        int yourNumber = 7;
+        boolean expectedResult = false;
+        Calculator calculator = new Calculator();
+        boolean realResult = calculator.isEven(yourNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Remainder test = OK");
+        } else {
+            System.out.println("Remainder test = FAIL");
+        }
+    }
+
+}
