d6f78de587f7b3844077c55aedf94dee151ba868
alex <kalashnikov_alex@yahoo.com>
2021-02-16 01:56:16 +0200

    Homework; Lesson 4; Level 5; Tasks 16.

---
+++ src/main/java/students/alex_kalashnikov/lesson_4/level_5/task_16/LightColorDetector.java
@@ -0,0 +1,14 @@
+package students.alex_kalashnikov.lesson_4.level_5.task_16;
+
+class LightColorDetector {
+
+    /*
+    380 ... 449 - Фиолетовый ("Violet")
+    450 ... 494 - Синий ("Blue")
+    495 ... 569 - Зеленый ("Green")
+    570 ... 589 - Желтый ("Yellow")
+    590 ... 619 - Оранжевый ("Orange")
+    620 ... 750 - Красный ("Red")
+    Вне диапазонов - невидимый спектр ("Invisible Light")
+     */
+}
