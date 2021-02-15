78d90ee736d3151531dd30561570de8245e7fcfa
PavStein <pav210985@gmail.com>
2021-02-15 12:51:48 +0100

    Lesson 4 Level 2 task 5

---
+++ src/main/java/students/pavels_steinbors/lesson_4_level_2_task_5/Max.java
@@ -0,0 +1,29 @@
+package students.pavels_steinbors.lesson_4_level_2_task_5;
+
+import java.util.Scanner;
+
+class Max {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        int max;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое целое число: ");
+        a = scanner.nextInt();
+        System.out.println("Введите второе целое число: ");
+        b = scanner.nextInt();
+
+        max = a;
+        if (b > a) {
+            max = b;
+        }
+
+        System.out.println("Наибольшее из двух чисел: " + max);
+
+    }
+
+}
