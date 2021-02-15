d5ba54020ffdeef7a568253f7636979c00a6f3e6
Andrejs24 <andrejs.kuramsins@gmail.com>
2021-02-15 16:32:02 +0200

    Andrejs Kuramšins lesson 3 task 1

---
+++ src/main/java/students/andrejs_kuramsins/lesson_2/level_0/task_0/lesson_3/level_1/task_1/Robot.java
@@ -0,0 +1,8 @@
+package students.andrejs_kuramsins.lesson_2.level_0.task_0.lesson_3.level_1.task_1;
+
+class Robot {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
