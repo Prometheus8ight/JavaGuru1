540f44380d9c1fade38e936e6f1721e86c024597
Julija Katukova <julija.katukova@gmail.com>
2021-02-15 17:12:52 +0200

    Lesson4 Level6 Task17

--- src/main/java/students/julija_katukova/lesson_4/level_6/task_17/Stock.java
+++ src/main/java/students/julija_katukova/lesson_4/level_6/task_17/Stock.java
@@ -2,18 +2,17 @@ package students.julija_katukova.lesson_4.level_6.task_17;
 
 class Stock {
     String companyName; //ввели характеристики
-    int stockInitialPrice;
     int currentPrice;
     int maxPrice;
     int minPrice;
 
-    public int updatePrice(int stockCurrentPrice){  //добавили метод updatePrice
+    public void updatePrice(int stockCurrentPrice){  //добавили метод updatePrice
         if (stockCurrentPrice > maxPrice) {  // если новая цена больше максимальной, она становится максимальной
-           return maxPrice = stockCurrentPrice;
+           maxPrice = stockCurrentPrice;
         }
         if (stockCurrentPrice < minPrice) {             // если новая цена меньше минимальной, она становится минимальной
-            return minPrice = stockCurrentPrice;}
-        return currentPrice = stockCurrentPrice;        // изменяется текущая стоймоть
+            minPrice = stockCurrentPrice;}
+        currentPrice = stockCurrentPrice;        // изменяется текущая стоймоть
     }
 
     public String getPriceInformation() {              //добавили метод getPriceInformation
@@ -24,12 +23,11 @@ class Stock {
         return company + current + min + max;
     }
 
-    Stock(String stockCompanyName, int stockInitialPrice) {  //добавили конструктор, чтобы Имя компании и начальную стоимость можно было задавать в момент создания акции
+    Stock(String stockCompanyName, int stockCurrentPrice) {  //добавили конструктор, чтобы Имя компании и начальную стоимость можно было задавать в момент создания акции
         this.companyName = stockCompanyName;
-        this.stockInitialPrice = stockInitialPrice;
-        currentPrice = stockInitialPrice;
-        maxPrice = stockInitialPrice;                   // в начале минимальная и максимальная цена равны изначальной цене
-        minPrice = stockInitialPrice;
+        this.currentPrice = stockCurrentPrice;
+        maxPrice = stockCurrentPrice;                   // в начале минимальная и максимальная цена равны изначальной цене
+        minPrice = stockCurrentPrice;
     }
 
     public static void main(String[] args) {
