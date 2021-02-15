19ed7e87b3cd8643fc1423773c52af8e1a098bad
alex <kalashnikov_alex@yahoo.com>
2021-02-15 23:01:28 +0200

    Homework, Lesson 4, Level 2, Tasks 5,6,7.

---
+++ src/main/java/students/alex_kalashnikov/lesson_4/level_4/task_11/Calculator.java
@@ -0,0 +1,9 @@
+package students.alex_kalashnikov.lesson_4.level_4.task_11;
+
+class Calculator {
+
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+}
