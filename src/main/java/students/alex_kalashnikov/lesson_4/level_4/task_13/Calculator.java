49c9a866fac487dee537ae717fa46808125778e2
alex <kalashnikov_alex@yahoo.com>
2021-02-15 23:45:04 +0200

    Homework; Lesson 4; Level 4; Tasks 11,12,13,14,15.

---
+++ src/main/java/students/alex_kalashnikov/lesson_4/level_4/task_13/Calculator.java
@@ -0,0 +1,28 @@
+package students.alex_kalashnikov.lesson_4.level_4.task_13;
+
+class Calculator {
+
+    public int sum1(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int sub1(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int div1(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public int multi1(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public boolean isEven1(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
