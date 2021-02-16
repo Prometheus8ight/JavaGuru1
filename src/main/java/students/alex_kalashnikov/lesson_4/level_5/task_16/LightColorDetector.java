0e9292cc638930ac19dc2c13bd5e92ac931198d0
alex <kalashnikov_alex@yahoo.com>
2021-02-16 02:16:29 +0200

    Homework; Lesson 4; Level 5; Tasks 16.

--- src/main/java/students/alex_kalashnikov/lesson_4/level_5/task_16/LightColorDetector.java
+++ src/main/java/students/alex_kalashnikov/lesson_4/level_5/task_16/LightColorDetector.java
@@ -11,4 +11,32 @@ class LightColorDetector {
     620 ... 750 - Красный ("Red")
     Вне диапазонов - невидимый спектр ("Invisible Light")
      */
+
+
+
+    public String detect(int wavelength) {
+       if (wavelength >= 380 && wavelength <=449) {
+           return "Violet";
+       }
+       else if (wavelength >= 450 && wavelength <=494) {
+           return "Blue";
+       }
+       else if (wavelength >= 495 && wavelength <=569) {
+           return "Green";
+       }
+       else if (wavelength >= 570 && wavelength <= 589) {
+           return "Yellow";
+       }
+       else if (wavelength >= 590 && wavelength <= 619) {
+           return "Orange";
+       }
+       else if (wavelength >= 620 && wavelength <= 750) {
+           return "Red";
+       }
+       else {
+           return "Invisible Light";
+       }
+
+    }
+
 }
