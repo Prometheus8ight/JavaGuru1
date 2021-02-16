5b52d1f0c070b77daeea406788cfe08aa317d5d6
DJ9780772 <denissjankovskis@outlook.com>
2021-02-16 13:51:18 +0200

    Lesson 3 Task 9 update

--- src/main/java/students/deniss_jankovskis/lesson_3/lesson_3_level_2/Task_9.java
+++ src/main/java/students/deniss_jankovskis/lesson_3/lesson_3_level_2/Task_9.java
@@ -1,10 +1,12 @@
 package students.deniss_jankovskis.lesson_3.lesson_3_level_2;
 
+import java.util.Scanner;
+
 class Task_9 {
 
     public static void main(String[] args) {
 
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter your name: ");
 
         String userName = scanner.nextLine();
