a52605330fae8c4f8edd40aed172276f36949f51
alex <kalashnikov_alex@yahoo.com>
2021-02-17 13:20:21 +0200

    Homework; Lesson 4; Level 3; Task 10 fixed!!!

--- src/main/java/students/alex_kalashnikov/lesson_4/level_3/task_10/IntegerMax.java
+++ src/main/java/students/alex_kalashnikov/lesson_4/level_3/task_10/IntegerMax.java
@@ -2,64 +2,22 @@ package students.alex_kalashnikov.lesson_4.level_3.task_10;
 
 class IntegerMax {
 
-    public static void main(String[] args) {
+    // max(a, max(b, c)) //
 
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print("Enter first integer: ");
-        int intFirst = scanner.nextInt();
-
-        System.out.print("Enter second integer: ");
-        int intSecond = scanner.nextInt();
-
-        System.out.print("Enter third integer: ");
-        int intThird = scanner.nextInt();
-
-        /* 1) int1 > int2 и int1 > int3;
-           2) int1 > int2 и int1 = int3;
-           3) int1 = int2 и int1 > int3;
-
-           4) int2 > int1 и int2 > int3;
-           5) int2 > int1 и int2 = int3;
-           6) int2 = int1 и int2 > int3;
-
-           7) int3 > int1 и int3 > int2;
-           8) int3 > int1 и int3 = int2;
-           9) int3 = int1 и int3 > int2;
-
-          10) int1 = int2 = int3;
-        */
-
-        if (intFirst > intSecond && intFirst > intThird) {             // 1
-            System.out.println(intFirst);
+    public int max1(int intSecond, int intThird) {   // сначала вычисляется Max(b, c)
+        if (intSecond > intThird) {
+            return intSecond;
+        } else {
+            return intThird;
         }
-        else if (intFirst > intSecond && intFirst == intThird) {       // 2
-            System.out.println(intFirst);
-        }
-        else if(intFirst == intSecond && intFirst > intThird) {        // 3
-            System.out.println(intFirst);
-        }
-        else if(intSecond > intFirst && intSecond > intThird) {        // 4
-            System.out.println(intSecond);
-        }
-        else if(intSecond > intFirst && intSecond == intThird) {       // 5
-            System.out.println(intSecond);
-        }
-        else if(intSecond == intFirst && intSecond > intThird) {       // 6
-            System.out.println(intSecond);
-        }
-        else if(intThird > intFirst && intThird > intSecond) {         // 7
-            System.out.println(intThird);
-        }
-        else if(intThird > intFirst && intThird == intSecond) {        // 8
-            System.out.println(intThird);
-        }
-        else if(intThird == intFirst && intThird > intSecond) {        // 9
-            System.out.println(intThird);
-        }
-        else {
-            System.out.println("All integers are equal");              // 10
-        }
-
     }
 
+
+    public int max(int intFirst, int x) {            // потом вычисляется Max(a, x) где x = Max(b, c)
+        if (intFirst > x) {
+            return intFirst;
+        } else {
+            return x;
+        }
+    }
 }
