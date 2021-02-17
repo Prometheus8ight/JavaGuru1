71955e7ac7e8ae13d2771b36a5f0052e5c50b222
Julija Katukova <julija.katukova@gmail.com>
2021-02-17 22:46:34 +0200

    Lesson5 Level1 Task3

---
+++ src/main/java/students/julija_katukova/lesson_5/level_1/task_3/ArrayDefinition3.java
@@ -0,0 +1,8 @@
+package students.julija_katukova.lesson_5.level_1.task_3;
+
+class ArrayDefinition3 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10]; // т.к. переменная numbers должна ссылаться на массив целых чисел, тип массива нужно поменять на int и добавить []
+    }
+
+}
