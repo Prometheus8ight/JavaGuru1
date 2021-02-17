e289c9bc5d217c5ae237589fd937c6bf9088a1e2
Julija Katukova <julija.katukova@gmail.com>
2021-02-17 23:12:37 +0200

    Lesson5 Level1 Task9

---
+++ src/main/java/students/julija_katukova/lesson_5/level_1/task_9/ArrayLength3.java
@@ -0,0 +1,8 @@
+package students.julija_katukova.lesson_5.level_1.task_9;
+
+class ArrayLength3 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2]; // добавили []
+        System.out.println(numbers.length); // убрали ()
+    }
+}
