c8725bc060c5c36c085493dce8b7700cbeaec8e3
Julija Katukova <julija.katukova@gmail.com>
2021-02-17 23:23:48 +0200

    Lesson5 Level2 Task10

---
+++ src/main/java/students/julija_katukova/lesson_5/level_2/task_10/Task10.java
@@ -0,0 +1,13 @@
+package students.julija_katukova.lesson_5.level_2.task_10;
+
+import java.util.Arrays;
+
+class Task10 {
+    public static void main(String[] args) {
+        int[] numbers = new int[3]; //создайте массив целых чисел длиной 3
+        numbers[0] = 123;//заполните каждую ячейку массива любым целым числом
+        numbers[1] = 321;
+        numbers [2] = 231;
+        System.out.println(Arrays.toString(numbers));//выведите значение каждой ячейки на консоль.
+    }
+}
