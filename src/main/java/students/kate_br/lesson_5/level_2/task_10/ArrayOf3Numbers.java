44bb9be1fa91909bf11e5704cd48d89f98136787
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-18 17:55:25 +0200

    Lesson 5. Task 10

---
+++ src/main/java/students/kate_br/lesson_5/level_2/task_10/ArrayOf3Numbers.java
@@ -0,0 +1,15 @@
+package students.kate_br.lesson_5.level_2.task_10;
+
+ class ArrayOf3Numbers {
+
+     public static void main(String[] args) {
+         int[] numbers = new int[3];
+         numbers[0] = 1;
+         numbers[1] = 2;
+         numbers[2] = 3;
+
+         System.out.println("Number One: " + numbers[0]);
+         System.out.println("Number Two: " + numbers[1]);
+         System.out.println("Number Three: " + numbers[2]);
+     }
+}
