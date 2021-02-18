05eb9dd4b81bad5bd0c92945b106b625d2db3277
alex <kalashnikov_alex@yahoo.com>
2021-02-18 17:46:30 +0200

    Homework; Lesson 5; Level 4; Tasks 25,26,27,.

---
+++ src/main/java/students/alex_kalashnikov/lesson_5/level_4/task_27/ArrayInt.java
@@ -0,0 +1,31 @@
+package students.alex_kalashnikov.lesson_5.level_4.task_27;
+
+class ArrayInt {
+
+    public static void main(String[] args) {
+
+        Prompter number = new Prompter();
+
+        int[] arrayInt = new int[number.promptArrayLength()];
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            arrayInt[i] = (int) (Math.pow(-2, 31) + (Math.random() * Math.pow(2, 32)));
+        }
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.println("Index [" + i + "]: " + arrayInt[i]);
+        }
+
+        int max = arrayInt[0];
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] > max) {
+                max = arrayInt[i];
+            }
+        }
+
+        System.out.println("Max number in Array is: " + max);
+
+    }
+
+}
