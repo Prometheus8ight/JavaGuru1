7a2b94e6bde322488ee17f7248eef4893f98a37e
RodionA <raf91@inbox.lv>
2021-02-18 02:39:13 +0200

    Lesson-5-level-1-task-2

---
+++ src/main/java/students/rodion_afanasjev/lesson_5/level_1/task_2/ArrayDefinition.java
@@ -0,0 +1,10 @@
+package students.rodion_afanasjev.lesson_5.level_1.task_2;
+
+class ArrayDefinition {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        System.out.println(numbers.length);
+    }
+
+}
