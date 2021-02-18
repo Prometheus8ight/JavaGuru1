3c9628d69d1686598af8d864421de2714d738ded
alex <kalashnikov_alex@yahoo.com>
2021-02-18 11:44:50 +0200

    Homework; Lesson 5; Level 1; Tasks 1,.

---
+++ src/main/java/students/alex_kalashnikov/lesson_5/level_1/task_1/ArrayDefinition1.java
@@ -0,0 +1,9 @@
+package students.alex_kalashnikov.lesson_5.level_1.task_1;
+
+class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10]; // поставить квадратные скобки, указать длину массива (например 10);
+    }
+
+}
