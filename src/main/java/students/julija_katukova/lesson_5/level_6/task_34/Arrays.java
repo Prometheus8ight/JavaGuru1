5dc99dd318ff450e5b6c6d22dc11e17b478670b9
Julija Katukova <julija.katukova@gmail.com>
2021-02-18 17:29:31 +0200

    Lesson5 Level6 Task34

---
+++ src/main/java/students/julija_katukova/lesson_5/level_6/task_34/Arrays.java
@@ -0,0 +1,7 @@
+package students.julija_katukova.lesson_5.level_6.task_34;
+
+class Arrays {
+    public void print(int[] array) {
+        System.out.println(java.util.Arrays.toString(array));
+    }
+}
