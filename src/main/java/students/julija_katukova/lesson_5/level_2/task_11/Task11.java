0f292a1934513cd712437492296a59f6aca18cdc
Julija Katukova <julija.katukova@gmail.com>
2021-02-18 14:02:12 +0200

    Lesson5 Level2 Task11

--- src/main/java/students/julija_katukova/lesson_5/level_2/task_11/Task11.java
+++ src/main/java/students/julija_katukova/lesson_5/level_2/task_11/Task11.java
@@ -7,13 +7,10 @@ class Task11 {
     public static void main(String[] args) {
        int[] numbers = new int[3];//создайте массив целых чисел длиной 3
         Scanner scanner= new Scanner(System.in);
-        System.out.println("Please enter first number: "); // запрашиваем число у пользователя
-        numbers[0] = scanner.nextInt(); // заполняем этим числом ячейку
-        System.out.println("Please enter second number: ");
-        numbers[1] = scanner.nextInt();
-        System.out.println("Please enter third number: ");
-        numbers[2] = scanner.nextInt();
-
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Please enter array element value: "); // запрашиваем число у пользователя
+            numbers[i] = scanner.nextInt(); // заполняем этим числом ячейку
+        }
         System.out.println(Arrays.toString(numbers)); // выводим на консоль
     }
 
