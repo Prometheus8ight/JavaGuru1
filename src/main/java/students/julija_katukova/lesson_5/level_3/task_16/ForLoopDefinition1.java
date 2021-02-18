1bb80a66d369b63480dba9ba8d3120b62369bee9
Julija Katukova <julija.katukova@gmail.com>
2021-02-18 12:36:27 +0200

    Lesson5 Level3 Task16

---
+++ src/main/java/students/julija_katukova/lesson_5/level_3/task_16/ForLoopDefinition1.java
@@ -0,0 +1,10 @@
+package students.julija_katukova.lesson_5.level_3.task_16;
+
+class ForLoopDefinition1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) { //добавили = 0
+            System.out.println(i);
+        }
+    }
+
+}
