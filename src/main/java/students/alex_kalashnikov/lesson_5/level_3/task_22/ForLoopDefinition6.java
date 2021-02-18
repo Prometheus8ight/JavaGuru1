06ce05c5d5afb4d6342e91ab0381f0601708ae65
alex <kalashnikov_alex@yahoo.com>
2021-02-18 16:31:13 +0200

    Homework; Lesson 5; Level 3; Tasks 16,17,18,19,20,21,22,.

---
+++ src/main/java/students/alex_kalashnikov/lesson_5/level_3/task_22/ForLoopDefinition6.java
@@ -0,0 +1,11 @@
+package students.alex_kalashnikov.lesson_5.level_3.task_22;
+
+class ForLoopDefinition6 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {           // пропущена круглая скобка
+            System.out.println(i);
+        }
+    }
+
+}
