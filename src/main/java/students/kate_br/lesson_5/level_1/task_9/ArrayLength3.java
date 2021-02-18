edf9a8f46881982d946d388a6fab7aeb3cf54c08
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-18 17:49:29 +0200

    Lesson 5. Task 9

---
+++ src/main/java/students/kate_br/lesson_5/level_1/task_9/ArrayLength3.java
@@ -0,0 +1,9 @@
+package students.kate_br.lesson_5.level_1.task_9;
+
+ class ArrayLength3 {
+
+     public static void main(String[] args) {
+         int[] numbers = new int[2];
+         System.out.println("Quantity of numbers: " + numbers.length);
+     }
+}
