9a1dd4eb539c3e720081a3cdf87883e130f04cb1
alex <kalashnikov_alex@yahoo.com>
2021-02-18 16:13:29 +0200

    Homework; Lesson 5; Level 3; Tasks 16,.

---
+++ src/main/java/students/alex_kalashnikov/lesson_5/level_3/task_16/ForLoopDefinition1.java
@@ -0,0 +1,11 @@
+package students.alex_kalashnikov.lesson_5.level_3.task_16;
+
+class ForLoopDefinition1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {            // для int i нужно задать начальное значение, например 0;
+            System.out.println(i);
+        }
+    }
+
+}
