a011e779414e04a1b3d4c92590d60b399adeff7f
Prometheus <pointofview88@gmail.com>
2021-02-18 15:49:25 +0300

    Lesson 5, level 5, task 31.

---
+++ src/main/java/students/vitaly_porsev/lesson_5/level_5/task_31/ArraysDemo.java
@@ -0,0 +1,9 @@
+package students.vitaly_porsev.lesson_5.level_5.task_31;
+
+class ArraysDemo {
+    public static void main(String[] args) {
+        Arrays method = new Arrays();
+        int[] array = method.create(10);
+        System.out.println(java.util.Arrays.toString(array));
+    }
+}
