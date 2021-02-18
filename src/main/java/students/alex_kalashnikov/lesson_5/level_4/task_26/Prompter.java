f6454c55e79d68445a7be97517f1f49468c3c17d
alex <kalashnikov_alex@yahoo.com>
2021-02-18 17:24:32 +0200

    Homework; Lesson 5; Level 4; Tasks 25,26,.

---
+++ src/main/java/students/alex_kalashnikov/lesson_5/level_4/task_26/Prompter.java
@@ -0,0 +1,13 @@
+package students.alex_kalashnikov.lesson_5.level_4.task_26;
+
+import java.util.Scanner;
+
+class Prompter {
+
+    int promptArrayLength() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please, enter Array length: ");
+        return scanner.nextInt();
+    }
+
+}
