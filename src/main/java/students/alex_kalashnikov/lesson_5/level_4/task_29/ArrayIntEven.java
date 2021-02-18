efcaacb802dcd3f7332e0350729238ecc2ad2041
alex <kalashnikov_alex@yahoo.com>
2021-02-18 18:03:41 +0200

    Homework; Lesson 5; Level 4; Tasks 25,26,27,28,29,.

---
+++ src/main/java/students/alex_kalashnikov/lesson_5/level_4/task_29/ArrayIntEven.java
@@ -0,0 +1,28 @@
+package students.alex_kalashnikov.lesson_5.level_4.task_29;
+
+class ArrayIntEven {
+
+    public static void main(String[] args) {
+
+        Prompter number = new Prompter();
+
+        int[] arrayInt = new int[number.promptArrayLength()];
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            arrayInt[i] = (int) (Math.pow(-2, 31) + (Math.random() * Math.pow(2, 32)));
+        }
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.println("Index [" + i + "]: " + arrayInt[i]);
+        }
+
+        System.out.println("=================================="); // пропуск в консоли
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] % 2 == 0) {
+                System.out.println("Index [" + i + "]: " + arrayInt[i] + " is EVEN");
+            }
+        }
+    }
+
+}
