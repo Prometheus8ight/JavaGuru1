0e4714c87aa53cbed83ade07c1b385af6a6ccfd9
PavStein <pav210985@gmail.com>
2021-02-19 21:36:22 +0100

    Lesson 5 level 1 task 6

---
+++ src/main/java/students/pavels_steinbors/_lesson_5/_level_1/_task_6/ArrayDefinition6.java
@@ -0,0 +1,8 @@
+package students.pavels_steinbors._lesson_5._level_1._task_6;
+
+class ArrayDefinition6 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, };
+    }
+}
