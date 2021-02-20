39041755dfc70ce38ff9dda6a81edbb0afaa78c0
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-20 17:33:13 +0200

    Lesson 5. Task 35

---
+++ src/main/java/students/kate_br/lesson_5/level_6/task_35/Arrays.java
@@ -0,0 +1,10 @@
+package students.kate_br.lesson_5.level_6.task_35;
+
+ class Arrays {
+
+     public int findMax(int[] array){
+          int max;
+         System.out.println(Arrays.stream(array).max().getAsInt);
+
+     }
+}
