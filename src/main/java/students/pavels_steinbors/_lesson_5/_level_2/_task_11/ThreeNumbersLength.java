56bffebfed91f6bae7e339d50de3c6f3ea875c2a
PavStein <pav210985@gmail.com>
2021-02-20 11:25:17 +0100

    Lesson 5 level 2 task 11

---
+++ src/main/java/students/pavels_steinbors/_lesson_5/_level_2/_task_11/ThreeNumbersLength.java
@@ -0,0 +1,16 @@
+package students.pavels_steinbors._lesson_5._level_2._task_11;
+
+import java.util.Arrays;
+
+class ThreeNumbersLength {
+
+    public static void main(String[] args) {
+
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+
+        System.out.println("Numbers: " + Arrays.toString(number) );
+    }
+}
