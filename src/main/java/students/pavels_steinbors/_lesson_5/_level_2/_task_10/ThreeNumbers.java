d17bb6382194a169e25ff4d31b17cd7e2e98e512
PavStein <pav210985@gmail.com>
2021-02-20 11:11:45 +0100

    Lesson 5 level 2 task 10

---
+++ src/main/java/students/pavels_steinbors/_lesson_5/_level_2/_task_10/ThreeNumbers.java
@@ -0,0 +1,16 @@
+package students.pavels_steinbors._lesson_5._level_2._task_10;
+
+class ThreeNumbers {
+
+    public static void main(String[] args) {
+
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+
+        System.out.println("number 1 = " + number[0]);
+        System.out.println("number 2 = " + number[1]);
+        System.out.println("number 3 = " + number[2]);
+    }
+}
