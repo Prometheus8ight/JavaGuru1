07e6ea1d7bf87da796902b7c408a898a54729fc7
PavStein <pav210985@gmail.com>
2021-02-20 10:01:50 +0100

    Lesson 5 level 1 task 7 8 9

---
+++ src/main/java/students/pavels_steinbors/_lesson_5/_level_1/_task_7_8_9/ArrayLength2.java
@@ -0,0 +1,11 @@
+package students.pavels_steinbors._lesson_5._level_1._task_7_8_9;
+
+class ArrayLength2 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
