fdc7712dd04ab38457fcd76e03b237a5f8f7603e
DJ9780772 <denissjankovskis@outlook.com>
2021-02-21 16:24:06 +0200

    Lesson 5, Tasks 1-9

---
+++ src/main/java/students/deniss_jankovskis/lesson_5/level_1/Task_7.java
@@ -0,0 +1,11 @@
+package students.deniss_jankovskis.lesson_5.level_1;
+
+class ArrayLength1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[0]);
+    }
+
+}
+
