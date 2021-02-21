751ae05c87aa1787fe8ec1af8f836aa316473f50
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-02-21 12:52:22 +0100

    Bogdans Pavlovs - Lesson 5 - Level 4 - Tasks 25 to 30

---
+++ src/main/java/students/bogdans_pavlovs/lesson_5/task_27/ArraysTask27.java
@@ -0,0 +1,40 @@
+package students.bogdans_pavlovs.lesson_5.task_27;
+
+import java.util.Random;
+
+class ArraysTask27 {
+
+    public static void main(String[] args) {
+
+        Random getNumber = new Random();
+
+        //создайте массив произвольной длины
+        int[] numbers = new int[10];
+        int max = numbers[0];
+
+        //заполните массив случайными числами
+        for (int i = 0; i < numbers.length; i++){
+
+            numbers[i] = getNumber.nextInt();
+
+        }
+
+        //распечатайте на консоль все элементы массива
+        System.out.print("Your array contains: ");
+
+        for (int i = 0; i <numbers.length; i++){
+
+            System.out.print(numbers[i] + "; ");
+
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+
+        }
+        System.out.println("");
+        //найдите наибольшее число в массиве и выведети его на консоль
+        System.out.print("Highest array value is: " + max);
+
+    }
+
+}
