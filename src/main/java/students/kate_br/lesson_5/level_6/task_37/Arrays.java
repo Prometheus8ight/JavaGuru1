657c840e994121dbaa92807cb329e68ce3615137
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-21 12:51:02 +0200

    Lesson 5. Task 37

---
+++ src/main/java/students/kate_br/lesson_5/level_6/task_37/Arrays.java
@@ -0,0 +1,16 @@
+package students.kate_br.lesson_5.level_6.task_37;
+
+ class Arrays {
+
+     public int findMin(int[] array){
+         int min = 0;
+
+         for(int num : array){
+             if (num < min){
+                 min = num;
+             }
+         }
+         return min;
+     }
+
+ }
