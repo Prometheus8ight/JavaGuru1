a1b2c8b42298f2c171a696f575088b571f11c462
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-21 21:22:44 +0200

    Lesson 5. Task 4

--- src/main/java/students/kate_br/lesson_5/level_1/task_4/ArrayDefinition4.java
+++ src/main/java/students/kate_br/lesson_5/level_1/task_4/ArrayDefinition4.java
@@ -3,7 +3,7 @@ package students.kate_br.lesson_5.level_1.task_4;
  class ArrayDefinition4 {
 
      public static void main(String[] args) {
-         int[] numbers = new int [123];
+         int[] numbers = { 1, 2, 3};
 
      }
  }
