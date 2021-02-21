3bc704d599e9430935638e82dd7c662c647b9f0b
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-02-21 20:39:56 +0200

    lesson5-level4-AllTask

---
+++ src/main/java/students/kristine_paskevica/lesson_5/level_4/Task28.java
@@ -0,0 +1,20 @@
+package students.kristine_paskevica.lesson_5.level_4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+class Task28 {
+
+    public static void main(String[] args) {
+
+        int [] masivs = new int[6];
+
+        Random genereSkaitlus = new Random();
+
+        for (int i = 0; i < 6; i ++){
+            masivs[i] = genereSkaitlus.nextInt();
+        }
+        System.out.println("Masīva šūnās glabājas šādas vērtības: " + Arrays.toString(masivs));
+        System.out.println("Vislielākais no masīvā esošiem skaitļiem ir: " + Arrays.stream(masivs).min().getAsInt());
+    }
+}
