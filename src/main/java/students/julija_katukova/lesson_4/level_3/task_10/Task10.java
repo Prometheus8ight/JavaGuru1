de3298c74d7df5149fe92cc9ac8836990ac7a06f
Julija Katukova <julija.katukova@gmail.com>
2021-02-22 10:58:16 +0200

    Lesson4 Level3 Task10

--- src/main/java/students/julija_katukova/lesson_4/level_3/task_10/Task10.java
+++ src/main/java/students/julija_katukova/lesson_4/level_3/task_10/Task10.java
@@ -13,22 +13,25 @@ class Task10 {
         int num3 = scanner.nextInt();
         // 1,2,3   1,3,2   2,1,3   2,3,1   3,1,2   3,2,1
 
-        if (num1 >= num2 && num2 >= num3) {
-            System.out.println(num1);
-        }
-        else if (num1 >= num3 && num3 >= num2) {
-            System.out.println(num1);
-        }
-        else if (num2 >= num1 && num1 >= num3) {
-            System.out.println(num2);
-        }
-        else if (num2 >= num3)  {
-            System.out.println(num2);
-        }
-        else  {
-            System.out.println(num3);
-        }
+        System.out.println(max3(num1, num2, num3));
 
+    }
+    public static int max2(int num1, int num2) {
+        if (num1 >= num2) {
+            return num1;
+        }
+        else {
+            return num2;
+        }
+    }
+    public static int max3 (int num1, int num2, int num3) {
+        int max2 = Task10.max2(num1, num2);
+        if (max2 >= num3) {
+            return max2;
+        }
+        else {
+            return num3;
+        }
     }
 
 }
