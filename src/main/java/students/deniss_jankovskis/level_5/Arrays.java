90ddf126129dad350530f90fe87e92c094487d11
DJ9780772 <denissjankovskis@outlook.com>
2021-02-22 16:05:37 +0200

    Level 5-6, Tasks 31-38

---
+++ src/main/java/students/deniss_jankovskis/level_5/Arrays.java
@@ -0,0 +1,45 @@
+package students.deniss_jankovskis.level_5;
+
+import java.util.Random;
+
+class Arrays {
+
+
+    public static int[] create(int length) {
+        int[] num = new int[length];
+        return num;
+    }
+
+    public void fillRandomly(int[] array) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt();
+        }
+    }
+
+    public void print(int[] array) {
+        System.out.println(java.util.Arrays.toString(array));
+    }
+
+    public static int findMax(int[] array) {
+        int maxValue = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
+
+
+    public static int findMin(int[] array) {
+        int minValue = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < minValue){
+                minValue = array[i];
+            }
+        }
+        return minValue;
+    }
+
+}
---
+++ src/main/java/students/deniss_jankovskis/level_5/ArraysTest.java
@@ -0,0 +1,46 @@
+package students.deniss_jankovskis.level_5;
+
+class ArraysTest {
+
+    public static void main(String[] args) {
+        ArraysTest test = new ArraysTest();
+        test.shouldCreateArray();
+        test.shouldFindMaxNumber();
+        test.shouldFindMinNumber();
+    }
+
+    public void shouldCreateArray() {
+        int length = 8;
+        int expectedResult = 8;
+        int realResult = Arrays.create(8).length;
+        check(realResult, expectedResult, "CreateArrayLength");
+
+    }
+
+    public void shouldFindMaxNumber() {
+        int[] array = {20, 78, 104, 350, 500, 650, 730, 920};
+        int expectedResult = 920;
+        int realResult = Arrays.findMax(array);
+        check(realResult, expectedResult, "FindMaxNumber");
+    }
+
+    public void shouldFindMinNumber() {
+        int[] array = {150, 315, 520, 700, 810, 920, 1000, 1240};
+        int expectedResult = 150;
+        int realResult = Arrays.findMin(array);
+        check(realResult, expectedResult, "FindMinNumber");
+    }
+
+
+    public void check(int expectedResult, int realResult, String testName) {
+        if (realResult == expectedResult) {
+            System.out.println(testName + " test has passed!");
+        }
+        else {
+            System.out.println(testName + " test has failed!");
+        }
+    }
+
+
+
+}
