de8fdf945cd90723e975c565b6d7f745b401abbc
Julija Katukova <julija.katukova@gmail.com>
2021-02-22 10:10:23 +0200

    Lesson5 Level2 Task15

--- src/main/java/students/julija_katukova/lesson_5/level_2/task_15/Task15.java
+++ src/main/java/students/julija_katukova/lesson_5/level_2/task_15/Task15.java
@@ -7,13 +7,13 @@ class Task15 {
     public static void main(String[] args) {
         int[] numbers = new int[3]; //создайте массив целых чисел длиной 3
         Random random = new Random();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             numbers[i] = random.nextInt(); //заполните каждую ячейку массива случайным числом
         }
         System.out.println(Arrays.toString(numbers));//выведите значение каждой ячейки на консоль
 
-        for (int i = 0; i < 3; i++) {
-            numbers[i] = numbers[i] * 2; //увеличьте значение каждой ячейки на 2
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = numbers[i] + 2; //увеличьте значение каждой ячейки на 2
         }
         System.out.println(Arrays.toString(numbers));//выведите значение каждой ячейки на консоль
     }
