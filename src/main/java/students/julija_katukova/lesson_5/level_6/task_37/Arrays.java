8be1dbc6eb376bfa449f0ebaa8da942989bce857
Julija Katukova <julija.katukova@gmail.com>
2021-02-23 18:53:53 +0200

    Lesson5 Level6 Task37

--- src/main/java/students/julija_katukova/lesson_5/level_6/task_37/Arrays.java
+++ src/main/java/students/julija_katukova/lesson_5/level_6/task_37/Arrays.java
@@ -2,6 +2,12 @@ package students.julija_katukova.lesson_5.level_6.task_37;
 
 class Arrays {
     public int findMin(int[] array) {
-        return java.util.Arrays.stream(array).min().getAsInt();
+        int min = array[0];
+        for (int j : array) {
+            if (j < min) {
+                min = j;
+            }
+        }
+        return min;
     }
 }
