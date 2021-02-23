e0efaf9f75509b7c68d84a640dc796e2cd41712a
RodionA <raf91@inbox.lv>
2021-02-23 21:54:26 +0200

    Lesson-5-level-4-task-27

---
+++ src/main/java/students/rodion_afanasjev/lesson_2/level_5/task_16/Hotkeys.java
@@ -0,0 +1,9 @@
+package students.rodion_afanasjev.lesson_2.level_5.task_16;
+
+class Hotkeys {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
--- src/main/java/students/rodion_afanasjev/lesson_5/level_4/task_28/ArrayMin.java
+++ src/main/java/students/rodion_afanasjev/lesson_5/level_4/task_28/ArrayMin.java
@@ -9,12 +9,22 @@ class ArrayMin {
 
         Random rand = new Random();
         int[] numbers = new int[10];
-        for ( int i = 0; i< numbers.length; i++){
+
+
+        for (int i = 0; i < numbers.length; i++) {
             numbers[i] = rand.nextInt(10);
         }
 
+        int min = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
         System.out.println(Arrays.toString(numbers));
-        System.out.println(Arrays.stream(numbers).min().getAsInt());
+        System.out.println(min);
+
 
     }
 
