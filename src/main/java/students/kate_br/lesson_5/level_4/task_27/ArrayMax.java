b636ff9c57c1554598cba221e3d7b092ddbadeac
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-23 19:36:24 +0200

    Lesson 5. Task 27

--- src/main/java/students/kate_br/lesson_5/level_4/task_27/ArrayMax.java
+++ src/main/java/students/kate_br/lesson_5/level_4/task_27/ArrayMax.java
@@ -1,5 +1,6 @@
 package students.kate_br.lesson_5.level_4.task_27;
 
+
 import java.util.Arrays;
 import java.util.Random;
 
@@ -9,12 +10,20 @@ class ArrayMax {
          int[] num = new int[10];
          Random random = new Random();
 
-         for(int i = 0; i < 10; i++){
+         for(int i = 0; i < num.length; i++){
              num[i] = random.nextInt(11);
          }
 
+         int max = num[0];
+         for (int i = 0; i < num.length; i++){
+             if (num[i] > max){
+                 max = num[i];
+             }
+
+         }
+
          System.out.println(Arrays.toString(num));
-         System.out.print("Max number is: ");
-         System.out.println(Arrays.stream(num).max().getAsInt());
+         System.out.println("Max num is: " + max);
+
      }
 }
