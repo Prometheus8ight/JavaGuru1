4fc65492b8ad3b1f48454921b76913e497ae3813
CoArchie <c.chikun@icloud.com>
2021-02-23 23:49:00 +0200

    Lesson 5 - task [ 16,17,18,19,20,21,22,23,24 ]

---
+++ src/main/java/students/arkadij/lesson_5/task_16/ForLoopDefinition4.java
@@ -0,0 +1,11 @@
+package students.arkadij.lesson_5.task_16;
+
+class ForLoopDefinition4 { // task 19
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){ // was --> i < 10; i--
+            System.out.println(i);
+        }
+    }
+
+}
