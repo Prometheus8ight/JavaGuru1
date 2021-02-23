9468ffd8018940d8332730403e035c2041e18f22
RodionA <raf91@inbox.lv>
2021-02-23 22:06:50 +0200

    Lesson-5-level-4-task-28

--- src/main/java/students/rodion_afanasjev/lesson_5/level_4/task_28/ArrayMin.java
+++ src/main/java/students/rodion_afanasjev/lesson_5/level_4/task_28/ArrayMin.java
@@ -16,9 +16,9 @@ class ArrayMin {
         }
 
         int min = numbers[0];
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] < min) {
-                min = numbers[i];
+        for (int number : numbers) {
+            if (number < min) {
+                min = number;
             }
         }
 
