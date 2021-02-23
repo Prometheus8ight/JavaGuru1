7b534e3faeff26f95ee828fe1c452ebe0e13ea73
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-23 20:18:34 +0200

    Lesson 5. Task 10

--- src/main/java/students/kate_br/lesson_5/level_2/task_10/ArrayOf3Numbers.java
+++ src/main/java/students/kate_br/lesson_5/level_2/task_10/ArrayOf3Numbers.java
@@ -1,14 +1,17 @@
 package students.kate_br.lesson_5.level_2.task_10;
 
 import java.util.Arrays;
+import java.util.Random;
 
 class ArrayOf3Numbers {
 
      public static void main(String[] args) {
          int[] numbers = new int[3];
-         numbers[0] = 1;
-         numbers[1] = 2;
-         numbers[2] = 3;
+         Random random = new Random();
+
+         for (int i = 0; i < numbers.length; i++){
+             numbers[i] = random.nextInt(4);
+         }
 
          System.out.println(Arrays.toString(numbers));
 
