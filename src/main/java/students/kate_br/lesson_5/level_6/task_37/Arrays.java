db628f476c3f94efd3c4b82da4c0117019deb99c
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-23 21:12:34 +0200

    Lesson 5. Task 37

--- src/main/java/students/kate_br/lesson_5/level_6/task_37/Arrays.java
+++ src/main/java/students/kate_br/lesson_5/level_6/task_37/Arrays.java
@@ -3,7 +3,7 @@ package students.kate_br.lesson_5.level_6.task_37;
  class Arrays {
 
      public int findMin(int[] array){
-         int min = array.length;
+         int min = array[0];
 
          for(int num : array){
              if (num < min){
