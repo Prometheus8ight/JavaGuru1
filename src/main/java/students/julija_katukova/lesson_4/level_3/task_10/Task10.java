435935cca22eedcc0eefb64962863410fd0c5b7c
Julija Katukova <julija.katukova@gmail.com>
2021-02-23 19:45:08 +0200

    Lesson4 Level3 Task10

--- src/main/java/students/julija_katukova/lesson_4/level_3/task_10/Task10.java
+++ src/main/java/students/julija_katukova/lesson_4/level_3/task_10/Task10.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 class Task10 {
     public static void main(String[] args) {
+        Task10 task10 = new Task10();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter first integer number:");
         int num1 = scanner.nextInt();
@@ -12,11 +13,9 @@ class Task10 {
         System.out.println("Please enter third integer number:");
         int num3 = scanner.nextInt();
         // 1,2,3   1,3,2   2,1,3   2,3,1   3,1,2   3,2,1
-
-        System.out.println(max3(num1, num2, num3));
-
+        System.out.println(task10.max3(num1,num2,num3));
     }
-    public static int max2(int num1, int num2) {
+    public int max2(int num1, int num2) {
         if (num1 >= num2) {
             return num1;
         }
@@ -24,8 +23,8 @@ class Task10 {
             return num2;
         }
     }
-    public static int max3 (int num1, int num2, int num3) {
-        int max2 = Task10.max2(num1, num2);
+    public int max3 (int num1, int num2, int num3) {
+        int max2 = max2(num1, num2);
         if (max2 >= num3) {
             return max2;
         }
@@ -33,5 +32,4 @@ class Task10 {
             return num3;
         }
     }
-
 }
