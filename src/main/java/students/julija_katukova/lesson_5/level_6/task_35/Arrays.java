b3fa4c9b0689916986e684714ddcfd51926fb029
Julija Katukova <julija.katukova@gmail.com>
2021-02-23 18:40:33 +0200

    Lesson5 Level6 Task35

--- src/main/java/students/julija_katukova/lesson_5/level_6/task_35/Arrays.java
+++ src/main/java/students/julija_katukova/lesson_5/level_6/task_35/Arrays.java
@@ -2,7 +2,12 @@ package students.julija_katukova.lesson_5.level_6.task_35;
 
 class Arrays {
     public int findMax(int[] array) {
-        int max = java.util.Arrays.stream(array).max().getAsInt();
+        int max = array[0];
+        for (int j : array) {
+            if (j > max) {
+                max = j;
+            }
+        }
         return max;
     }
 }
