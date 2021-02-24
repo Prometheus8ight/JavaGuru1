99a04411556b3f9e14e34e0f1a019a034e2b0ec9
Prometheus <pointofview88@gmail.com>
2021-02-24 16:36:27 +0300

    Lesson 5, level 7, task 41 corrected.

--- src/main/java/students/vitaly_porsev/lesson_5/level_7/task_41/Array.java
+++ src/main/java/students/vitaly_porsev/lesson_5/level_7/task_41/Array.java
@@ -1,34 +1,60 @@
 package students.vitaly_porsev.lesson_5.level_7.task_41;
 
+import java.util.Locale;
 import java.util.Random;
 import java.util.Arrays;
+import java.util.Scanner;
 
 class Array {
 
+    void userArray() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите длину массива: ");
+        int[] array = makeArray(scanner.nextInt());
+        System.out.println("Это ваш массив: " + Arrays.toString(array));
+        keepWorking(array);
+        scanner.close();
+    }
+
+    void keepWorking(int[] array) {
+        sortArray(array);
+    }
+
+    int[] makeArray(int arrayLength) {
+        int[] array = new int[arrayLength];
+        fillArray(array);
+        return array;
+    }
+
     void fillArray(int[] array) {
         Random random = new Random();
-        Arrays.fill(array, random.nextInt(100));
-    }
-
-    void sortArray(int[] array){
-        Arrays.sort(array);
-    }
-
-    void searchIndex(int[] array, int num){
-        System.out.println("Индекс: " + Arrays.binarySearch(array, num));
-    }
-
-    void equalArrays(int[] array1, int[] array2){
-        boolean trueOrNot = Arrays.equals(array1, array2);
-        if(trueOrNot){
-            System.out.println("Arrays are equal!");
-        }else {
-            System.out.println("Arrays are not equal!");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
         }
+
     }
 
-    void copyRangeArray(int[] array, int rangeFrom, int rangeTo){
-        System.out.println(Arrays.toString(Arrays.copyOfRange(array, rangeFrom, rangeTo)));
+    void sortArray(int[] array) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Хотите отсортировать ваш массив по возрастанию? Введите \"да\" или \"нет\".");
+        if (scanner.next().toLowerCase(Locale.ROOT).equals("да")) {
+            Arrays.sort(array);
+            System.out.println("Теперь ваш массив выглядит так: " + Arrays.toString(array));
+            searchIndex(array);
+        } else {
+            System.out.println("Всего хорошего!");
+        }scanner.close();
+    }
+
+    void searchIndex(int[] array) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Желаете найти под каким индексом находится ваше число? Введите \"да\" или \"нет\".");
+        if (scanner.next().toLowerCase(Locale.ROOT).equals("да")) {
+            System.out.println("Введите ваше число: ");
+            System.out.println("Число находится под индексом: " + Arrays.binarySearch(array, scanner.nextInt()));
+        } else {
+            System.out.println("Всего хорошего!");
+        }
     }
 
 }
--- src/main/java/students/vitaly_porsev/lesson_5/level_7/task_41/ArrayDemo.java
+++ src/main/java/students/vitaly_porsev/lesson_5/level_7/task_41/ArrayDemo.java
@@ -5,36 +5,9 @@ import java.util.Arrays;
 class ArrayDemo {
 
     public static void main(String[] args) {
-        Array methods = new Array();
-        int[] array = {10, 5, 99, 34, 89, 31, 1, 69};
-        int[] array2 = {10, 5, 99, 34, 89, 31, 1, 69};
+     Array methods = new Array();
+        methods.userArray();
 
-        System.out.println(Arrays.toString(array));
-        System.out.println(Arrays.toString(array2));
-
-        methods.equalArrays(array, array2);
-
-        System.out.println();
-
-        methods.sortArray(array);
-        System.out.println(Arrays.toString(array));
-        System.out.println(Arrays.toString(array2));
-
-        System.out.println();
-
-        methods.searchIndex(array, 89);
-        System.out.println();
-
-
-        methods.fillArray(array);
-        System.out.println(Arrays.toString(array));
-        System.out.println(Arrays.toString(array2));
-
-        methods.equalArrays(array, array2);
-
-        System.out.println();
-
-        methods.copyRangeArray(array2,3,6);
     }
 
 }
