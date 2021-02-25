776133e268bb4239bec578cb63a422ab67c97b22
juka2506 <julija.katukova@gmail.com>
2021-02-25 16:34:51 +0200

    Lesson6 Level3 Task12

--- src/main/java/students/julija_katukova/lesson_6/level_3/task_12/ArrayService.java
+++ src/main/java/students/julija_katukova/lesson_6/level_3/task_12/ArrayService.java
@@ -1,7 +1,5 @@
 package students.julija_katukova.lesson_6.level_3.task_12;
 
-import java.util.Random;
-
 class ArrayService {
     public boolean contains(int[] arr, int numberToSearch) {
         boolean check = false;
