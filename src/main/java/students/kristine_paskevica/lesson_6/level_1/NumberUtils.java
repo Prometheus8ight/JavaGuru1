a6177e5705835dc3736f0fbdbdb9be4b6bbaecb8
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-02-25 19:22:27 +0200

    lesson6-level1-AllTask

---
+++ src/main/java/students/kristine_paskevica/lesson_6/level_1/NumberUtils.java
@@ -0,0 +1,10 @@
+package students.kristine_paskevica.lesson_6.level_1;
+
+class NumberUtils {
+
+    public boolean isEven(int number) {
+
+        return number % 2 == 0;
+    }
+}
+
---
+++ src/main/java/students/kristine_paskevica/lesson_6/level_1/WhileDefinition1.java
@@ -0,0 +1,12 @@
+package students.kristine_paskevica.lesson_6.level_1;
+
+class WhileDefinition1 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {             //nebija iekavas
+            System.out.println(i);
+            i++;
+        }
+    }
+}
