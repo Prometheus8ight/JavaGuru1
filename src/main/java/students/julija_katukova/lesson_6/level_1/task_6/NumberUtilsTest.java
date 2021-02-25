c4de051797c597895aef9b2b36bf85cdf579d3f3
juka2506 <julija.katukova@gmail.com>
2021-02-25 11:18:51 +0200

    Lesson6 Level1 Task6

---
+++ src/main/java/students/julija_katukova/lesson_6/level_1/task_6/NumberUtilsTest.java
@@ -0,0 +1,39 @@
+package students.julija_katukova.lesson_6.level_1.task_6;
+
+class NumberUtilsTest {
+    public static void main(String[] args) {
+        NumberUtilsTest numberUtilsTest = new NumberUtilsTest();
+        numberUtilsTest.evenTest(20);
+        numberUtilsTest.oddTest(17);
+        numberUtilsTest.zeroTest(0);
+    }
+
+    public void evenTest(int number) {
+        NumberUtils numberUtils = new NumberUtils();
+        boolean realResult = numberUtils.isEven(number);
+        check(realResult, true, "Even Test");
+    }
+
+    public void oddTest(int number) {
+        NumberUtils numberUtils = new NumberUtils();
+        boolean realResult = numberUtils.isEven(number);
+        check(realResult, false, "Odd Test");
+    }
+
+    public void zeroTest(int number) {
+        NumberUtils numberUtils = new NumberUtils();
+        boolean realResult = numberUtils.isEven(number);
+        check(realResult, true, "Zero Test");
+    }
+
+
+    public void check (boolean realResult, boolean expectedResult, String testName) {
+        if(realResult == expectedResult) {
+            System.out.println(testName + " = OK");
+        }
+        else {
+            System.out.println(testName + " = FAIL");
+        }
+    }
+
+}
