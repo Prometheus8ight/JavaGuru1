9667beb31c22a841dcb5ee5c2b4c1509f22c3b45
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-25 13:15:49 +0200

    Lesson 6. Task 5

---
+++ src/main/java/students/kate_br/lesson_6/level_1/task_5/WhileDefinition5.java
@@ -0,0 +1,14 @@
+package students.kate_br.lesson_6.level_1.task_5;
+
+ class WhileDefinition5 {
+
+     public static void main(String[] args) {
+
+         int i = 1;
+
+         while (i <= 100){
+             System.out.println(i);
+             i++;
+         }
+     }
+}
