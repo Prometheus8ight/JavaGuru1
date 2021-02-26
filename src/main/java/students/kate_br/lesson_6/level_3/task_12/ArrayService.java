de337c43bbb981cab52e7e45bad0d4ca7a3219ea
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-26 13:22:55 +0200

    Lesson 6. Task 12

---
+++ src/main/java/students/kate_br/lesson_6/level_3/task_12/ArrayService.java
@@ -0,0 +1,13 @@
+package students.kate_br.lesson_6.level_3.task_12;
+
+class ArrayService {
+
+    boolean contains(int[] arr, int numberToSearch) {
+
+        for (int i = 0; i < arr.length; i++) {
+            return true;
+        }
+        return false;
+
+    }
+}
