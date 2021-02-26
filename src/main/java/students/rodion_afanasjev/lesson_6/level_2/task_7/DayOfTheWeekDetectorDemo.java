3db10ff569f7830575a717d61bdf3b4915365e20
RodionA <raf91@inbox.lv>
2021-02-26 12:38:59 +0200

    Lesson-6-level-1-task-7

---
+++ src/main/java/students/rodion_afanasjev/lesson_6/level_2/task_7/DayOfTheWeekDetectorDemo.java
@@ -0,0 +1,14 @@
+package students.rodion_afanasjev.lesson_6.level_2.task_7;
+
+import java.util.Scanner;
+
+class DayOfTheWeekDetectorDemo {
+
+    public static void main(String[] args) {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        int dayNumber = detector.getDayNumberFromUser();
+        String dayOfTheWeek = detector.findDayOfTheWeek(dayNumber);
+        System.out.println(dayOfTheWeek);
+    }
+
+}
