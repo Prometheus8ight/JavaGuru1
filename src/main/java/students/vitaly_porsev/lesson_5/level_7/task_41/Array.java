3cf95d2c7ac59c320163e10af9b231a555cf037a
Prometheus <pointofview88@gmail.com>
2021-02-26 20:57:47 +0300

    Lesson 5, level 7, task 17 corrected N2)

--- src/main/java/students/vitaly_porsev/lesson_5/level_7/task_41/Array.java
+++ src/main/java/students/vitaly_porsev/lesson_5/level_7/task_41/Array.java
@@ -36,25 +36,53 @@ class Array {
 
     void sortArray(int[] array) {
         Scanner scanner = new Scanner(System.in);
+        Array method = new Array();
         System.out.println("Хотите отсортировать ваш массив по возрастанию? Введите \"да\" или \"нет\".");
         if (scanner.next().toLowerCase(Locale.ROOT).equals("да")) {
-            Arrays.sort(array);
+            method.sort(array);
             System.out.println("Теперь ваш массив выглядит так: " + Arrays.toString(array));
             searchIndex(array);
         } else {
             System.out.println("Всего хорошего!");
-        }scanner.close();
+        }
+        scanner.close();
     }
 
     void searchIndex(int[] array) {
         Scanner scanner = new Scanner(System.in);
+        Array method = new Array();
         System.out.println("Желаете найти под каким индексом находится ваше число? Введите \"да\" или \"нет\".");
         if (scanner.next().toLowerCase(Locale.ROOT).equals("да")) {
             System.out.println("Введите ваше число: ");
-            System.out.println("Число находится под индексом: " + Arrays.binarySearch(array, scanner.nextInt()));
+            int index = method.searchIndex(array, scanner.nextInt());
+            System.out.println("Число находится под индексом: " + index);
         } else {
             System.out.println("Всего хорошего!");
         }
+    }
+
+    void sort(int[] array) {
+        int temp;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    int searchIndex(int[] array, int searchNumber) {
+        int index = 0;
+        for (int i : array) {
+            if (i == searchNumber) {
+                break;
+            }
+            index++;
+        }
+        return index;
     }
 
 }
--- src/main/java/students/vitaly_porsev/lesson_5/level_7/task_41/ArrayDemo.java
+++ src/main/java/students/vitaly_porsev/lesson_5/level_7/task_41/ArrayDemo.java
@@ -1,14 +1,11 @@
 package students.vitaly_porsev.lesson_5.level_7.task_41;
 
-import java.util.Arrays;
-
 class ArrayDemo {
 
     public static void main(String[] args) {
-     Array methods = new Array();
+        Array methods = new Array();
         methods.userArray();
 
     }
-
 }
 
