86e4abc8f658684e5d1145f153acdac9a61a92c3
juka2506 <julija.katukova@gmail.com>
2021-02-26 16:00:20 +0200

    Lesson6 Level4 Task18

---
+++ src/main/java/students/julija_katukova/lesson_6/level_4/task_18/ForInfiniteLoop.java
@@ -0,0 +1,9 @@
+package students.julija_katukova.lesson_6.level_4.task_18;
+
+class ForInfiniteLoop {
+    public static void main(String[] args) {
+        for (;;) {
+            System.out.println("Hello, Konstantin!");
+        }
+    }
+}
