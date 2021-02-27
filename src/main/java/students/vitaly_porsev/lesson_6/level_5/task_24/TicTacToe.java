2d75e7c04a6e0cdf55d0e470dfdcfa4b66f867d7
Prometheus <pointofview88@gmail.com>
2021-02-27 15:12:40 +0300

    Lesson 6, level 5, task 24.

---
+++ src/main/java/students/vitaly_porsev/lesson_6/level_5/task_24/TicTacToe.java
@@ -0,0 +1,13 @@
+package students.vitaly_porsev.lesson_6.level_5.task_24;
+
+class TicTacToe {
+
+    public boolean isWinPositionForDiagonals(int[][] field, int playerToCheck){
+        if(field[0][0] == playerToCheck && field[1][1] == playerToCheck
+                && field[2][2] == playerToCheck){
+            return true;
+        }
+        return field[0][2] == playerToCheck && field[1][1] == playerToCheck
+                && field[2][0] == playerToCheck;
+    }
+}
