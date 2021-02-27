8c4303fc4c731465ac7d024c11a4ce4d2736d131
RodionA <raf91@inbox.lv>
2021-02-27 15:34:17 +0200

    Lesson-6-level-4-task-18

---
+++ src/main/java/students/rodion_afanasjev/lesson_6/level_4/task_18/ForInfiniteLoop.java
@@ -0,0 +1,13 @@
+package students.rodion_afanasjev.lesson_6.level_4.task_18;
+
+class ForInfiniteLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i > 0; i++) {
+            System.out.println("Endless for loop");
+        }
+
+    }
+
+}
