1dcc12d477c9269c6df1de84ce415800c392942f
Prometheus <pointofview88@gmail.com>
2021-02-27 10:56:26 +0300

    Lesson 6, level 4, task 18.

---
+++ src/main/java/students/vitaly_porsev/lesson_6/level_4/task_18/WhileInfiniteLoop.java
@@ -0,0 +1,16 @@
+package students.vitaly_porsev.lesson_6.level_4.task_18;
+
+class WhileInfiniteLoop {
+
+    void infiniteWhile(){
+        while(true){
+            System.out.println("Hello!");
+        }
+    }
+
+    void infiniteFor(){
+        for(;;) {
+            System.out.println("Bye!");
+        }
+    }
+}
