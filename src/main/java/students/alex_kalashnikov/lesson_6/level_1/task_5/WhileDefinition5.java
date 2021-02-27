acf86fa41210c0bef350f5a9939f93405313ec95
alex <kalashnikov_alex@yahoo.com>
2021-02-27 15:40:45 +0200

    Homework; Lesson 6; Level 1; Tasks 1,2,3,4,5,6.

---
+++ src/main/java/students/alex_kalashnikov/lesson_6/level_1/task_5/WhileDefinition5.java
@@ -0,0 +1,13 @@
+package students.alex_kalashnikov.lesson_6.level_1.task_5;
+
+class WhileDefinition5 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {                      // поменять > на <
+            System.out.println(i);
+            i++;                                // неверный инкремент, исправить на ++
+        }
+    }
+
+}
