5941d0ba6ef546e0fc85c5603653611dbb03e795
juka2506 <julija.katukova@gmail.com>
2021-02-27 17:09:15 +0200

    Lesson6 Level5 Task23

---
+++ src/main/java/students/julija_katukova/lesson_6/level_5/task_23/TicTacToe.java
@@ -0,0 +1,23 @@
+package students.julija_katukova.lesson_6.level_5.task_23;
+
+class TicTacToe {
+    public boolean isWinPositionForVerticals(int[][] field, int playerToCheck) {
+        TicTacToe ticTacToe = new TicTacToe();
+        return ticTacToe.checkForVerticals(field, playerToCheck);
+    }
+
+    public boolean checkForVerticals(int[][] field, int playerToCheck) {
+        for (int i = 0; i < field.length; i++) {
+            int count = 0;
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[j][i] == playerToCheck) {
+                    count++;
+                }
+                if (count == field[i].length) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
