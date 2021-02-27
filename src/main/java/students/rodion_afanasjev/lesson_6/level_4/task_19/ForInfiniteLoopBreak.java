f38e64a6ec3934c6cf4ebd81270b6613a396de11
RodionA <raf91@inbox.lv>
2021-02-27 18:31:49 +0200

    Lesson-6-level-4-task-19

---
+++ src/main/java/students/rodion_afanasjev/lesson_6/level_4/task_19/ForInfiniteLoopBreak.java
@@ -0,0 +1,14 @@
+package students.rodion_afanasjev.lesson_6.level_4.task_19;
+
+class ForInfiniteLoopBreak {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i > 0; i++) {
+            System.out.println("Endless for loop");
+            break;
+        }
+
+    }
+
+}
