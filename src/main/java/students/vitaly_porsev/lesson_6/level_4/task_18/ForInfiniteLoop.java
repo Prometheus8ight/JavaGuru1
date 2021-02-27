098aafd4c267091d8f53bffe5950e2a0261c5544
Prometheus <pointofview88@gmail.com>
2021-02-27 11:35:42 +0300

    Lesson 6, level 4, task 18 corrected.

---
+++ src/main/java/students/vitaly_porsev/lesson_6/level_4/task_18/ForInfiniteLoop.java
@@ -0,0 +1,10 @@
+package students.vitaly_porsev.lesson_6.level_4.task_18;
+
+class ForInfiniteLoop {
+
+    void infiniteFor() {
+        for (; ; ) {
+            System.out.println("Bye!");
+        }
+    }
+}
