f5c4d81d1b3952f35ab92d7c3ca12412e82f172a
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-28 11:13:52 +0200

    Lesson 6. Task 15

--- src/main/java/students/kate_br/lesson_6/level_3/task_15/ArrayService.java
+++ src/main/java/students/kate_br/lesson_6/level_3/task_15/ArrayService.java
@@ -7,18 +7,18 @@ class ArrayService {
         int count = 0;
 
 
-       for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
 
-           if (arr[i] == numberToReplace){
-               arr[i] = newNumber;
-              count++;
-           }
-           }
+            if (arr[i] == numberToReplace) {
+                arr[i] = newNumber;
+                count++;
+            }
+        }
 
-       return count;
-
-       }
+        return count;
 
     }
+
+}
 
 
