ab3ff5648ecdcf9ecb49e79e10a607b6ea5e1379
juka2506 <julija.katukova@gmail.com>
2021-02-28 18:16:19 +0200

    Lesson6 Level6 Task30

---
+++ src/main/java/students/julija_katukova/lesson_6/level_5/task_30/Move.java
@@ -0,0 +1,19 @@
+package students.julija_katukova.lesson_6.level_5.task_30;
+
+class Move {
+    private int x;
+    private int y;
+
+    Move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+}
