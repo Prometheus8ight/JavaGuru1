2287050cc0c3c6dcd5e41998377b316752bada42
Prometheus <pointofview88@gmail.com>
2021-02-28 19:10:27 +0300

    Lesson 6, level 6, task 30.

---
+++ src/main/java/students/vitaly_porsev/lesson_6/level_6/task_30/TicTacToeDemo.java
@@ -0,0 +1,8 @@
+package students.vitaly_porsev.lesson_6.level_6.task_30;
+
+class TicTacToeDemo {
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.play();
+    }
+}
