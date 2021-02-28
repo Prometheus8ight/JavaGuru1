f69d897cffeaa8490acc03a5b4567d13bc574335
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-02-28 12:24:50 +0100

    Bogdans Pavlovs - Lesson 6 - Level 2 - Tasks 7 to 11

---
+++ src/main/java/students/bogdans_pavlovs/lesson_6/level_2/DayOfTheWeekDetector.java
@@ -0,0 +1,31 @@
+package students.bogdans_pavlovs.lesson_6.level_2;
+
+
+import java.util.Scanner;
+
+class DayOfTheWeekDetector {
+
+    public int getDayNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input day number between 1 and 7 : ");
+        return scanner.nextInt();
+    }
+
+    public String findDayOfTheWeek(int dayNumber) {
+
+        String weekDay;
+
+        switch (dayNumber) {
+            case 1 -> weekDay = "Monday";
+            case 2 -> weekDay = "Tuesday";
+            case 3 -> weekDay = "Wednesday";
+            case 4 -> weekDay = "Thursday";
+            case 5 -> weekDay = "Friday";
+            case 6 -> weekDay = "Saturday";
+            case 7 -> weekDay = "Sunday";
+            default -> weekDay = "Not correct day number";
+        }
+
+        return weekDay;
+    }
+}
