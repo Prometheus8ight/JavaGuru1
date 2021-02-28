9d0306da7a0ec094bf709830955362d28375aad0
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-28 11:30:12 +0200

    Lesson 5. Task 36

--- src/main/java/students/kate_br/lesson_5/level_6/task_36/ArraysTest.java
+++ src/main/java/students/kate_br/lesson_5/level_6/task_36/ArraysTest.java
@@ -1,44 +1,44 @@
 package students.kate_br.lesson_5.level_6.task_36;
 
- class ArraysTest {
+class ArraysTest {
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
 
-         ArraysTest test = new ArraysTest();
-         test.shouldCreateArray();
-         test.shouldFindMaxNumber();
-     }
-
-
-     public void shouldCreateArray(){
-
-         int expectedResult = 10;
-         Arrays arrays = new Arrays();
-         int[] num = arrays.create(10);
-         int realResult = num.length;
-
-         if (expectedResult == realResult){
-             System.out.println("Test OK");
-         }else {
-             System.out.println("Test FAIL");
-         }
-     }
-
-     public void shouldFindMaxNumber(){
-
-         int expectedResult = 15;
-         int[] array = { 5, 7, 15,};
-         Arrays arrays = new Arrays();
-         int realResult = arrays.findMax(array);
-
-         if (expectedResult == realResult){
-             System.out.println("Test OK");
-         }else {
-             System.out.println("Test FAIL");
-         }
+        ArraysTest test = new ArraysTest();
+        test.shouldCreateArray();
+        test.shouldFindMaxNumber();
+    }
 
 
-     }
+    public void shouldCreateArray() {
+
+        int expectedResult = 10;
+        Arrays arrays = new Arrays();
+        int[] num = arrays.create(10);
+        int realResult = num.length;
+
+        if (expectedResult == realResult) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Test FAIL");
+        }
+    }
+
+    public void shouldFindMaxNumber() {
+
+        int expectedResult = 15;
+        int[] array = {5, 7, 15,};
+        Arrays arrays = new Arrays();
+        int realResult = arrays.findMax(array);
+
+        if (expectedResult == realResult) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Test FAIL");
+        }
+
+
+    }
 
 
 }
