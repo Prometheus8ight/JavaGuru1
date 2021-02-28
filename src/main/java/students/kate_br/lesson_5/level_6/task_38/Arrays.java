7f32c006f3bed0bd2ce0d612e980e833010ecafc
KateBristol <jekaterina.telezkina@gmail.com>
2021-02-28 11:31:18 +0200

    Lesson 5. Task 38

--- src/main/java/students/kate_br/lesson_5/level_6/task_38/Arrays.java
+++ src/main/java/students/kate_br/lesson_5/level_6/task_38/Arrays.java
@@ -1,31 +1,31 @@
 package students.kate_br.lesson_5.level_6.task_38;
 
- class Arrays {
+class Arrays {
 
-     public int[] create(int length){
-         return new int[length];
-     }
+    public int[] create(int length) {
+        return new int[length];
+    }
 
-     public int findMax(int[] array){
-          int max = array.length;
+    public int findMax(int[] array) {
+        int max = array[0];
 
-          for(int num : array){
-              if(num > max){
-                  max = num;
-              }
-          }
-          return max;
-     }
+        for (int num : array) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
 
-     public int findMin(int[] array){
-         int min = array[0];
+    public int findMin(int[] array) {
+        int min = array[0];
 
-         for(int num : array){
-             if(num < min){
-                 min = num;
-             }
-         }
+        for (int num : array) {
+            if (num < min) {
+                min = num;
+            }
+        }
 
-         return min;
-     }
+        return min;
+    }
 }
