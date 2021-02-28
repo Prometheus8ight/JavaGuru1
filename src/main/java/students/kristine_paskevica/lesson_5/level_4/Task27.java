105865fdf673c5e135b0a19068561f371823f5a1
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-02-28 16:36:27 +0200

    lesson6-level2-7Task

--- src/main/java/students/kristine_paskevica/lesson_5/level_4/Task27.java
+++ src/main/java/students/kristine_paskevica/lesson_5/level_4/Task27.java
@@ -1,22 +1,29 @@
 package students.kristine_paskevica.lesson_5.level_4;
 
-import java.util.Arrays;
 import java.util.Random;
 
 class Task27 {
 
     public static void main(String[] args) {
 
-    int [] masivs = new int[6];
+        int[] masivs = new int[6];                     //izveido masīvu ar 6 elementiem
+        int max = masivs[0];
 
-    Random genereSkaitlus = new Random();
+        Random genereSkaitlus = new Random();
 
-         for (int i = 0; i < 6; i ++){
+        for (int i = 0; i < masivs.length; i++) {       //aizpildam masīvu ar nejaušiem sk
             masivs[i] = genereSkaitlus.nextInt();
+        }
+        System.out.println("Masīva šūnās glabājas šādas vērtības: ");   //izdrukā visus masīva elementus
+        for (int i = 0; i < masivs.length; i++) {
+            System.out.println(masivs[i]);
+
+            if (masivs[i] > max) {                      //atrodam max masīva vērtību
+                max = masivs[i];
+            }
+        }
+        System.out.println("Vislielākais no masīvā esošiem skaitļiem ir: " + max);
     }
-         System.out.println("Masīva šūnās glabājas šādas vērtības: " + Arrays.toString(masivs));
-         System.out.println("Vislielākais no masīvā esošiem skaitļiem ir: " + Arrays.stream(masivs).max().getAsInt());
-}
 }
 
 
