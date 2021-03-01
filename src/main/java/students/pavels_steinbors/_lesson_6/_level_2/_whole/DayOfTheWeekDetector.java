ae1a2f199fdccfb86dca509a9650a54507660ccc
PavStein <pav210985@gmail.com>
2021-03-01 14:22:34 +0100

    Lesson 6 level 2 task 7 8 9 10 11

---
+++ src/main/java/students/pavels_steinbors/_lesson_6/_level_2/_whole/DayOfTheWeekDetector.java
@@ -0,0 +1,30 @@
+package students.pavels_steinbors._lesson_6._level_2._whole;
+
+import java.util.Scanner;
+
+class DayOfTheWeekDetector {
+
+    public int getDayNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input day number between 1 and 7 : ");
+        return scanner.nextInt();
+    }
+
+    public String findDayOfTheWeek(int dayNumber) {
+        String day;
+
+        switch (dayNumber) {
+            case 1 -> day = "Monday";
+            case 2 -> day = "Tuesday";
+            case 3 -> day = "Wednesday";
+            case 4 -> day = "Thursday";
+            case 5 -> day = "Friday";
+            case 6 -> day = "Saturday";
+            case 7 -> day = "Sunday";
+            default -> day = "Not correct day number";
+        }
+        return day;
+    }
+
+}
+
---
+++ src/main/java/students/pavels_steinbors/_lesson_6/_level_2/_whole/DayOfTheWeekDetectorDemo.java
@@ -0,0 +1,13 @@
+package students.pavels_steinbors._lesson_6._level_2._whole;
+
+class DayOfTheWeekDetectorDemo {
+
+    public static void main(String[] args) {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        int dayNumber = detector.getDayNumberFromUser();
+        String dayOfTheWeek = detector.findDayOfTheWeek(dayNumber);
+        System.out.println(dayOfTheWeek);
+
+    }
+
+}
