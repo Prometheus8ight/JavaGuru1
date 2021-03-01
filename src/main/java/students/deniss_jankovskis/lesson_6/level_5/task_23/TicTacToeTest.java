f0749e9571c987d90e748f069b86fba80e426040
DJ9780772 <denissjankovskis@outlook.com>
2021-03-01 19:06:02 +0200

    Level 5, Task 23

--- src/main/java/students/deniss_jankovskis/lesson_6/level_5/task_23/TicTacToeTest.java
+++ src/main/java/students/deniss_jankovskis/lesson_6/level_5/task_23/TicTacToeTest.java
@@ -5,21 +5,35 @@ class TicTacToeTest {
     public static void main(String[] args) {
 
         TicTacToeTest test = new TicTacToeTest();
-        test.winHorizontalTest(new int[][]{{1, 0, 1}, {1, 1, 0}, {0, 0, 0}}, 0);
-        test.lostHorizontalTest(new int[][]{{1, 0, 0}, {-1, 1, 1}, {1, 0, 1}}, 1);
+        test.winHorizontalTest1(new int[][]{{1, 0, 1}, {1, 1, 0}, {0, 0, 0}}, 0);
+        test.winHorizontalTest2(new int[][]{{0, 1, -1}, {-1, -1, 0}, {1, 0, 0}}, 1);
+        test.lostHorizontalTest(new int[][]{{1, 0, 0}, {-1, 1, 1}, {1, 0, 1}}, 0);
+        test.lostHorizontalTest2(new int[][]{{-1, 1, 0}, {1, 0, -1}, {1, 1, 1}}, 1);
 
     }
 
-    public void winHorizontalTest(int[][] field, int playerToCheck) {
+    public void winHorizontalTest1(int[][] field, int playerToCheck) {
         TicTacToe ticTacToe = new TicTacToe();
         boolean realResult = ticTacToe.checkWinPositionForVerticals(field, playerToCheck);
         checkTestResult(realResult, "Horizontal Test Win");
+    }
+
+    public void winHorizontalTest2(int[][] field, int playerToCheck) {
+        TicTacToe ticTacToe = new TicTacToe();
+        boolean realResult = ticTacToe.checkWinPositionForVerticals(field, playerToCheck);
+        checkTestResult(realResult, "Horizontal Test Win 2");
     }
 
     public void lostHorizontalTest(int[][] field, int playerToCheck) {
         TicTacToe ticTacToe = new TicTacToe();
         boolean realResult = ticTacToe.checkWinPositionForVerticals(field, playerToCheck);
         checkTestResult(realResult, "Horizontal Test Lost");
+    }
+
+    public void lostHorizontalTest2(int[][] field, int playerToCheck) {
+        TicTacToe ticTacToe = new TicTacToe();
+        boolean realResult = ticTacToe.checkWinPositionForVerticals(field, playerToCheck);
+        checkTestResult(realResult, "Horizontal Test Lost 2");
     }
 
 
