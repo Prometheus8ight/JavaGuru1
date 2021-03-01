b82a5f29103e48b7f3794eb85bfd47e30b91cb83
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-03-01 19:44:53 +0200

    lesson6-level4-AllTask-18-21

---
+++ src/main/java/students/kristine_paskevica/lesson_6/level_4/ForInfiniteLoop.java
@@ -0,0 +1,11 @@
+package students.kristine_paskevica.lesson_6.level_4;
+
+ class ForInfiniteLoop {
+
+     public static void main(String[] args) {
+
+         for (int i = 0; i < 5; i--) {
+             System.out.println(i);
+         }
+     }
+}
