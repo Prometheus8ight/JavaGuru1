a91a00c8cbd866947abf4813d1810ff3e3f74ea6
DJ9780772 <denissjankovskis@outlook.com>
2021-03-01 17:12:01 +0200

    Level 5, Task 22

---
+++ src/main/java/students/deniss_jankovskis/lesson_6/level_5/task_22/TicTacToe.java
@@ -0,0 +1,32 @@
+package students.deniss_jankovskis.lesson_6.level_5.task_22;
+
+class TicTacToe {
+
+    public boolean isWinPositionForHorizontals(int[][] field, int playerToCheck) {
+
+        TicTacToe ticTacToe = new TicTacToe();
+        return isWinPositionForHorizontals(field, playerToCheck);
+    }
+
+    public boolean checkForHorizontalPosition(int[][] field, int playerToCheck) {
+
+        int count = 0;
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j] == playerToCheck) {
+                    count++;
+                }
+                if (count == field[i].length) {
+                    return true;
+                }
+
+            }
+
+        }
+        return false;
+
+    }
+
+}
+
+
