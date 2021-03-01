0d0e1621225f03f0733d6337d452d4ffd70d1c47
KateBristol <jekaterina.telezkina@gmail.com>
2021-03-01 16:48:51 +0200

    Lesson 6. Task 32

--- src/main/java/students/kate_br/lesson_6/level_7/task_32/FizzBuzz.java
+++ src/main/java/students/kate_br/lesson_6/level_7/task_32/FizzBuzz.java
@@ -1,17 +1,17 @@
 package students.kate_br.lesson_6.level_7.task_32;
 
- class FizzBuzz {
+class FizzBuzz {
 
-     public String detect(int number){
+    public String detect(int number) {
 
-         if (number % 3 == 0 && number % 5 == 0){
-             return "FizzBuzz";
-         }else if (number % 3 == 0){
-             return "Fizz";
-         }else if (number % 5 == 0){
-             return "Buzz";
-         }else {
-             return "Number: " + number;
-         }
-     }
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return "Number: " + number;
+        }
+    }
 }
--- src/main/java/students/kate_br/lesson_6/level_7/task_32/FizzBuzzTest.java
+++ src/main/java/students/kate_br/lesson_6/level_7/task_32/FizzBuzzTest.java
@@ -1,53 +1,53 @@
 package students.kate_br.lesson_6.level_7.task_32;
 
- class FizzBuzzTest {
+class FizzBuzzTest {
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
 
-         FizzBuzzTest test = new FizzBuzzTest();
-         test.fizzBuzzTest(15);
-         test.fizzTest(21);
-         test.buzzTest(20);
-         test.numberTest(23);
-     }
+        FizzBuzzTest test = new FizzBuzzTest();
+        test.fizzBuzzTest(15);
+        test.fizzTest(21);
+        test.buzzTest(20);
+        test.numberTest(23);
+    }
 
-     public void fizzBuzzTest(int number){
+    public void fizzBuzzTest(int number) {
 
-         String expectedResult = "FizzBuzz";
-         FizzBuzz fizzBuzz = new FizzBuzz();
-         String realResult = fizzBuzz.detect(number);
-         checkTestResult(expectedResult, realResult, "Fizz Buzz Test");
-     }
+        String expectedResult = "FizzBuzz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String realResult = fizzBuzz.detect(number);
+        checkTestResult(expectedResult, realResult, "Fizz Buzz Test");
+    }
 
-     public void fizzTest(int number){
+    public void fizzTest(int number) {
 
-         String expectedResult = "Fizz";
-         FizzBuzz fizzBuzz = new FizzBuzz();
-         String realResult = fizzBuzz.detect(number);
-         checkTestResult(expectedResult, realResult, "Fizz Test");
-     }
+        String expectedResult = "Fizz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String realResult = fizzBuzz.detect(number);
+        checkTestResult(expectedResult, realResult, "Fizz Test");
+    }
 
-     public void buzzTest(int number){
+    public void buzzTest(int number) {
 
-         String expectedResult = "Buzz";
-         FizzBuzz fizzBuzz = new FizzBuzz();
-         String realResult = fizzBuzz.detect(number);
-         checkTestResult(expectedResult, realResult, "Buzz Test");
-     }
+        String expectedResult = "Buzz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String realResult = fizzBuzz.detect(number);
+        checkTestResult(expectedResult, realResult, "Buzz Test");
+    }
 
-     public void numberTest(int number){
+    public void numberTest(int number) {
 
-         String expectedResult = "Number: " + number;
-         FizzBuzz fizzBuzz = new FizzBuzz();
-         String realResult = fizzBuzz.detect(number);
-         checkTestResult(expectedResult, realResult, "Number Test");
-     }
+        String expectedResult = "Number: " + number;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String realResult = fizzBuzz.detect(number);
+        checkTestResult(expectedResult, realResult, "Number Test");
+    }
 
-     private void checkTestResult(String expectedResult, String realResult, String testName){
-         if (realResult == expectedResult){
-             System.out.println(testName + ": Test OK");
-         }else {
-             System.out.println(testName + ": Test FAIL");
-         }
-     }
+    private void checkTestResult(String expectedResult, String realResult, String testName) {
+        if (realResult == expectedResult) {
+            System.out.println(testName + ": Test OK");
+        } else {
+            System.out.println(testName + ": Test FAIL");
+        }
+    }
 }
