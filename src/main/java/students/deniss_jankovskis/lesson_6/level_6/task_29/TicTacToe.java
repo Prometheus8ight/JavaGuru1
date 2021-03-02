877c05e15df1c68528d25b3c061600f378e0a24e
DJ9780772 <denissjankovskis@outlook.com>
2021-03-03 00:16:29 +0200

    Task 29

---
+++ src/main/java/students/deniss_jankovskis/lesson_6/level_6/task_29/TicTacToe.java
@@ -0,0 +1,15 @@
+package students.deniss_jankovskis.lesson_6.level_6.task_29;
+
+class TicTacToe {
+
+    public void printFieldToConsole(int[][] field) {
+
+        for (int[] ints : field) {
+            for (int anInt : ints) {
+                System.out.println(anInt + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
