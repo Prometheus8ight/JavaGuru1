d17f06d743e62b923598bfa626270bc742320d2a
DJ9780772 <denissjankovskis@outlook.com>
2021-03-03 00:17:08 +0200

    Task 30

---
+++ src/main/java/students/deniss_jankovskis/lesson_6/level_6/task_30/TicTacToe.java
@@ -0,0 +1,8 @@
+package students.deniss_jankovskis.lesson_6.level_6.task_30;
+
+class TicTacToe {
+
+}
+
+
+
---
+++ src/main/java/students/deniss_jankovskis/lesson_6/level_6/task_30/TicTacToeDemo.java
@@ -0,0 +1,10 @@
+package students.deniss_jankovskis.lesson_6.level_6.task_30;
+
+class TicTacToeDemo {
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.play();
+    }
+
+}
