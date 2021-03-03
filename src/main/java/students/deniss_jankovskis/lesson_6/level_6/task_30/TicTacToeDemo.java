77381d7724cfea501583fcb983b4cca10ebcc1e4
DJ9780772 <denissjankovskis@outlook.com>
2021-03-03 14:58:02 +0200

    Task 30

--- src/main/java/students/deniss_jankovskis/lesson_6/level_6/task_30/TicTacToeDemo.java
+++ src/main/java/students/deniss_jankovskis/lesson_6/level_6/task_30/TicTacToeDemo.java
@@ -1,8 +1,5 @@
 package students.deniss_jankovskis.lesson_6.level_6.task_30;
 
-import students.deniss_jankovskis.lesson_6.level_5.task_26.TicTacToe;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 class TicTacToeDemo {
@@ -87,7 +84,96 @@ class TicTacToeDemo {
         return false;
     }
 
+    public boolean isWin(int[][] field) {
+
+        boolean isWin0 = isWinPosition(field, 0);
+        boolean isWin1 = isWinPosition(field, 1);
+        return isWin0 || isWin1;
+    }
+
+    public boolean isWinPosition(int[][] field, int playerToCheck) {
+        TicTacToe ticTacToe = new TicTacToe();
+        boolean isWinHorizontal = ticTacToe.isWinPositionHorizontals(field, playerToCheck);
+        boolean isWinVertical = ticTacToe.isWinPositionVerticals(field, playerToCheck);
+        boolean isWinDiagonal = ticTacToe.isWinPositionDiagonals(field, playerToCheck);
+        return isWinHorizontal || isWinVertical || isWinDiagonal;
+    }
+
+    public boolean checkWinHorizontalPosition(int[][] field, int playerToCheck) {
+
+        int count = 0;
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+
+                if (field[i][j] == playerToCheck) {
+                    count++;
+                }
+
+                if (count == field[i].length) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    public boolean checkWinVerticalPosition(int[][] field, int playerToCheck) {
+
+        for (int j = 0; j < field.length; j++) {
+            int count = 0;
+            for (int i = 0; i < field[j].length; i++) {
+
+                if (field[i][j] == playerToCheck) {
+                    count++;
+                }
+
+                if (count == field[j].length) {
+                    return true;
+
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean checkWinDiagonalPosition(int[][] field, int playerToCheck) {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                if (field[0][0] == field[1][1] && field[1][1] == field[2][2]) {
+                    return true;
+                } else if (field[2][0] == field[1][1] && field[1][1] == field[0][2]) {
+                    return true;
+
+                }
+
+            }
+        }
+        return false;
+    }
+
+
+    public boolean isWinPositionHorizontals(int[][] field, int playerToCheck) {
+
+        TicTacToe ticTacToe = new TicTacToe();
+        return checkWinHorizontalPosition(field, playerToCheck);
+    }
+
+    public boolean isWinPositionVerticals(int[][] field, int playerToCheck) {
+
+        TicTacToe ticTacToe = new TicTacToe();
+        return checkWinVerticalPosition(field, playerToCheck);
+    }
+
+    public boolean isWinPositionDiagonals(int[][] field, int playerToCheck) {
+
+        TicTacToe ticTacToe = new TicTacToe();
+        return checkWinDiagonalPosition(field, playerToCheck);
+    }
+
+
 }
-
-
-
