104903170369e41c90c8d87f0c7d6207a62c4291
juka2506 <julija.katukova@gmail.com>
2021-03-03 10:09:38 +0200

    Lesson6 Level4 Task21

--- src/main/java/students/julija_katukova/lesson_6/level_4/task_21/TwoDimensionalArray.java
+++ src/main/java/students/julija_katukova/lesson_6/level_4/task_21/TwoDimensionalArray.java
@@ -6,26 +6,33 @@ import java.util.Random;
 class TwoDimensionalArray {
     public static void main(String[] args) {
         TwoDimensionalArray twoDimensionalArray = new TwoDimensionalArray();
-        int[][] numbers = new int [5][5]; //создайте двумерный массив
-        int sum = 0;
-        sum = twoDimensionalArray.action(numbers, sum);
+        int[][] numbers = new int[5][5]; //создайте двумерный массив
+        twoDimensionalArray.action(numbers);
+        int sum = twoDimensionalArray.sum(numbers);
         twoDimensionalArray.display(Arrays.deepToString(numbers), sum);
     }
 
-    public int action (int[][] numbers, int sum) {
+    public void action(int[][] numbers) {
         Random random = new Random();
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers[i].length; j++) {
-                numbers[i][j] = random.nextInt(10); //заполните двумерный массив случайными числами
-                sum += numbers[i][j]; //посчитайте сумму всех чисел в двумерном массиве
+                numbers[i][j] = random.nextInt(10);
+            }
+        }
+    }
+
+    public int sum(int[][] numbers) {
+        int sum = 0;
+        for (int[] number : numbers) {
+            for (int i : number) {
+                sum += i;
             }
         }
         return sum;
     }
 
-    public void display (String numbers, int sum) {
+    public void display(String numbers, int sum) {
         System.out.println(numbers);
         System.out.println(sum);
     }
-
 }
