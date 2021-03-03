7d945db108877ddf07fc30ad095569947bd74560
CoArchie <c.chikun@icloud.com>
2021-03-03 22:26:19 +0200

    Lesson 6 - task [ 23 ] (correction!)

--- src/main/java/students/arkadij/lesson_6/level_4/task_23/TicTacToe.java
+++ src/main/java/students/arkadij/lesson_6/level_4/task_23/TicTacToe.java
@@ -3,18 +3,14 @@ package students.arkadij.lesson_6.level_4.task_23;
 class TicTacToe {
 
     public boolean isWinPositionForVerticals(int[][] field, int playerToCheck){
-        for (int rowNum : field[0]){
-            int count = 0;
-            for (int colNum : field[rowNum]){
-                if ((rowNum & colNum) == playerToCheck) {
-                    count++;
-                } else {
-                    count = 0;
-                } if (count >= 3){
-                        return true;
-                    }
-                }
+        for (int colNum = 0; colNum < 3; colNum++){
+            if (field[0][colNum] == playerToCheck
+                    && field[1][colNum] == playerToCheck
+                    && field[2][colNum] == playerToCheck
+                    && field[0][colNum] != -1){
+                return true;
             }
+        }
         return false;
     }
 
