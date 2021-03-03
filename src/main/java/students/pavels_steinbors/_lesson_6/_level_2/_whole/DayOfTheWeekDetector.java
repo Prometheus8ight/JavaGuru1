507d4c9ac265919ac218a4286e9f5ad0ca5b6cb6
PavStein <pav210985@gmail.com>
2021-03-03 16:57:25 +0100

    Lesson 6 level 2 task 7 8 9 10 11 corrected

--- src/main/java/students/pavels_steinbors/_lesson_6/_level_2/_whole/DayOfTheWeekDetector.java
+++ src/main/java/students/pavels_steinbors/_lesson_6/_level_2/_whole/DayOfTheWeekDetector.java
@@ -11,19 +11,17 @@ class DayOfTheWeekDetector {
     }
 
     public String findDayOfTheWeek(int dayNumber) {
-        String day;
 
-        switch (dayNumber) {
-            case 1 -> day = "Monday";
-            case 2 -> day = "Tuesday";
-            case 3 -> day = "Wednesday";
-            case 4 -> day = "Thursday";
-            case 5 -> day = "Friday";
-            case 6 -> day = "Saturday";
-            case 7 -> day = "Sunday";
-            default -> day = "Not correct day number";
-        }
-        return day;
+        return switch (dayNumber) {
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            case 7 -> "Sunday";
+            default -> "Not correct day number";
+        };
     }
 
 }
