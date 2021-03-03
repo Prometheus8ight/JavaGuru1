25534211a9cd078ff876f8cbee8935b74b62eebd
juka2506 <julija.katukova@gmail.com>
2021-03-03 10:01:34 +0200

    Lesson6 Level3 Task16

--- src/main/java/students/julija_katukova/lesson_6/level_3/task_16/ArrayServiceTest.java
+++ src/main/java/students/julija_katukova/lesson_6/level_3/task_16/ArrayServiceTest.java
@@ -1,7 +1,6 @@
 package students.julija_katukova.lesson_6.level_3.task_16;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 class ArrayServiceTest {
     public static void main(String[] args){
@@ -16,40 +15,35 @@ class ArrayServiceTest {
     public void positiveNumbers1 (int[] arr) {
         ArrayService arrayService = new ArrayService();
         arrayService.revert(arr);
-        String realResult = Arrays.toString(arr);
-        check(realResult, "[3, 2, 1]", "Positive Numbers Test1");
+        check(arr, new int[]{3, 2, 1}, "Positive Numbers Test1");
     }
 
     public void positiveNumbers2 (int[] arr) {
         ArrayService arrayService = new ArrayService();
         arrayService.revert(arr);
-        String realResult = Arrays.toString(arr);
-        check(realResult, "[4000, 3000, 2000, 1000]", "Positive Numbers Test2");
+        check(arr, new int[]{4000, 3000, 2000, 1000}, "Positive Numbers Test2");
     }
 
     public void zeroNumber1 (int[] arr) {
         ArrayService arrayService = new ArrayService();
         arrayService.revert(arr);
-        String realResult = Arrays.toString(arr);
-        check(realResult, "[0, 0, 1, 0, 1, 0]", "Zero Number Test1");
+        check(arr, new int[]{0, 0, 1, 0, 1, 0}, "Zero Number Test1");
     }
 
     public void negativeNumbers1 (int[] arr) {
         ArrayService arrayService = new ArrayService();
         arrayService.revert(arr);
-        String realResult = Arrays.toString(arr);
-        check(realResult, "[-3, -89856151, -2, -5696511, -1]", "Negative Numbers Test1");
+        check(arr, new int[]{-3, -89856151, -2, -5696511, -1}, "Negative Numbers Test1");
     }
 
     public void mixNumbers1 (int[] arr) {
         ArrayService arrayService = new ArrayService();
         arrayService.revert(arr);
-        String realResult = Arrays.toString(arr);
-        check(realResult, "[0, 942587, -2, -87981, 1]", "Mix Numbers Test1");
+        check(arr, new int[]{0, 942587, -2, -87981, 1}, "Mix Numbers Test1");
     }
 
-    public void check (String realResult, String expectedResult, String testName) {
-        if (Objects.equals(realResult, expectedResult)) {
+    public void check (int[] realResult, int[] expectedResult, String testName) {
+        if (Arrays.equals(realResult, expectedResult)) {
             System.out.println(testName + " = OK");
         }
         else {
