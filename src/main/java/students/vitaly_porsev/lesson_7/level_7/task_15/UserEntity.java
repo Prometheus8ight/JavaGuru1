5b32c23795d05648a3721573f6a9e08693b03aa1
Prometheus <pointofview88@gmail.com>
2021-03-07 11:51:51 +0300

    Lesson 7, level 7, task 15.

---
+++ src/main/java/students/vitaly_porsev/lesson_7/level_7/task_15/UserEntity.java
@@ -0,0 +1,8 @@
+package students.vitaly_porsev.lesson_7.level_7.task_15;
+
+class UserEntity {
+    private int id;
+    private String name;
+    private String surname;
+    private int personalCode;
+}
