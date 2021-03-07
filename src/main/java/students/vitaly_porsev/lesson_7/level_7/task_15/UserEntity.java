820f819807bd5275721bf2b03e30d5e8e7c6f32c
Prometheus <pointofview88@gmail.com>
2021-03-07 22:07:46 +0300

    Lesson 7, level 7, task 15 corrected.

--- src/main/java/students/vitaly_porsev/lesson_7/level_7/task_15/UserEntity.java
+++ src/main/java/students/vitaly_porsev/lesson_7/level_7/task_15/UserEntity.java
@@ -5,4 +5,36 @@ class UserEntity {
     private String name;
     private String surname;
     private int personalCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPersonalCode(int personalCode) {
+        this.personalCode = personalCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getPersonalCode() {
+        return personalCode;
+    }
 }
