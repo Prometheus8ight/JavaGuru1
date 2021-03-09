b38c90a84fd85361befd0405b74bd0d2d02bf9d7
DJ9780772 <denissjankovskis@outlook.com>
2021-03-09 19:00:29 +0200

    Lesson 7, task 15

---
+++ src/main/java/students/deniss_jankovskis/lesson_7/task_15/UserEntity.java
@@ -0,0 +1,13 @@
+package students.deniss_jankovskis.lesson_7.task_15;
+
+class UserEntity {
+
+    private String id;
+    private String name;
+    private String surname;
+    private String personalCode;
+
+}
+
+
+
