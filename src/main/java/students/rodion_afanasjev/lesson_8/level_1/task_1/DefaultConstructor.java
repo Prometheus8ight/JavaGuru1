391c210ace59658377bffc165640897ea8abe412
RodionA <raf91@inbox.lv>
2021-03-11 01:12:45 +0200

    Lesson-8-level-1-task-1

---
+++ src/main/java/students/rodion_afanasjev/lesson_8/level_1/task_1/DefaultConstructor.java
@@ -0,0 +1,12 @@
+package students.rodion_afanasjev.lesson_8.level_1.task_1;
+
+class DefaultConstructor {
+
+    private String name;
+    private int age;
+
+    public DefaultConstructor(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
