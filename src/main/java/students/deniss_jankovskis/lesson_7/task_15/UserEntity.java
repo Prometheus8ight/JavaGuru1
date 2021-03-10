1b44d35d7745c027118a76fa205cb30db276465f
DJ9780772 <denissjankovskis@outlook.com>
2021-03-10 17:49:43 +0200

    UserEntity update

--- src/main/java/students/deniss_jankovskis/lesson_7/task_15/UserEntity.java
+++ src/main/java/students/deniss_jankovskis/lesson_7/task_15/UserEntity.java
@@ -7,6 +7,47 @@ class UserEntity {
     private String surname;
     private String personalCode;
 
+
+    public UserEntity(String id, String name, String surname, String personalCode) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.personalCode = personalCode;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPersonalCode(String personalCode) {
+        this.personalCode = personalCode;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPersonalCode() {
+        return personalCode;
+    }
+
 }
 
 
