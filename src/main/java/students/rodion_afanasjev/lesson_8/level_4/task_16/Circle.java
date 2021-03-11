b540fe5af8d24ddc70c4fe0eed0d54b3ac76806a
RodionA <raf91@inbox.lv>
2021-03-11 21:25:44 +0200

    Lesson-8-level-4-task-16

---
+++ src/main/java/students/rodion_afanasjev/lesson_8/level_4/task_16/Circle.java
@@ -0,0 +1,22 @@
+package students.rodion_afanasjev.lesson_8.level_4.task_16;
+
+class Circle extends Shape {
+
+    private double radius;
+
+    Circle(String title, double radius) {
+        super(title);
+        this.radius = radius;
+    }
+
+    @Override
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return Math.PI * radius * 2;
+    }
+
+}
