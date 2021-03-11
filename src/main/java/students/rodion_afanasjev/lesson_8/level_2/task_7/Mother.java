740a742eabd1075957b1e69402e0a69a4bcf5ae5
RodionA <raf91@inbox.lv>
2021-03-11 15:48:17 +0200

    Lesson-8-level-2-task-7

---
+++ src/main/java/students/rodion_afanasjev/lesson_8/level_2/task_7/Mother.java
@@ -0,0 +1,13 @@
+package students.rodion_afanasjev.lesson_8.level_2.task_7;
+
+class Mother {
+
+    String name;
+    int age;
+
+    public Mother(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+}
