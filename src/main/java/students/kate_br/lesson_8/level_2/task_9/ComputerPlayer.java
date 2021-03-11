374c036c8a044170b250516ff5d775232d6a0811
KateBristol <jekaterina.telezkina@gmail.com>
2021-03-11 22:27:57 +0200

    Lesson 8. Task 9

---
+++ src/main/java/students/kate_br/lesson_8/level_2/task_9/ComputerPlayer.java
@@ -0,0 +1,15 @@
+package students.kate_br.lesson_8.level_2.task_9;
+
+
+ class ComputerPlayer extends Player {
+
+     public ComputerPlayer() {
+         super();
+     }
+
+
+     public static void main(String[] args) {
+
+         ComputerPlayer player = new ComputerPlayer();
+     }
+ }
