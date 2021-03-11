4e46ec2e4a1e4592b29c9679012accd6656cc700
alex <kalashnikov_alex@yahoo.com>
2021-03-11 18:37:10 +0200

    Homework; Lesson 7; Level 7; Tasks 13,15,16.

--- src/main/java/students/alex_kalashnikov/lesson_7/level_7/task_13/WordService.java
+++ src/main/java/students/alex_kalashnikov/lesson_7/level_7/task_13/WordService.java
@@ -2,14 +2,17 @@ package students.alex_kalashnikov.lesson_7.level_7.task_13;
 
 class WordService {
 
+    private String[] createTextArray(String text) {
+        return text.split(" ");
+    }
 
-
-    public String findMostFrequentWord(String text) {
-        String[] textArray;
+    private int[] createWordCountArray(String[] textArray) {
         int[] wordCountArray;
-        textArray = text.split(" ");
         wordCountArray = new int[textArray.length];
+        return wordCountArray;
+    }
 
+    private int[] countWords(String[] textArray, int[] wordCountArray) {
         int wordCounter = 0;
         for (int wordCount = 0; wordCount < textArray.length; wordCount++) {
             for (String s : textArray) {
@@ -20,9 +23,12 @@ class WordService {
             wordCountArray[wordCount] = wordCounter;
             wordCounter = 0;
         }
+        return wordCountArray;
+    }
 
+    private String findMax(String[] textArray, int[] wordCountArray) {
         int max = 0;
-        for (int count =0; count < wordCountArray.length; count++) {
+        for (int count = 0; count < wordCountArray.length; count++) {
             if (wordCountArray[count] > max) {
                 max = count;
             }
@@ -30,5 +36,11 @@ class WordService {
         return textArray[max];
     }
 
+    public String findMostFrequentWord(String text) {
+        String[] textArray = createTextArray(text);
+        int[] wordCountArray = createWordCountArray(textArray);
+        countWords(textArray, wordCountArray);
+        return findMax(textArray, wordCountArray);
+    }
 
 }
