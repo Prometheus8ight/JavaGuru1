35040428188955a621f2f77b1e761c3f21907d72
KateBristol <jekaterina.telezkina@gmail.com>
2021-03-11 21:21:24 +0200

    Lesson 8. Task 3

---
+++ src/main/java/students/kate_br/lesson_8/level_1/task_3/DefaultConstructorV3.java
@@ -0,0 +1,25 @@
+package students.kate_br.lesson_8.level_1.task_3;
+
+ class DefaultConstructorV3 {
+
+     private String fullName;
+     private int age;
+     private boolean male;
+     private boolean female;
+
+     public String getFullName() {
+         return fullName;
+     }
+
+     public boolean isFemale() {
+         return female;
+     }
+
+     public boolean isMale() {
+         return male;
+     }
+
+     public int getAge() {
+         return age;
+     }
+ }
---
+++ src/main/java/students/kate_br/lesson_8/level_1/task_3/DefaultConstructorV3Demo.java
@@ -0,0 +1,14 @@
+package students.kate_br.lesson_8.level_1.task_3;
+
+ class DefaultConstructorV3Demo {
+
+     public static void main(String[] args) {
+
+         DefaultConstructorV3 constructorV3 = new DefaultConstructorV3();
+         System.out.println(constructorV3.getFullName());
+         System.out.println(constructorV3.getAge());
+         System.out.println(constructorV3.isMale());
+         System.out.println(constructorV3.isFemale());
+
+     }
+}
