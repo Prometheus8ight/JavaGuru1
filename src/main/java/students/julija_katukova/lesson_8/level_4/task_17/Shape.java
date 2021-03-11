e08ab503ba2e1e5d68fc24bf3320610681656a45
juka2506 <julija.katukova@gmail.com>
2021-03-11 16:20:07 +0200

    Lesson8 Level4 Task17

---
+++ src/main/java/students/julija_katukova/lesson_8/level_4/task_17/Shape.java
@@ -0,0 +1,14 @@
+package students.julija_katukova.lesson_8.level_4.task_17;
+
+abstract class Shape {
+
+    private String title;
+
+    Shape(String title) {
+        this.title = title;
+    }
+
+    abstract double calculateArea();
+    abstract double calculatePerimeter();
+
+}
