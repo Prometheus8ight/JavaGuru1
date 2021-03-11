af308a5b965fcfd4af4c03485b170eb2d85a11da
PavStein <pav210985@gmail.com>
2021-03-11 14:03:34 +0100

    Lesson 8 level 1 task 1

---
+++ src/main/java/students/pavels_steinbors/_lesson_8/_level_1/_task_1/DefaultConstructor.java
@@ -0,0 +1,11 @@
+package students.pavels_steinbors._lesson_8._level_1._task_1;
+
+class DefaultConstructor {
+
+    String name;
+    int age;
+
+    public void printInfo() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
