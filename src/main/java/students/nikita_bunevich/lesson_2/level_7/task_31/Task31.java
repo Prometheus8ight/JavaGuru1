35dce4ab7be530ab56edf340cf0a28c21c5a589f
NikitaBunevich <nikitei2001@gmail.com>
2021-03-11 19:22:10 +0200

    st-nikita-bunevich-lesson2-level7-task31

---
+++ src/main/java/students/nikita_bunevich/lesson_2/level_7/task_31/Task31.java
@@ -0,0 +1,16 @@
+package students.nikita_bunevich.lesson_2.level_7.task_31;
+
+class Task31 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Бобер у борбра ");
+        // System.out.println("украл");
+        System.out.println("отдолжил на время ");
+        System.out.println("сковородку. ");
+        /*
+        System.out.println("Чтобы пожарить ");
+        System.out.println("пельмени.");
+        */
+    }
+}
