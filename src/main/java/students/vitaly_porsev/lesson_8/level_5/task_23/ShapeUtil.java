39aeb60c81f04cd435d4da97f899b46e70c1a662
Prometheus <pointofview88@gmail.com>
2021-03-12 18:34:20 +0300

    Lesson 8, level 5, task 23.

---
+++ src/main/java/students/vitaly_porsev/lesson_8/level_5/task_23/ShapeUtil.java
@@ -0,0 +1,11 @@
+package students.vitaly_porsev.lesson_8.level_5.task_23;
+
+import java.util.Random;
+
+class ShapeUtil {
+
+    Triangle createRandomTriangle(){
+        Random random = new Random();
+        return new Triangle(random.nextInt(100), random.nextInt(100), random.nextInt(200));
+    }
+}
---
+++ src/main/java/students/vitaly_porsev/lesson_8/level_5/task_23/Triangle.java
@@ -0,0 +1,21 @@
+package students.vitaly_porsev.lesson_8.level_5.task_23;
+
+class Triangle {
+
+    private double SideA, SideB, SideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        SideA = sideA;
+        SideB = sideB;
+        SideC = sideC;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "SideA = " + SideA +
+                ", SideB = " + SideB +
+                ", SideC = " + SideC +
+                '}';
+    }
+}
