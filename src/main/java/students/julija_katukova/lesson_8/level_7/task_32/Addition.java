e2295f35bf4534ae245de290104b49d81bc124a6
juka2506 <julija.katukova@gmail.com>
2021-03-13 22:13:57 +0200

    Lesson8 Level7 Task32

---
+++ src/main/java/students/julija_katukova/lesson_8/level_7/task_32/Addition.java
@@ -0,0 +1,14 @@
+package students.julija_katukova.lesson_8.level_7.task_32;
+
+class Addition extends TwoArgumentMathOperation {
+
+	public Addition(MathOperation leftArgument,
+					MathOperation rightArgument) {
+		super(leftArgument, rightArgument);
+	}
+
+	@Override
+	public double calculate() {
+		return calculateLeftSide() + calculateRightSide();
+	}
+}
