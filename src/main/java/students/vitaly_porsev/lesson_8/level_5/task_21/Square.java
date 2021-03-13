926ad5a83b765f90c49540f2dfb5ce3ef2097d5d
Prometheus <pointofview88@gmail.com>
2021-03-13 13:38:57 +0300

    Lesson 8, level 5, task 21 corrected.

--- src/main/java/students/vitaly_porsev/lesson_8/level_5/task_21/Square.java
+++ src/main/java/students/vitaly_porsev/lesson_8/level_5/task_21/Square.java
@@ -2,16 +2,22 @@ package students.vitaly_porsev.lesson_8.level_5.task_21;
 
 class Square {
 
-    private double side;
+    private double sideA, sideB, SideC, SideD;
 
-    public Square(double side) {
-        this.side = side;
+    public Square(double sideA) {
+        this.sideA = sideA;
+        this.sideB = sideA;
+        this.SideC = sideA;
+        this.SideD = sideA;
     }
 
     @Override
     public String toString() {
         return "Square{" +
-                "side = " + side +
+                "sideA=" + sideA +
+                ", sideB=" + sideB +
+                ", SideC=" + SideC +
+                ", SideD=" + SideD +
                 '}';
     }
 }
