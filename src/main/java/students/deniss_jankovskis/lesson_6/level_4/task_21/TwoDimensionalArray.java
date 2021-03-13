0db756317176888092a0ba2a153d26ac454b3e45
DJ9780772 <denissjankovskis@outlook.com>
2021-03-14 00:07:20 +0200

    Task 21 update

--- src/main/java/students/deniss_jankovskis/lesson_6/level_4/task_21/TwoDimensionalArray.java
+++ src/main/java/students/deniss_jankovskis/lesson_6/level_4/task_21/TwoDimensionalArray.java
@@ -6,22 +6,30 @@ class TwoDimensionalArray {
 
 
     public void fillTwoDimArr(int[][] arr) {
+
         Random random = new Random();
         for (int i = 0; i < arr.length; i++) {
-            arr[i][i] = random.nextInt(20);
-        }
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = random.nextInt(20);
+            }
 
+        }
 
     }
 
     public int sumTwoDimArr(int[][] arr) {
+
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            sum += arr[i][i];
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
 
         }
+
         return sum;
     }
 
 
 }
+
