32882488c41e69950587588aded61a00a1622874
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-03-13 20:53:40 +0100

    Bogdans Pavlovs - Lesson 8 - Level 1 - Tasks 1 to 4

---
+++ src/main/java/students/bogdans_pavlovs/lesson_8/level_1/task_1/DefaultConstructorDemo.java
@@ -0,0 +1,12 @@
+package students.bogdans_pavlovs.lesson_8.level_1.task_1;
+
+class DefaultConstructorDemo {
+
+    public static void main(String[] args) {
+
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+
+    }
+
+
+}
---
+++ src/main/java/students/bogdans_pavlovs/lesson_8/level_1/task_4/Person.java
@@ -0,0 +1,5 @@
+package students.bogdans_pavlovs.lesson_8.level_1.task_4;
+
+class Person {
+    // 'extends Object' takes place by default
+}
