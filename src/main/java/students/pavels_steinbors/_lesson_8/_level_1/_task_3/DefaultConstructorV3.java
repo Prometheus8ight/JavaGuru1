9c68684064caba146878c2d6b3444f078a9d344a
PavStein <pav210985@gmail.com>
2021-03-14 11:39:17 +0100

    Lesson 8 level 1 task 3

---
+++ src/main/java/students/pavels_steinbors/_lesson_8/_level_1/_task_3/DefaultConstructorV3.java
@@ -0,0 +1,22 @@
+package students.pavels_steinbors._lesson_8._level_1._task_3;
+
+class DefaultConstructorV3 {
+
+    private String fullName;
+    private int age;
+    private boolean male;
+    private Boolean female;
+
+    public String getFullName () {
+        return fullName;
+    }
+    public int getAge () {
+        return age;
+    }
+    public boolean getMale () {
+        return male;
+    }
+    public Boolean getFemale () {
+        return female;
+    }
+}
