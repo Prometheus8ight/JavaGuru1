07935bbbda21a2c45cf479e90227e24e30acb6a8
PavStein <pav210985@gmail.com>
2021-03-14 16:06:33 +0100

    Lesson 8 level 3 task 12

---
+++ src/main/java/students/pavels_steinbors/_lesson_8/_level_3/_task_12/Human.java
@@ -0,0 +1,9 @@
+package students.pavels_steinbors._lesson_8._level_3._task_12;
+
+class Human extends LoanSystem {
+
+    @Override
+    boolean canReceiveLoan(Human human) {
+        return false;
+    }
+}
