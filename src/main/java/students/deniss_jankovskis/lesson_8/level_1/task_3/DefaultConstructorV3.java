992fe1003012be47b373f0fdca3f64e1b979fe53
DJ9780772 <denissjankovskis@outlook.com>
2021-03-14 14:41:48 +0200

    Lesson 8, Level 1

---
+++ src/main/java/students/deniss_jankovskis/lesson_8/level_1/task_3/DefaultConstructorV3.java
@@ -0,0 +1,25 @@
+package students.deniss_jankovskis.lesson_8.level_1.task_3;
+
+class DefaultConstructorV3 {
+
+    private String fullName;
+    private int age;
+    private boolean male;
+    private boolean female;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean getMale() {
+        return male;
+    }
+
+    public boolean getFemale() {
+        return female;
+    }
+}
