4fe7ff7ea46260c305a4e57ac792f176a9a4aedc
Prometheus <pointofview88@gmail.com>
2021-03-16 15:53:13 +0300

    Lesson 8, level 7, task 32.

---
+++ src/main/java/students/vitaly_porsev/lesson_8/level_7/task_32/MathOperationDemo.java
@@ -0,0 +1,36 @@
+package students.vitaly_porsev.lesson_8.level_7.task_32;
+
+class MathOperationDemo {
+
+    public static void main(String[] args) {
+        // build and calculate: 10 + 20
+        Argument arg1 = new Argument(10);
+        Argument arg2 = new Argument(20);
+        MathOperation addition = new Addition(arg1, arg2);
+        System.out.println("Left side Addition result = " + addition.calculate());
+
+        // build and calculate: 50 - 20
+        Argument arg3 = new Argument(50);
+        Argument arg4 = new Argument(20);
+        MathOperation subtraction = new Subtraction(arg3, arg4);
+        System.out.println("Right side Subtraction result = " + addition.calculate());
+
+        // build and calculate: (10 + 20) + (50 - 20)
+        MathOperation addition1 = new Addition(addition, subtraction);
+        System.out.println("Final Addition result = " + addition1.calculate());
+
+        Argument arg5 = new Argument(10);
+        Argument arg6 = new Argument(5);
+        MathOperation subtraction2 = new Subtraction(arg5, arg6);
+        System.out.println("Left side Subtraction result = " + subtraction2.calculate());
+
+        Argument arg7 = new Argument(20);
+        Argument arg8 = new Argument(5);
+        MathOperation divide = new Divide(arg7, arg8);
+        System.out.println("Right side Divide result = " + divide.calculate());
+
+        MathOperation multiply = new Multiply(subtraction2, divide);
+        System.out.println("Final Multiply result = " + multiply.calculate());
+
+    }
+}
