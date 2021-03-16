e9d03dde48a3ee34c8c05492065ada4efee558d4
bpavlovs <bogdans.pavlovs@hotmail.com>
2021-03-16 01:39:41 +0100

    Bogdans Pavlovs - Lesson 8 - Level 2 - Tasks 5 to 9

---
+++ src/main/java/students/bogdans_pavlovs/lesson_8/level_2/task_7/Mother.java
@@ -0,0 +1,13 @@
+package students.bogdans_pavlovs.lesson_8.level_2.task_7;
+
+class Mother {
+
+    String hairColor;
+    String eyeColor;
+
+    public Mother(String hairColor, String eyeColor) {
+
+        this.eyeColor = eyeColor;
+        this.hairColor = hairColor;
+    }
+}
---
+++ src/main/java/students/bogdans_pavlovs/lesson_8/level_2/task_8/HumanPlayer.java
@@ -0,0 +1,11 @@
+package students.bogdans_pavlovs.lesson_8.level_2.task_8;
+
+class HumanPlayer extends Player {
+
+    HumanPlayer(String name) {
+
+        super(name);
+
+    }
+
+}
