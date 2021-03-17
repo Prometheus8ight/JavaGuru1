956be2dd44049327e45d3b4fb0f29635552bc2c9
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-03-17 09:41:50 +0200

    lesson8-level2-Task7

---
+++ src/main/java/students/kristine_paskevica/lesson_8/level_2/task_7/Mother.java
@@ -0,0 +1,13 @@
+package students.kristine_paskevica.lesson_8.level_2.task_7;
+
+ class Mother {
+     String name;
+     String workplace;
+     int age;
+
+     public Mother(String name, String workplace, int age){
+         this.name=name;
+         this.workplace=workplace;
+         this.age=age;
+     }
+}
