8582090dab21d2253d166a68a77b64ca7e8b8159
DJ9780772 <denissjankovskis@outlook.com>
2021-03-19 14:16:03 +0200

    Lesson 9, task 8

---
+++ src/main/java/students/deniss_jankovskis/lesson_9/level_2/task_8/Task_8.java
@@ -0,0 +1,14 @@
+package students.deniss_jankovskis.lesson_9.level_2.task_8;
+
+class ProtectedClass {
+
+    private String title;
+    private String author;
+
+    /*
+    Модификатор protected не может быть применён к классу и интерфейсам.
+    Protected могут быть объявлены методы и поля.
+    */
+
+
+}
