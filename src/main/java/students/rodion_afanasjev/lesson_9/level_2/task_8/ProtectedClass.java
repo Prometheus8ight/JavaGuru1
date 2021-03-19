59652ff11b41f878a82956c8108a7cdc4caa71b3
RodionA <raf91@inbox.lv>
2021-03-19 22:05:28 +0200

    Lesson-9-level-2-task-8

---
+++ src/main/java/students/rodion_afanasjev/lesson_9/level_2/task_8/ProtectedClass.java
@@ -0,0 +1,10 @@
+package students.rodion_afanasjev.lesson_9.level_2.task_8;
+
+// Модификатор доступа protected в Java не может быть применен к классу.
+
+class ProtectedClass {
+
+    private String title;
+    private String author;
+
+}
