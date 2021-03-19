00a2f4ce3b7f22273b6cb377ea5be4b04f56fa18
DJ9780772 <denissjankovskis@outlook.com>
2021-03-19 14:18:21 +0200

    Lesson 9, task 9

---
+++ src/main/java/students/deniss_jankovskis/lesson_9/level_2/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,11 @@
+package students.deniss_jankovskis.lesson_9.level_2.task_9;
+
+class PrivateConstructorDemo {
+
+    public static void main(String[] args) {
+
+        PrivateConstructor privateConstructor = new PrivateConstructor();
+
+
+    }
+}
