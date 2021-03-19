055d201282b88942381391a4ff85bf8071ec59ce
Julija Katukova <julija.katukova@gmail.com>
2021-03-19 13:28:39 +0200

    Lesson9 Level2 Task9

---
+++ src/main/java/students/julija_katukova/lesson_9/level_2/task_9/PrivateConstructor.java
@@ -0,0 +1,4 @@
+package main.java.students.julija_katukova.lesson_9.level_2.task_9;
+
+class Private {
+}
---
+++ src/main/java/students/julija_katukova/lesson_9/level_2/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,8 @@
+package main.java.students.julija_katukova.lesson_9.level_2.task_9;
+
+class PrivateConstructorDemo {
+    public static void main(String[] args) {
+        /*PrivateConstructor privateConstructor = new PrivateConstructor();
+        Private private = new Private();*/
+    }
+}
