301d5ee8cd382b690b81509aacba3d3456e12f82
RodionA <raf91@inbox.lv>
2021-03-20 00:19:12 +0200

    Lesson-9-level-2-task-11

---
+++ src/main/java/students/rodion_afanasjev/lesson_9/level_2/task_11/Mother.java
@@ -0,0 +1,13 @@
+package students.rodion_afanasjev.lesson_9.level_2.task_11;
+
+class Mother {
+
+    private String name;
+    private int age;
+
+    protected Mother(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+}
