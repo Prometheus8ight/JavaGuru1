d019bce8270df3a1db7c609a03df586f63855983
DJ9780772 <denissjankovskis@outlook.com>
2021-03-19 14:23:17 +0200

    Lesson 9, task 12

---
+++ src/main/java/students/deniss_jankovskis/lesson_9/level_2/task_12/MainClass.java
@@ -0,0 +1,12 @@
+package students.deniss_jankovskis.lesson_9.level_2.task_12;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        C c = new C();
+    }
+
+    /*
+    Выведутся строки всех конструкторов
+     */
+}
