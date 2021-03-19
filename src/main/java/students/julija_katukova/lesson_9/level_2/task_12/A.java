283e5fd706016feaead0a65ef9f42216308f3057
Julija Katukova <julija.katukova@gmail.com>
2021-03-19 13:54:42 +0200

    Lesson9 Level2 Task12

---
+++ src/main/java/students/julija_katukova/lesson_9/level_2/task_12/A.java
@@ -0,0 +1,20 @@
+package main.java.students.julija_katukova.lesson_9.level_2.task_12;
+
+class A {
+    public A() {
+        System.out.println("Class A Constructor");
+    }
+}
+
+class B extends A {
+    public B() {
+        System.out.println("Class B Constructor");
+    }
+}
+
+class C extends B {
+    public C() {
+        System.out.println("Class C Constructor");
+    }
+}
+
---
+++ src/main/java/students/julija_katukova/lesson_9/level_2/task_12/MainClass.java
@@ -0,0 +1,12 @@
+package main.java.students.julija_katukova.lesson_9.level_2.task_12;
+
+public class MainClass {
+    public static void main(String[] args) {
+        C c = new C();
+        /* The result will be the following:
+        Class A Constructor
+        Class B Constructor
+        Class C Constructor
+         */
+    }
+}
