5ae127b3df8784381a5a8afad29fffce7f6c36ed
DJ9780772 <denissjankovskis@outlook.com>
2021-03-19 14:24:26 +0200

    Lesson 9, task 13

---
+++ src/main/java/students/deniss_jankovskis/lesson_9/level_2/task_13/Task_13.java
@@ -0,0 +1,14 @@
+package students.deniss_jankovskis.lesson_9.level_2.task_13;
+
+class ClassX {
+    public ClassX(int i) {
+        System.out.println(1);
+    }
+}
+
+class ClassY extends ClassX {
+    public ClassY(int i) {
+        super(i);
+        System.out.println(2);
+    }
+}
