8006d53dc6f30900dbb0b8f4cbee80796582ed0c
NikitaBunevich <nikitei2001@gmail.com>
2021-03-21 17:55:24 +0200

    st-nikita-bunevich-lesson3-level1-task1-6

---
+++ src/main/java/students/nikita_bunevich/lesson_3/level_1/RobotDemo.java
@@ -0,0 +1,19 @@
+package students.nikita_bunevich.lesson_3.level_1;
+
+class RobotDemo {
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+        robot.sayHello();
+        robot.sayYourName();
+
+        Robot bober = new Robot();
+        bober.sayHello();
+        bober.sayYourName();
+
+        Robot utka = new Robot();
+        utka.sayHello();
+        utka.sayYourName();
+
+
+    }
+}
