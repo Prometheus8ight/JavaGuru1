eea1a240269c84aaabb1657f0ecc2bdd84cb0c30
C0mply <c0mplyster1@gmail.com>
2021-03-21 14:28:10 +0300

    Lesson 6 Homework

--- src/main/java/students/andrew_galashin/lesson_6/level_3/task_12/ArrayService.java
+++ src/main/java/students/andrew_galashin/lesson_6/level_3/task_12/ArrayService.java
@@ -2,14 +2,12 @@ package students.andrew_galashin.lesson_6.level_3.task_12;
 
 class ArrayService {
     public boolean contains(int[] arr, int numberToSearch) {
-        boolean check = false;
         for (int i : arr) {
             if (i == numberToSearch) {
-                check = true;
-                break;
+                return true;
             }
 
         }
-        return check;
+        return false;
     }
 }
