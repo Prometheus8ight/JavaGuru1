5caf511038a0158cbd209013798f8ee6f0fc5bef
Konstantins Buts <kjb.dgsv@gmail.com>
2021-03-21 14:18:11 +0200

    Repackage

--- src/main/java/students/julija_katukova/lesson_9/level_5/FraudRule.java
+++ src/main/java/students/julija_katukova/lesson_9/level_5/FraudRule.java
@@ -1,4 +1,4 @@
-package main.java.students.julija_katukova.lesson_9.level_5;
+package students.julija_katukova.lesson_9.level_5;
 
 abstract class FraudRule {
 
--- src/main/java/students/julija_katukova/lesson_9/level_5/FraudRule2Test.java
+++ src/main/java/students/julija_katukova/lesson_9/level_5/FraudRule2Test.java
@@ -1,4 +1,4 @@
-package main.java.students.julija_katukova.lesson_9.level_5;
+package students.julija_katukova.lesson_9.level_5;
 
 class FraudRule2Test {
     public static void main(String[] args) {
