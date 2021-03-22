31f2f5751d1ed40b87c8c31e29c5df698f8fc2a5
NikitaBunevich <nikitei2001@gmail.com>
2021-03-22 20:48:10 +0200

    st-nikita-bunevich-lesson3-level3-task11-13

---
+++ src/main/java/students/nikita_bunevich/lesson_3/level_3/Robot.java
@@ -0,0 +1,16 @@
+package students.nikita_bunevich.lesson_3.level_3;
+
+class Robot {
+    String name;
+    Robot(String robotName) {this.name = robotName;}
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+    public void sayYourName(){
+        System.out.println("My name is " + this.name + ".");
+    }
+
+
+
+
+}
