a177a4738cb05eb90e11d7c6044a2cb36222bf02
KristinePaskevica <kristine.paskevica@inbox.lv>
2021-03-22 11:54:53 +0200

    lesson9-level2-AllTask

---
+++ src/main/java/students/kristine_paskevica/lesson_9/level_1/level_2/MatesKlase.java
@@ -0,0 +1,14 @@
+package students.kristine_paskevica.lesson_9.level_1.level_2;
+
+class MatesKlase {
+
+    String uzvards;
+    String vards;
+    String darbaVieta;
+
+    protected MatesKlase(String uzvards, String vards, String darbaVieta) {
+        this.uzvards = uzvards;
+        this.vards = vards;
+        this.darbaVieta = darbaVieta;
+    }
+}
---
+++ src/main/java/students/kristine_paskevica/lesson_9/level_1/level_2/PrivateConstructorDemo.java
@@ -0,0 +1,10 @@
+package students.kristine_paskevica.lesson_9.level_1.level_2;
+
+ class PrivateConstructorDemo {
+
+     public static void main(String[] args) {
+
+        /* PrivateConstructor privateConstructor = new PrivateConstructor();
+         Private private = new Private();*/
+     }
+}
