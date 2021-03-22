601498be50cb2b0824e1c543da5591e2dd4e3092
NikitaBunevich <nikitei2001@gmail.com>
2021-03-22 21:31:53 +0200

    st-nikita-bunevich-lesson3-level4-task14-17

---
+++ src/main/java/students/nikita_bunevich/lesson_3/task_15/Phone.java
@@ -0,0 +1,17 @@
+package students.nikita_bunevich.lesson_3.task_15;
+
+class Phone {
+    String model;
+        Phone(String newModel) {this.model = newModel;}
+        String getModel() {return this.model;}
+    }
+
+    class PhoneDemo {
+
+        public static void main(String[] args) {
+            Phone myPhone = new Phone("Huawei");
+            String phoneModel = myPhone.getModel();
+            System.out.println("Phone model = " + phoneModel);
+        }
+
+    }
