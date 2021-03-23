2d2030adbd1302394dca84b37a351af805a9b392
PavStein <pav210985@gmail.com>
2021-03-23 22:42:24 +0100

    Lesson 9 level 2 task 8

---
+++ src/main/java/students/pavels_steinbors/_lesson_9/_level_2/_task_8/ProtectedClass.java
@@ -0,0 +1,11 @@
+package students.pavels_steinbors._lesson_9._level_2._task_8;
+
+class ProtectedClass {
+
+    private String title;
+    private String author;
+}
+/* Модификатор доступа Protected, прменяется только к переменным,
+ методам и конструкторам, но не к классам. И такие переменные, методы и конструктора
+ доступны в подклассах в других пакетах и в любых классах в том же пакете.
+ */
