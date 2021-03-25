ad762dabbed1dfa13735a6a5007d47a6c914c811
Julija Katukova <julija.katukova@gmail.com>
2021-03-25 10:13:04 +0200

    Lesson10 Level1 Task2

---
+++ src/main/java/students/julija_katukova/lesson_10/level_1/task_2/ArrayUtil.java
@@ -0,0 +1,10 @@
+package students.julija_katukova.lesson_10.level_1.task_2;
+
+interface ArrayUtil {
+
+     int[] createArray(int arrayLength);
+     void fillArrayWithRandomNumbers(int[] array);
+     void printArrayToConsole(int[] array);
+     int findMaxNumber(int[] array);
+     int findMinNumber(int[] array);
+}
