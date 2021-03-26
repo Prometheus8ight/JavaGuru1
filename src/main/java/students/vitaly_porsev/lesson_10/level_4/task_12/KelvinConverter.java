bc17bcdea90fe8936bbe5ef6750e5c81c3058b87
Prometheus <pointofview88@gmail.com>
2021-03-26 18:25:57 +0300

    Lesson 10, level 4, task12.

---
+++ src/main/java/students/vitaly_porsev/lesson_10/level_4/task_12/KelvinConverter.java
@@ -0,0 +1,9 @@
+package students.vitaly_porsev.lesson_10.level_4.task_12;
+
+class KelvinConverter implements TemperatureConverter {
+
+    @Override
+    public double convert(double celsiusTemperature) {
+        return celsiusTemperature + 273.15;
+    }
+}
