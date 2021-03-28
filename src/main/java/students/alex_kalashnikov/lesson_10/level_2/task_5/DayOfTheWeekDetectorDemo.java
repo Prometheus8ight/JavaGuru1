96725734f76379b047236350a1494143769c43fe
alex <kalashnikov_alex@yahoo.com>
2021-03-28 23:46:51 +0300

    Homework; Lesson 10; Level 2; Tasks 4,5.

--- src/main/java/students/alex_kalashnikov/lesson_10/level_2/task_5/DayOfTheWeekDetectorDemo.java
+++ src/main/java/students/alex_kalashnikov/lesson_10/level_2/task_5/DayOfTheWeekDetectorDemo.java
@@ -2,10 +2,23 @@ package students.alex_kalashnikov.lesson_10.level_2.task_5;
 
 class DayOfTheWeekDetectorDemo {
 
-    DayOfTheWeekDetector versionArray;
+    DayOfTheWeekDetector detector;
 
-    public DayOfTheWeekDetectorDemo() {
-        versionArray = new DayOfTheWeekDetectorArrayVersion();
+    public DayOfTheWeekDetectorDemo(DayOfTheWeekDetector detector) {
+        this.detector = detector;
+    }
+
+    public static void main(String[] args) {
+
+        DayOfTheWeekDetectorDemo demoIf = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorIfVersion());
+        demoIf.run();
+
+        DayOfTheWeekDetectorDemo demoSwitch = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorSwitchVersion());
+        demoSwitch.run();
+
+        DayOfTheWeekDetectorDemo demoArray = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorArrayVersion());
+        demoArray.run();
+
     }
 
     void run() {
@@ -14,18 +27,11 @@ class DayOfTheWeekDetectorDemo {
             java.util.Scanner scanner = new java.util.Scanner(System.in);
             System.out.print("Enter day of the week (number 1 - 7): ");
             int dayNumber = scanner.nextInt();
-            System.out.println(versionArray.detectDayName(dayNumber));
+            System.out.println(detector.detectDayName(dayNumber));
             if (dayNumber >= 1 && dayNumber <= 7) {
                 x = false;
             }
         }
-    }
-
-    public static void main(String[] args) {
-
-        DayOfTheWeekDetectorDemo demoArray = new DayOfTheWeekDetectorDemo();
-        demoArray.run();
-
     }
 
 }
