265f0b7595b28aa7b6438f8bae7c0fbd9950de85
RodionA <raf91@inbox.lv>
2021-03-29 16:09:38 +0300

    Lesson-10-level-3-task-10

---
+++ src/main/java/students/rodion_afanasjev/lesson_10/level_3/task_10/ProductDatabase.java
@@ -0,0 +1,10 @@
+package students.rodion_afanasjev.lesson_10.level_3.task_10;
+import java.util.Optional;
+
+interface ProductDatabase {
+
+    void save(Product product);
+
+    Optional<Product> findByTitle(String productTitle);
+
+}
