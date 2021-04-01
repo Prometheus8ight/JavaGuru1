f2b40032c637206182d088e080f530c5aee8f3a7
Prometheus <pointofview88@gmail.com>
2021-04-01 12:41:17 +0300

    Lesson 11, level 1, task2.

---
+++ src/main/java/students/vitaly_porsev/lesson_11/level_1/task_2/Lists.java
@@ -0,0 +1,14 @@
+package students.vitaly_porsev.lesson_11.level_1.task_2;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+
+    List<String> str = new ArrayList<>();
+    List<Integer> integers = new ArrayList<>();
+    List<Object> obj = new ArrayList<>();
+    List<Long> l = new ArrayList<>();
+    List<File> file = new ArrayList<>();
+}
