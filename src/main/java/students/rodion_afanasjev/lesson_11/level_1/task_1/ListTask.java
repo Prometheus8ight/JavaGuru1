e1b2c535e64d3f7ef7ccfb8717b124145e78ffc8
RodionA <raf91@inbox.lv>
2021-04-01 14:39:12 +0300

    Lesson-11-level-1-task-1

---
+++ src/main/java/students/rodion_afanasjev/lesson_11/level_1/task_1/ListTask.java
@@ -0,0 +1,18 @@
+package students.rodion_afanasjev.lesson_11.level_1.task_1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class ListTask {
+
+    public static void main(String[] args) {
+
+        List<String> books = new ArrayList<>();
+        List<String> students = new LinkedList<>();
+
+    }
+    
+}
+// класс ArrayList это список на основе массива, а LinkedList это список на основе
+// элементов и связи между ними.
