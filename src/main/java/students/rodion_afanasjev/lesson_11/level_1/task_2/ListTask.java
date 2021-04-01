0dd37f5b1f50b66d4f73740207407b7414286053
RodionA <raf91@inbox.lv>
2021-04-01 14:47:15 +0300

    Lesson-11-level-1-task-2

---
+++ src/main/java/students/rodion_afanasjev/lesson_11/level_1/task_2/ListTask.java
@@ -0,0 +1,17 @@
+package students.rodion_afanasjev.lesson_11.level_1.task_2;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class ListTask {
+
+    public static void main(String[] args) {
+
+        List<String> words = new ArrayList<>();
+        List<Integer> value = new ArrayList<>();
+        List<Character> alphabet = new LinkedList<>();
+        List<Object> box = new LinkedList<>();
+
+    }
+
+}
