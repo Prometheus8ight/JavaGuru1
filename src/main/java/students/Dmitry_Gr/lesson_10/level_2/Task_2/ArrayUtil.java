46bb8100927e172889df0a71a4e3aec8907ab144
DmitryGrishin1805 <dg9261115311@gmail.com>
2021-04-04 19:36:25 +0300

    new task 2 level 1 lesson 10

---
+++ src/main/java/students/Dmitry_Gr/lesson_10/level_2/Task_2/ArrayUtil.java
@@ -0,0 +1,15 @@
+package students.Dmitry_Gr.lesson_10.level_2.Task_2;
+
+interface ArrayUtil {
+
+        int[] createArray(int arrayLength);
+
+        void fillArrayWithRandomNumbers(int[] array);
+
+        void printArrayToConsole(int[] array);
+
+        int findMaxNumber(int[] array);
+
+        int findMinNumber(int[] array);
+
+    }
