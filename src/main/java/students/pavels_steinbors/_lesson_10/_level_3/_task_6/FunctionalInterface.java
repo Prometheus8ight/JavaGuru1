d35b9903b9a4382a9ffee4072141ab0a5633290e
PavStein <pav210985@gmail.com>
2021-04-04 11:54:03 +0200

    Lesson 10 level 3 task 6

---
+++ src/main/java/students/pavels_steinbors/_lesson_10/_level_3/_task_6/FunctionalInterface.java
@@ -0,0 +1,8 @@
+package students.pavels_steinbors._lesson_10._level_3._task_6;
+
+interface FunctionalInterface {
+
+    // Функциональный интерфейс – это интерфейс, который содержат в себе только один абстрактный метод.
+
+    abstract public void abstractMethod();
+}
