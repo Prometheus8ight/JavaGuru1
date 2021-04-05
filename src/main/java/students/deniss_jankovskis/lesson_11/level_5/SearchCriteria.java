c2f23f50e77576c7010643d534ec822fdd5e54eb
DJ9780772 <denissjankovskis@outlook.com>
2021-04-06 00:48:50 +0300

    Lesson 11, level 5, tasks 28-29

---
+++ src/main/java/students/deniss_jankovskis/lesson_11/level_5/SearchCriteria.java
@@ -0,0 +1,6 @@
+package students.deniss_jankovskis.lesson_11.level_5;
+
+interface SearchCriteria {
+
+    boolean match(Book book);
+}
--- src/main/java/students/julija_katukova/lesson_11/level_3/SearchCriteria.java
+++ src/main/java/students/julija_katukova/lesson_11/level_3/SearchCriteria.java
@@ -1,6 +1,6 @@
 package students.julija_katukova.lesson_11.level_3;
 
-interface SearchCriteria {
+public interface SearchCriteria {
 
     boolean match(Book book);
 
