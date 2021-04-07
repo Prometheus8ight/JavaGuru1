400a98c879da5c22ae9655da284f98c217889809
alex <kalashnikov_alex@yahoo.com>
2021-04-07 10:58:59 +0300

    Homework; Lesson 11; Level 1; Tasks 1,2,3,4,5.

---
+++ src/main/java/students/alex_kalashnikov/lesson_11/level_1/task_1/Lists.java
@@ -0,0 +1,16 @@
+package students.alex_kalashnikov.lesson_11.level_1.task_1;
+
+import java.util.*;
+
+class Lists {
+
+    public static void main(String[] args) {
+
+        List<String> arr = new ArrayList<>();
+        List<Integer> linked = new LinkedList<>();
+        Map<Integer, String> hashMap = new HashMap<>();
+        Set<String> hashSet = new HashSet<>();
+
+    }
+
+}
