4fe23af4638ae2b2975c759692f3423de9a8f455
RodionA <raf91@inbox.lv>
2021-04-08 20:06:44 +0300

    Lesson-12-level-1-task-3

---
+++ src/main/java/students/rodion_afanasjev/lesson_12/level_1/task_3/MyException.java
@@ -0,0 +1,13 @@
+package students.rodion_afanasjev.lesson_12.level_1.task_3;
+
+class MyExceptionOne extends Throwable {
+
+}
+
+class MyExceptionTwo extends Exception {
+
+}
+
+class MyExceptionThree extends RuntimeException {
+
+}
