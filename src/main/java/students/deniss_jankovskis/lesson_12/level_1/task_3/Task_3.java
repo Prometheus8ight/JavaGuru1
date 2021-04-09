5f2f4bbbbf969f80ff77a323f34b248d6b4fd490
DJ9780772 <denissjankovskis@outlook.com>
2021-04-09 12:49:44 +0300

    Lesson 12, level 1, task 3

---
+++ src/main/java/students/deniss_jankovskis/lesson_12/level_1/task_3/Task_3.java
@@ -0,0 +1,13 @@
+package students.deniss_jankovskis.lesson_12.level_1.task_3;
+
+class MyException1 extends Throwable {
+
+}
+
+class MyException2 extends Exception {
+
+}
+
+class MyException3 extends RuntimeException {
+
+}
