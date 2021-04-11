32d07f4333f68ce1b88510432da9265a5824582d
DJ9780772 <denissjankovskis@outlook.com>
2021-04-11 13:27:51 +0300

    Lesson 12, level 3, task 21

---
+++ src/main/java/students/deniss_jankovskis/lesson_12/level_3/task_21/Task_21.java
@@ -0,0 +1,21 @@
+package students.deniss_jankovskis.lesson_12.level_3.task_21;
+
+class TestException extends Exception { }
+
+class Main {
+    public static void main(String args[]) {
+        try {
+            throw new TestException();
+        } catch(TestException t) {
+            System.out.println("Got the Test Exception");
+        } finally {
+            System.out.println("Inside finally block ");
+        }
+    }
+}
+
+/*
+1. Got the Test Exception
+   Inside finally block.
+   finally будет выполнен после блока catch.
+ */
