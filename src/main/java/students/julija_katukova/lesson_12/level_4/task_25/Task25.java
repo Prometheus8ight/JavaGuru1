00286d757b913ce5c04ee3c3dc770d6a04d07afd
Julija Katukova <julija.katukova@gmail.com>
2021-04-11 22:41:54 +0300

    Lesson12 Level4 Task25

---
+++ src/main/java/students/julija_katukova/lesson_12/level_4/task_25/Task25.java
@@ -0,0 +1,14 @@
+package students.julija_katukova.lesson_12.level_4.task_25;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task25 {
+
+    public static void main(String[] args) throws Exception {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 1_000_000_000; i++) {
+            list.add(i, i^i);
+        }
+    }
+}
