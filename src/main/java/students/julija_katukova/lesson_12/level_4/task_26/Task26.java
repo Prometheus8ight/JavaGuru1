db25325c13d526d3cd0e12e39e376f9d119b989d
Julija Katukova <julija.katukova@gmail.com>
2021-04-11 22:49:03 +0300

    Lesson12 Level4 Task26

---
+++ src/main/java/students/julija_katukova/lesson_12/level_4/task_26/Task26.java
@@ -0,0 +1,14 @@
+package students.julija_katukova.lesson_12.level_4.task_26;
+
+class Task26 {
+
+    public static void main(String[] args) {
+        Task26 task26 = new Task26();
+        task26.print();
+    }
+
+    private void print() {
+        System.out.println("Hello!");
+        print();
+    }
+}
