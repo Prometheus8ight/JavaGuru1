136bded1f59196d62e8214c70f3b0a9d91c820a4
NikitaBunevich <nikitei2001@gmail.com>
2021-04-17 20:02:17 +0300

    st-nikita-bunevich-lesson4-level1-task1-4

---
+++ src/main/java/students/nikita_bunevich/lesson_4/task_3/Task3.java
@@ -0,0 +1,36 @@
+package students.nikita_bunevich.lesson_4.task_3;
+
+import java.util.Scanner;
+
+class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Введите целое число от 1 до 7:");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("Понедельник");
+                break;
+            case 2:
+                System.out.println("Вторник");
+                break;
+            case 3:
+                System.out.println("Среда");
+                break;
+            case 4:
+                System.out.println("Четверг");
+                break;
+            case 5:
+                System.out.println("Пятница");
+                break;
+            case 6:
+                System.out.println("Суббота");
+                break;
+            case 7:
+                System.out.println("Воскресенье");
+               break;
+
+        }
+    }
+}
---
+++ src/main/java/students/nikita_bunevich/lesson_4/task_4/Task4.java
@@ -0,0 +1,17 @@
+package students.nikita_bunevich.lesson_4.task_4;
+
+import java.util.Scanner;
+
+class Task4 {
+    public static void main(String[] args) {
+        System.out.println("Введите целое число:");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        if (num % 2 == 0)
+            System.out.println("Это число четное: " + num);
+        else{
+            System.out.println("Это число не четное: " + num);
+        }
+    }
+}
