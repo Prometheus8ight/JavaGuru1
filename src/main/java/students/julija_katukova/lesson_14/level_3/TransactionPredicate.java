fcbf30a610a83b96e3c241a1d728633a414b4375
Julija Katukova <julija.katukova@gmail.com>
2021-04-24 10:23:08 +0300

    Lesson14 Level3 Task18-24

--- src/main/java/students/julija_katukova/lesson_14/level_3/TransactionPredicate.java
+++ src/main/java/students/julija_katukova/lesson_14/level_3/TransactionPredicate.java
@@ -1,7 +1,7 @@
 package students.julija_katukova.lesson_14.level_3;
 
 @FunctionalInterface
-public interface TransactionPredicate {
+interface TransactionPredicate {
 
     boolean test(Transaction transaction);
 }
