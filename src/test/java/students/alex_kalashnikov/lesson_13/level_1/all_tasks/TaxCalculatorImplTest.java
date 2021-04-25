a8cebf68a4d7ba6a9d593d7455552156db7960d0
Konstantins Buts <kjb.dgsv@gmail.com>
2021-04-25 16:04:23 +0300

    Use JUnit 5 instead of 4

--- src/test/java/students/alex_kalashnikov/lesson_13/level_1/all_tasks/TaxCalculatorImplTest.java
+++ src/test/java/students/alex_kalashnikov/lesson_13/level_1/all_tasks/TaxCalculatorImplTest.java
@@ -1,10 +1,10 @@
 package students.alex_kalashnikov.lesson_13.level_1.all_tasks;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaxCalculatorImplTest {
 
--- src/test/java/students/alex_kalashnikov/lesson_13/level_3/all_tasks/GameOfLifeNextGenerationCalculatorTest.java
+++ src/test/java/students/alex_kalashnikov/lesson_13/level_3/all_tasks/GameOfLifeNextGenerationCalculatorTest.java
@@ -1,8 +1,8 @@
 package students.alex_kalashnikov.lesson_13.level_3.task_10;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class GameOfLifeNextGenerationCalculatorTest {
 
