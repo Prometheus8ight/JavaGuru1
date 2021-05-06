6d81ff1e6853032ffd4078c656aac07a5610c91b
PavStein <pav210985@gmail.com>
2021-05-06 13:20:56 +0200

    Lesson 9 level 2 task 10

---
+++ src/main/java/students/pavels_steinbors/_lesson_9/_level_2/_task_10/ConstructorChaining.java
@@ -0,0 +1,16 @@
+package students.pavels_steinbors._lesson_9._level_2._task_10;
+
+class ConstructorChaining {
+
+    private String constructorName;
+    private int parameterCount;
+
+    private ConstructorChaining(String constructorName) {
+        this.constructorName = constructorName;
+    }
+
+    public ConstructorChaining(String constructorName, int parameterCount) {
+        this(constructorName);
+        this.parameterCount = parameterCount;
+    }
+}
