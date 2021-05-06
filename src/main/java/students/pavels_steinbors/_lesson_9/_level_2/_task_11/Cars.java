fe672d3184d4691abc3a6897cf6139f7bae46c84
PavStein <pav210985@gmail.com>
2021-05-06 13:35:06 +0200

    Lesson 9 level 2 task 11

---
+++ src/main/java/students/pavels_steinbors/_lesson_9/_level_2/_task_11/Cars.java
@@ -0,0 +1,14 @@
+package students.pavels_steinbors._lesson_9._level_2._task_11;
+
+class Cars {
+
+    String nameOfModel;
+    int yearOfManufacture;
+    String typeOfCar;
+
+    protected Cars(String nameOfModel, int yearOfManufacture, String typeOfCar) {
+        this.nameOfModel = nameOfModel;
+        this.yearOfManufacture = yearOfManufacture;
+        this.typeOfCar = typeOfCar;
+    }
+}
