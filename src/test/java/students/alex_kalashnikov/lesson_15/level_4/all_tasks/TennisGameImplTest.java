7bb03b150f7144a271306034f562915c04ae7dc6
Konstantins Buts <kjb.dgsv@gmail.com>
2021-05-15 13:09:58 +0300

    Temporarily comment out junit 4 tests

--- src/test/java/students/alex_kalashnikov/lesson_15/level_4/all_tasks/TennisGameImplTest.java
+++ src/test/java/students/alex_kalashnikov/lesson_15/level_4/all_tasks/TennisGameImplTest.java
@@ -1,16 +1,13 @@
 package students.alex_kalashnikov.lesson_15.level_4.all_tasks;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collection;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(Parameterized.class)
+
 public class TennisGameImplTest {
 
     private final int player1Score;
@@ -23,47 +20,46 @@ public class TennisGameImplTest {
         this.expectedScore = expectedScore;
     }
 
-    @Parameters
     public static Collection<Object[]> getAllScores() {
-        return Arrays.asList(new Object[][] {
-                { 0, 0, "Love - Love" },
-                { 1, 1, "Fifteen - Fifteen" },
-                { 2, 2, "Thirty - Thirty"},
-                { 3, 3, "Deuce"},
-                { 4, 4, "Deuce"},
+        return Arrays.asList(new Object[][]{
+                {0, 0, "Love - Love"},
+                {1, 1, "Fifteen - Fifteen"},
+                {2, 2, "Thirty - Thirty"},
+                {3, 3, "Deuce"},
+                {4, 4, "Deuce"},
 
-                { 1, 0, "Fifteen - Love"},
-                { 0, 1, "Love - Fifteen"},
-                { 2, 0, "Thirty - Love"},
-                { 0, 2, "Love - Thirty"},
-                { 3, 0, "Forty - Love"},
-                { 0, 3, "Love - Forty"},
-                { 4, 0, "Win player1"},
-                { 0, 4, "Win player2"},
+                {1, 0, "Fifteen - Love"},
+                {0, 1, "Love - Fifteen"},
+                {2, 0, "Thirty - Love"},
+                {0, 2, "Love - Thirty"},
+                {3, 0, "Forty - Love"},
+                {0, 3, "Love - Forty"},
+                {4, 0, "Win player1"},
+                {0, 4, "Win player2"},
 
-                { 2, 1, "Thirty - Fifteen"},
-                { 1, 2, "Fifteen - Thirty"},
-                { 3, 1, "Forty - Fifteen"},
-                { 1, 3, "Fifteen - Forty"},
-                { 4, 1, "Win player1"},
-                { 1, 4, "Win player2"},
+                {2, 1, "Thirty - Fifteen"},
+                {1, 2, "Fifteen - Thirty"},
+                {3, 1, "Forty - Fifteen"},
+                {1, 3, "Fifteen - Forty"},
+                {4, 1, "Win player1"},
+                {1, 4, "Win player2"},
 
-                { 3, 2, "Forty - Thirty"},
-                { 2, 3, "Thirty - Forty"},
-                { 4, 2, "Win player1"},
-                { 2, 4, "Win player2"},
+                {3, 2, "Forty - Thirty"},
+                {2, 3, "Thirty - Forty"},
+                {4, 2, "Win player1"},
+                {2, 4, "Win player2"},
 
-                { 4, 3, "Advantage player1"},
-                { 3, 4, "Advantage player2"},
-                { 5, 4, "Advantage player1"},
-                { 4, 5, "Advantage player2"},
-                { 15, 14, "Advantage player1"},
-                { 14, 15, "Advantage player2"},
+                {4, 3, "Advantage player1"},
+                {3, 4, "Advantage player2"},
+                {5, 4, "Advantage player1"},
+                {4, 5, "Advantage player2"},
+                {15, 14, "Advantage player1"},
+                {14, 15, "Advantage player2"},
 
-                { 6, 4, "Win player1"},
-                { 4, 6, "Win player2"},
-                { 16, 14, "Win player1"},
-                { 14, 16, "Win player2"},
+                {6, 4, "Win player1"},
+                {4, 6, "Win player2"},
+                {16, 14, "Win player1"},
+                {14, 16, "Win player2"},
         });
     }
 
@@ -78,10 +74,9 @@ public class TennisGameImplTest {
         assertEquals(this.expectedScore, game.score());
     }
 
-    @Test
+//    @Test
     public void checkAllScoresTennisGame() {
         TennisGameImpl game = new TennisGameImpl("player1", "player2");
         checkAllScores(game);
     }
-
 }
